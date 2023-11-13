08727e72e772c3e490ae206cac750df487f8a228
BolotnikovMS <bolotnikovms@yandex.ru>
2023-11-13 21:50:58 +0500

    feat: :sparkles: Использованы компоненты, добавлена фун-я submit

--- src/views/district/DistrictPage.tsx
+++ src/views/district/DistrictPage.tsx
@@ -1,10 +1,10 @@
 import './district.scss'
 
-import { Button, Error, FormGroup, InfoMessage, Loader, SmallCard, TextInput } from '../../components'
+import { Button, CustomInput, Error, FormGroup, InfoMessage, Loader, SmallCard } from '../../components'
+import { SubmitHandler, useForm } from 'react-hook-form'
 
 import { AxiosError } from 'axios'
 import { useDistrict } from '../../hooks'
-import { useForm } from 'react-hook-form'
 
 interface IDistrictFields {
   name: string
@@ -14,9 +14,14 @@ interface IDistrictFields {
 
 export const DistrictPage = () => {
   const { districts, error, isError, isLoading } = useDistrict()
-  const { register, handleSubmit, formState: { errors, isValid }, reset, control } = useForm<IDistrictFields>({
+  const { register, handleSubmit, formState: { errors, isValid }, reset } = useForm<IDistrictFields>({
     mode: 'onBlur'
   })
+
+  const submit: SubmitHandler<IDistrictFields> = (data) => {
+    console.log(data);
+    reset()
+  }
   
   return (
     <div className="districts">
@@ -25,10 +30,10 @@ export const DistrictPage = () => {
           <h2 className="title">Районы и ГП</h2>
         </div>
         <div className="districts__form">
-          <form action="" className="form">
+          <form className="form" onSubmit={handleSubmit(submit)}>
             <div className="form__content">
               <FormGroup>
-                <TextInput
+                <CustomInput
                   label='Название Района или ГП'
                   name='name'
                   register={register}
@@ -41,17 +46,31 @@ export const DistrictPage = () => {
                   placeholder='Введите название...'
                 />
               </FormGroup>
-              <div className="form__group">
-                <label htmlFor="shortName">Короткое название</label>
-                <input type="text" id='shortName' name='shortName' className='input' />
-              </div>
-              <div className="form__group">
-                <label htmlFor="active">Используется?</label>
-                <input type="checkbox" id='active' name='active' className='input' />
-              </div>
+              <FormGroup>
+                <CustomInput
+                  label='Сокращенное название'
+                  name='shortName'
+                  register={register}
+                  error={errors.shortName?.message}
+                  validation={{
+                    required: {value: true, message: 'Поле является обязательным!'},
+                    minLength: {value: 3, message: 'Минимальная длина поля 3 символа!'},
+                    maxLength: {value: 200, message: 'Максимальная длина поля 200 символов!'}
+                  }}
+                  placeholder='Введите сокращенное название...'
+                />
+              </FormGroup>
+              <FormGroup className='form__group-row'>
+                <CustomInput
+                  label='Используется?'
+                  name='active'
+                  register={register}
+                  type='checkbox'
+                />
+              </FormGroup>
             </div>
             <div className="form__btns">
-              <Button disabled={isLoading || isError} classBtn='btn-bg_green'>Добавить</Button>
+              <Button disabled={isLoading || isError || !isValid} classBtn='btn-bg_green'>Добавить</Button>
             </div>
           </form>
         </div>
