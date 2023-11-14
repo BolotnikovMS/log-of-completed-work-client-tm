7ec046f51b8e3f9d05d15af40917ed8d906c54b9
BolotnikovMS <bolotnikovms@yandex.ru>
2023-11-14 17:07:06 +0500

    feat: :sparkles: Добавлен роут с подробным просмотром

--- src/components/routing/Router.tsx
+++ src/components/routing/Router.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom'
 
 import { DistrictPage } from '../../views/district/DistrictPage'
 import { HomePage } from '../../views/home/HomePage'
+import { Info } from '../../views/district/components/info/Info'
 import { Layout } from '../layout/Layout'
 import React from 'react'
 
@@ -11,6 +12,7 @@ export const Router: React.FC = () => {
       <Route path='/' element={<Layout />}>
         <Route index element={<HomePage />} />
         <Route path='/districts' element={<DistrictPage />} />
+        <Route path='/districts/:id/substations' element={<Info />} />
         <Route path='*' element={<div>Not Found</div>} />
       </Route>
     </Routes>
