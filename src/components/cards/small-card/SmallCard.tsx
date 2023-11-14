6ee24723d87b8bc22f5ba90f3c5b9138254a124e
BolotnikovMS <bolotnikovms@yandex.ru>
2023-11-14 17:04:13 +0500

    feat: Добавлен link из react-router

--- src/components/cards/small-card/SmallCard.tsx
+++ src/components/cards/small-card/SmallCard.tsx
@@ -1,13 +1,14 @@
 import './small-card.scss'
 
 import { IPropsSmallCard } from './smallCard.interface'
+import { Link } from 'react-router-dom'
 import React from 'react'
 import cx from 'classnames'
 
-export const SmallCard: React.FC<IPropsSmallCard> = ({ children, className, cardText }) => {
+export const SmallCard: React.FC<IPropsSmallCard> = ({ children, className, cardText, path }) => {
   return (
     <div className={cx('card', className)}>
-      <a href="!#" className='card__link'></a>
+      <Link to={path || '!#'} className='card__link'/>
       <div className="card__content">
         {children}
         <p className='card__text'>{cardText}</p>
