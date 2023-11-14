1a6ba0ad9a3026e9fcf9c15731b822acd4845d82
BolotnikovMS <bolotnikovms@yandex.ru>
2023-11-14 17:09:46 +0500

    feat: Добавлен интерфейс ПС

---
+++ src/services/substations/substation.interface.ts
@@ -0,0 +1,18 @@
+export interface ISubstation {
+  id: number
+  active: boolean
+  name: string
+  fullNameSubstation: string
+  slug: string
+  rdu: boolean
+  districtId: number
+  voltageClassesId: number
+  typeKpId: number
+  headControllerId: number
+  mainChannelId: number
+  backupChannelId: number | null
+  additionalChannelId: number | null
+  gsmId: number | null
+  mainChannelIp: string
+  backupChannelIp: string | null
+}
