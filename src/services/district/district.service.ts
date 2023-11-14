aa6b9232274a89b0c9b706cd9e4b6b0e7867a332
BolotnikovMS <bolotnikovms@yandex.ru>
2023-11-14 17:09:04 +0500

    feat: :sparkles: Добавлен метод для добавления и получения ПС

--- src/services/district/district.service.ts
+++ src/services/district/district.service.ts
@@ -1,4 +1,5 @@
 import { IDistrict } from './district.interface'
+import { TDistrictData } from './district.type'
 import axios from 'axios'
 import { url } from '../../constants'
 
@@ -8,4 +9,16 @@ export const DistrictService = {
 
     return response.data
   },
+
+  async create(data: TDistrictData) {
+    return axios.post<TDistrictData>(`${url}/districts`, data, {
+      headers: {'Content-Type': 'application/json'}
+    })
+  },
+
+  async getSubstationsRelatedDistrict(id: string) {
+    const response = await axios.get(`${url}/districts/${id}/substations`)
+
+    return response.data
+  }
 }
