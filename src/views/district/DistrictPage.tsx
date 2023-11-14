2b2f0306364d29ce7de08518c608195413354769
BolotnikovMS <bolotnikovms@yandex.ru>
2023-11-14 17:10:25 +0500

    refactor: Вынесена форма в отдельный компонент и интерфес

--- src/views/district/DistrictPage.tsx
+++ src/views/district/DistrictPage.tsx
@@ -1,27 +1,13 @@
 import './district.scss'
 
-import { Button, CustomInput, Error, FormGroup, InfoMessage, Loader, SmallCard } from '../../components'
-import { SubmitHandler, useForm } from 'react-hook-form'
+import { Error, InfoMessage, Loader, SmallCard } from '../../components'
 
 import { AxiosError } from 'axios'
-import { useDistrict } from '../../hooks'
-
-interface IDistrictFields {
-  name: string
-  shortName: string
-  active?: boolean
-}
+import { DistrictForm } from './components/form/DistrictForm'
+import { useDistricts } from '../../hooks'
 
 export const DistrictPage = () => {
-  const { districts, error, isError, isLoading } = useDistrict()
-  const { register, handleSubmit, formState: { errors, isValid }, reset } = useForm<IDistrictFields>({
-    mode: 'onBlur'
-  })
-
-  const submit: SubmitHandler<IDistrictFields> = (data) => {
-    console.log(data);
-    reset()
-  }
+  const { districts, error, isError, isLoading } = useDistricts()
   
   return (
     <div className="districts">
@@ -30,49 +16,7 @@ export const DistrictPage = () => {
           <h2 className="title">Районы и ГП</h2>
         </div>
         <div className="districts__form">
-          <form className="form" onSubmit={handleSubmit(submit)}>
-            <div className="form__content">
-              <FormGroup>
-                <CustomInput
-                  label='Название Района или ГП'
-                  name='name'
-                  register={register}
-                  error={errors.name?.message}
-                  validation={{
-                    required: {value: true, message: 'Поле является обязательным!'},
-                    minLength: {value: 3, message: 'Минимальная длина поля 3 символа!'},
-                    maxLength: {value: 200, message: 'Максимальная длина поля 200 символов!'}
-                  }}
-                  placeholder='Введите название...'
-                />
-              </FormGroup>
-              <FormGroup>
-                <CustomInput
-                  label='Сокращенное название'
-                  name='shortName'
-                  register={register}
-                  error={errors.shortName?.message}
-                  validation={{
-                    required: {value: true, message: 'Поле является обязательным!'},
-                    minLength: {value: 3, message: 'Минимальная длина поля 3 символа!'},
-                    maxLength: {value: 200, message: 'Максимальная длина поля 200 символов!'}
-                  }}
-                  placeholder='Введите сокращенное название...'
-                />
-              </FormGroup>
-              <FormGroup className='form__group-row'>
-                <CustomInput
-                  label='Используется?'
-                  name='active'
-                  register={register}
-                  type='checkbox'
-                />
-              </FormGroup>
-            </div>
-            <div className="form__btns">
-              <Button disabled={isLoading || isError || !isValid} classBtn='btn-bg_green'>Добавить</Button>
-            </div>
-          </form>
+          <DistrictForm />
         </div>
         <div className="districts__control">Control</div>
           {isLoading && <Loader />}
@@ -80,11 +24,11 @@ export const DistrictPage = () => {
           {!!districts?.length && (
             <div className="districts__cards">
               {
-                districts.map(district => <SmallCard key={district.id} cardText={district.name} />)
+                districts.map(district => <SmallCard key={district.id} cardText={district.name} path={`/districts/${district.id}/substations`} />)
               }
             </div>
           )}
-          {!districts?.length && !isLoading && <InfoMessage text='Районов или ГП пока не добавлено...' />}
+          {(!districts?.length && !isLoading) || isError && <InfoMessage text='Районов или ГП пока не добавлено...' />}
       </div>
     </div>
   )
