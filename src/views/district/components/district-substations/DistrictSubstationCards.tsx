a3a0344598478a3412a5c50d95864ad2b85c6d33
BolotnikovMS <bolotnikovms@yandex.ru>
2023-11-19 20:32:09 +0500

    feat: :sparkles: Компонент с выводом ПС района

---
+++ src/views/district/components/district-substations/DistrictSubstationCards.tsx
@@ -0,0 +1,26 @@
+import { Error, InfoMessage, Loader, SmallCard } from '../../../../components'
+
+import React from 'react'
+import { isAxiosError } from 'axios'
+import { useDistrictSubstations } from '../../../../hooks'
+import { useParams } from 'react-router-dom'
+
+export const DistrictSubstationCards: React.FC = () => {
+  const { id } = useParams()
+  const { substations, error, isError, isLoading } = useDistrictSubstations(id)
+  
+  return (
+    <>
+      {isLoading && <Loader />}
+      {(isError && isAxiosError(error)) && <Error error={error}/>}
+      {!!substations?.length && (
+        <div className="districts__cards">
+          {
+            substations.map(substation => <SmallCard key={substation.id} cardText={substation.name} path={'#'} />)
+          }
+        </div>
+      )}
+      {(!substations?.length && !isLoading && !isError) && <InfoMessage text='Районов или ГП пока не добавлено...' />}
+    </>
+  )
+}
