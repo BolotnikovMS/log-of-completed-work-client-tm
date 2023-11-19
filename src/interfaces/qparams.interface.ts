09b90d84809bd0843bec7dad7d9524039c2c3f09
BolotnikovMS <bolotnikovms@yandex.ru>
2023-11-19 20:26:50 +0500

    feat: :sparkles: Интерфейс параметров

---
+++ src/interfaces/qparams.interface.ts
@@ -0,0 +1,11 @@
+import { TOrderSort } from '../types/order.types'
+
+export interface IQueryParams {
+  page?: number
+  size?: number
+  search?: string
+  sort?: string
+  order?: TOrderSort
+  offset?: number
+  limit?: number
+}
