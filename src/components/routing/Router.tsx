2b02e1012de855fdb414d42e8a9291dbcfde3cf7
BolotnikovMS <bolotnikovms@yandex.ru>
2023-11-19 20:23:10 +0500

    feat: :sparkles: Пересмотрены компоненты

--- src/components/routing/Router.tsx
+++ src/components/routing/Router.tsx
@@ -1,18 +1,55 @@
+import { DistrictControl, DistrictForm, DistrictSubstationCards, DistrictsCards } from '../../views/district/components'
 import { Route, Routes } from 'react-router-dom'
 
-import { DistrictPage } from '../../views/district/DistrictPage'
+import { DistrictsPage } from '../../views/district/DistrictsPage'
 import { HomePage } from '../../views/home/HomePage'
-import { Info } from '../../views/district/components/info/Info'
-import { Layout } from '../layout/Layout'
+import { Layout } from '..'
 import React from 'react'
+import { SubstationsCards } from '../../views/substations/components/substations-cards/SubstationsCards'
+import { SubstationsPage } from '../../views/substations/SubstationsPage'
 
 export const Router: React.FC = () => {
   return (
     <Routes>
       <Route path='/' element={<Layout />}>
         <Route index element={<HomePage />} />
-        <Route path='/districts' element={<DistrictPage />} />
-        <Route path='/districts/:id/substations' element={<Info />} />
+        <Route path='/districts' 
+          element={
+            <DistrictsPage 
+              title='Районы и ГП'
+              children={
+                <>
+                  <DistrictForm />
+                  <DistrictControl />
+                  <DistrictsCards />
+                </>
+              } 
+          />}
+        />
+        <Route path='/districts/:id/substations' 
+          element={
+            <DistrictsPage 
+              title='Подстанций'
+              children={
+                <>
+                  <DistrictSubstationCards />
+                </>
+              }
+            />        
+          }
+        />
+        <Route path='/substations'
+          element={
+            <SubstationsPage
+              title='Подстанций'
+              children={
+                <>
+                  <SubstationsCards />
+                </>
+              }
+            />
+          }
+        />
         <Route path='*' element={<div>Not Found</div>} />
       </Route>
     </Routes>
