cea8692ea639d89aed610d8fc26ccf1fc67dd50e
BolotnikovMS <bolotnikovms@yandex.ru>
2023-11-19 20:27:17 +0500

    feat: :sparkles: Общий файл експорта

---
+++ src/interfaces/index.ts
@@ -0,0 +1,6 @@
+import { IDistrict } from './district.interface'
+import { IMeta } from './meta.interface'
+import { IQueryParams } from './qparams.interface'
+import { ISubstation } from './substation.interface'
+
+export type { IDistrict, ISubstation, IQueryParams, IMeta }
