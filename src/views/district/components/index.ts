08c069c2333c54cf3f7380228f0ffa7601132c54
BolotnikovMS <bolotnikovms@yandex.ru>
2023-11-19 20:31:44 +0500

    feat: Файл экспорта

---
+++ src/views/district/components/index.ts
@@ -0,0 +1,6 @@
+import { DistrictControl } from './district-control/DistrictControl'
+import { DistrictForm } from './form/DistrictForm'
+import { DistrictSubstationCards } from './district-substations/DistrictSubstationCards'
+import { DistrictsCards } from './districts-cards/DistrictsCards'
+
+export { DistrictForm, DistrictControl, DistrictsCards, DistrictSubstationCards }
