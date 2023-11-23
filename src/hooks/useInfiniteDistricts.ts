1d19d8db5b66638e9d6f9887f4d92a128885e616
BolotnikovMS <bolotnikovms@yandex.ru>
2023-11-23 21:17:00 +0500

    refactor: Рефакторинг
    Логика подругзки данных вынесена в отдельный хук

---
+++ src/hooks/useInfiniteDistricts.ts
@@ -0,0 +1,26 @@
+import { DistrictService } from '../services/district/district.service'
+import { useInfiniteQuery } from '@tanstack/react-query'
+
+export const useInfiniteDistricts = ({ limit }: { limit: number }) => {
+  const { 
+    data,
+    error,
+    isError,
+    fetchNextPage,
+    hasNextPage,
+    isFetching,
+    isFetchingNextPage,
+    } = useInfiniteQuery({
+    queryKey: ['districts'],
+    queryFn: ({ pageParam = 1 }) => DistrictService.getDistricts({page: pageParam, limit}),
+    initialPageParam: 1,
+    getNextPageParam: (lastPage, allPages) => {
+      const nextPage = allPages.length + 1
+      const totalPages = Math.ceil(allPages[0].meta.total / limit)
+
+      return totalPages >= nextPage && lastPage.data.length !== 0 ? nextPage : undefined
+    }
+  })
+
+  return { data, error, isError, fetchNextPage, hasNextPage, isFetching, isFetchingNextPage }
+}
--- src/views/district/components/districts-cards/DistrictsCards.tsx
+++ src/views/district/components/districts-cards/DistrictsCards.tsx
@@ -1,32 +1,11 @@
-import { Button, Error, InfoMessage, Loader, SmallCard } from '../../../../components'
+import { Badge, Button, Error, InfoMessage, Loader, SmallCard } from '../../../../components'
 
-import { DistrictService } from '../../../../services/district/district.service'
 import React from 'react'
 import { isAxiosError } from 'axios'
-import { useInfiniteQuery } from '@tanstack/react-query'
+import { useInfiniteDistricts } from '../../../../hooks'
 
 export const DistrictsCards: React.FC = () => {
-  // const { districts, error, isError, isLoading } = useDistricts({ limit: 5 })
-  const limit = 5
-  const { 
-    data,
-    error,
-    isError,
-    fetchNextPage,
-    hasNextPage,
-    isFetching,
-    isFetchingNextPage,
-    } = useInfiniteQuery({
-    queryKey: ['districts'],
-    queryFn: ({ pageParam = 1 }) => DistrictService.getDistricts({page: pageParam, limit}),
-    initialPageParam: 1,
-    getNextPageParam: (lastPage, allPages) => {
-      const nextPage = allPages.length + 1
-      const totalPages = Math.round(allPages[0].meta.total / limit)
-
-      return totalPages >= nextPage && lastPage.data.length !== 0 ? nextPage : undefined
-    }
-  })
+  const { data, error, fetchNextPage, hasNextPage, isError, isFetching, isFetchingNextPage } = useInfiniteDistricts({ limit: 10 })
 
   return (
     <>
@@ -34,23 +13,38 @@ export const DistrictsCards: React.FC = () => {
       {!!data?.pages.length && (
         <div className="districts__cards">
           {data.pages.map(districts => (
-            districts.data.map(district => <SmallCard key={district.id} cardText={district.name} path={`/districts/${district.id}/substations`} />)
+            districts.data.map(district => (
+              <SmallCard
+                key={district.id}
+                cardText={district.name}
+                className={district.active ? '' : 'inActive'}
+                path={`/districts/${district.id}/substations`}
+              >
+                {
+                  !district.active && <Badge text='Не исп.' className={district.active ? '' : 'badge-color_red'} />
+                }
+              </SmallCard>
+            ))
           ))}
         </div>
       )}
       {isFetching && <Loader />}
       {(!data?.pages?.length && !isFetching && !isError) && <InfoMessage text='Районов или ГП пока не добавлено...' />}
-      <div className="districts__btns">
-        <Button classBtn='btn-bg_blue' type='button' onClick={() => fetchNextPage()} disabled={!hasNextPage || isFetchingNextPage}>
-          {
-            isFetchingNextPage || isFetching ? 
-            'Загрузка...' : 
-            hasNextPage ?
-            'Показать еще' :
-            'Больше нет данных'
-          }
-        </Button>
-      </div>
+      {
+        hasNextPage && (
+          <div className="districts__btns">
+            <Button classBtn='btn-bg_blue' type='button' onClick={() => fetchNextPage()} disabled={!hasNextPage || isFetchingNextPage}>
+              {
+                isFetchingNextPage || isFetching ? 
+                'Загрузка...' : 
+                hasNextPage ?
+                'Показать еще' :
+                'Больше нет данных'
+              }
+            </Button>
+          </div>
+        )
+      }
     </>
   )
 }
