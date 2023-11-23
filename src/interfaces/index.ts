2a9cb9f9092ee4e97b50b27cf2fa058714651b24
BolotnikovMS <bolotnikovms@yandex.ru>
2023-11-23 21:17:25 +0500

    feat: Добавлен экспорт IVoltageClass

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -2,5 +2,6 @@ import { IDistrict } from './district.interface'
 import { IMeta } from './meta.interface'
 import { IQueryParams } from './qparams.interface'
 import { ISubstation } from './substation.interface'
+import { IVoltageClass } from './voltage-class.interface'
 
-export type { IDistrict, ISubstation, IQueryParams, IMeta }
+export type { IDistrict, ISubstation, IQueryParams, IMeta, IVoltageClass }
