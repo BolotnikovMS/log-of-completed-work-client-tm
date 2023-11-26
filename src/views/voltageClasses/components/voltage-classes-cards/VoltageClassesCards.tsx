66cbdbb3b44c38ac1ea5c7b9fa0dc79fa68867a0
BolotnikovMS <bolotnikovms@yandex.ru>
2023-11-26 17:47:46 +0500

    feat: :sparkles: Добавлена мутация для удаления

--- src/views/voltageClasses/components/voltage-classes-cards/VoltageClassesCards.tsx
+++ src/views/voltageClasses/components/voltage-classes-cards/VoltageClassesCards.tsx
@@ -1,11 +1,23 @@
 import { Button, Error, Loader, SmallCard } from '../../../../components'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
 
 import React from 'react'
+import { VoltageClassService } from '../../../../services/voltage-class/voltage-class.service'
 import { isAxiosError } from 'axios'
 import { useInfiniteVoltageClasses } from '../../../../hooks'
 
 export const VoltageClassesCards: React.FC = () => {
   const { data, error, fetchNextPage, hasNextPage, isError, isFetching, isFetchingNextPage } = useInfiniteVoltageClasses({ limit: 10 })
+  const queryClient = useQueryClient()
+  const deleteVoltageClass = useMutation({
+    mutationFn: (id: number) => VoltageClassService.deleteVoltageClass(id),
+    onSuccess: async () => {
+      await queryClient.cancelQueries({queryKey: ['voltageClasses']})
+    },
+    onSettled: async () => {
+      await queryClient.invalidateQueries({queryKey: ['voltageClasses']})
+    }
+  })
   
   return (
     <>
@@ -17,27 +29,30 @@ export const VoltageClassesCards: React.FC = () => {
               <SmallCard
                 key={voltageClass.id}
                 cardText={voltageClass.name}
+                childrenControl={
+                  <>
+                    <Button>Edit</Button>              
+                    <Button classBtn='btn-bg_red' onClick={() => deleteVoltageClass.mutate(voltageClass.id)}>Delete</Button>              
+                  </>
+                }
               />
             ))
           ))}
         </div>
       )}
       {isFetching && <Loader />}
-      {
-        hasNextPage && (
-          <div className="voltage__btns">
-            <Button classBtn='btn-bg_blue' type='button' onClick={() => fetchNextPage()} disabled={!hasNextPage || isFetchingNextPage}>
-              {
-                isFetchingNextPage || isFetching ? 
-                'Загрузка...' : 
-                hasNextPage ?
-                'Показать еще' :
-                'Больше нет данных'
-              }
-            </Button>
-          </div>
-        )
-      }
+      {hasNextPage && (
+        <div className="voltage__btns">
+          <Button classBtn='btn-bg_blue' type='button' onClick={() => fetchNextPage()} disabled={!hasNextPage || isFetchingNextPage}>
+            {isFetchingNextPage || isFetching ? 
+              'Загрузка...' : 
+              hasNextPage ?
+              'Показать еще' :
+              'Больше нет данных'
+            }
+          </Button>
+        </div>
+      )}
     </>
   )
 }
