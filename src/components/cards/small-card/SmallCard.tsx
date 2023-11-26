5ea976241eb8d1089dc65acd4026f95ef2f70bbe
BolotnikovMS <bolotnikovms@yandex.ru>
2023-11-26 17:32:11 +0500

    fix: Добавлена проверка, потомки

--- src/components/cards/small-card/SmallCard.tsx
+++ src/components/cards/small-card/SmallCard.tsx
@@ -5,15 +5,17 @@ import { Link } from 'react-router-dom'
 import React from 'react'
 import cx from 'classnames'
 
-export const SmallCard: React.FC<IPropsSmallCard> = ({ children, className, cardText, path }) => {
+export const SmallCard: React.FC<IPropsSmallCard> = ({ childrenContent, childrenControl, className, cardText, path }) => {
   return (
     <div className={cx('card', className)}>
-      <Link to={path || '!#'} className='card__link'/>
+      {path && <Link to={path} className='card__link'/>}
       <div className="card__content">
-        {children}
+        {childrenContent}
         <p className='card__text'>{cardText}</p>
       </div>
-      <div className="card__control">Edit / Delete</div>
+      <div className="card__control card__control-row">
+        {childrenControl}
+      </div>
     </div>
   )
 }
