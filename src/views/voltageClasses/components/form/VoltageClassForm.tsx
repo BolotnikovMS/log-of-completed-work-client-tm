717b95cd68aef434059c1fc46d2832ea5b89fce5
BolotnikovMS <bolotnikovms@yandex.ru>
2023-11-26 17:37:15 +0500

    feat: :sparkles: Компонент формы добавления записи

---
+++ src/views/voltageClasses/components/form/VoltageClassForm.tsx
@@ -0,0 +1,63 @@
+import { Button, CustomInput, Error, FormGroup } from '../../../../components'
+import { SubmitHandler, useForm } from 'react-hook-form'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+
+import { IVoltageClassFields } from './voltageClassForm.interface'
+import React from 'react'
+import { TVoltageClass } from '../../../../services/voltage-class/voltage-class.type'
+import { VoltageClassService } from '../../../../services/voltage-class/voltage-class.service'
+import { isAxiosError } from 'axios'
+
+export const VoltageClassForm: React.FC = () => {
+  const { register, handleSubmit, formState: { errors, isValid }, reset } = useForm<IVoltageClassFields>({
+    mode: 'onBlur'
+  })
+  const queryClient = useQueryClient()
+  const { mutateAsync, isError: isErrorMutate, error: errorMutate } = useMutation({
+    mutationFn: (data: TVoltageClass) => VoltageClassService.create(data),
+    onSuccess: async () => {
+      await queryClient.cancelQueries({queryKey: ['voltageClasses']})
+    },
+    onSettled: async () => {
+      await queryClient.invalidateQueries({queryKey: ['voltageClasses']})
+    }
+  })
+
+  const submit: SubmitHandler<IVoltageClassFields> = (data) => {
+    mutateAsync(data)
+    reset()
+  }
+  
+  return (
+    <>
+      <div className="voltage__form">
+        {(isErrorMutate && isAxiosError(errorMutate)) && <Error error={errorMutate} />}
+        <form className="form" onSubmit={handleSubmit(submit)}>
+          <div className="form__content">
+            <FormGroup>
+              <CustomInput
+                label='Класс напряжения'
+                name='name'
+                register={register}
+                error={errors.name?.message}
+                validation={{
+                  required: {value: true, message: 'Поле является обязательным!'},
+                  minLength: {value: 3, message: 'Минимальная длина поля 3 символа!'},
+                  maxLength: {value: 150, message: 'Максимальная длина поля 150 символов!'},
+                  pattern: {
+                    value: /^[0-9+/]+$/,
+                    message: 'Формат данных: ххх/хх/хх!'
+                  }
+                }}
+                placeholder='Введите класс напряжения...'
+              />
+            </FormGroup>
+          </div>
+          <div className="form__btns">
+            <Button disabled={!isValid} classBtn='btn-bg_green'>Добавить</Button>
+          </div>
+        </form>
+      </div>
+    </>
+  )
+}
