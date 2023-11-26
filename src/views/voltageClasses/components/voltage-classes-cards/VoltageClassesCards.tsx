73a9bcba401399971e2e03eb9651e1bdb8ef1371
BolotnikovMS <bolotnikovms@yandex.ru>
2023-11-26 17:37:45 +0500

    feat: :sparkles: Компонент карточек классов U

--- src/views/voltageClasses/components/voltage-classes-cards/VoltageClassesCards.tsx
+++ src/views/voltageClasses/components/voltage-classes-cards/VoltageClassesCards.tsx
@@ -1,7 +1,43 @@
+import { Button, Error, Loader, SmallCard } from '../../../../components'
+
 import React from 'react'
+import { isAxiosError } from 'axios'
+import { useInfiniteVoltageClasses } from '../../../../hooks'
 
 export const VoltageClassesCards: React.FC = () => {
+  const { data, error, fetchNextPage, hasNextPage, isError, isFetching, isFetchingNextPage } = useInfiniteVoltageClasses({ limit: 10 })
+  
   return (
-    <div>VoltageClassesCards</div>
+    <>
+      {(isError && isAxiosError(error)) && <Error error={error}/>}
+      {!!data?.pages.length && (
+        <div className="voltage__cards">
+          {data.pages.map(voltageClasses => (
+            voltageClasses.data.map(voltageClass => (
+              <SmallCard
+                key={voltageClass.id}
+                cardText={voltageClass.name}
+              />
+            ))
+          ))}
+        </div>
+      )}
+      {isFetching && <Loader />}
+      {
+        hasNextPage && (
+          <div className="voltage__btns">
+            <Button classBtn='btn-bg_blue' type='button' onClick={() => fetchNextPage()} disabled={!hasNextPage || isFetchingNextPage}>
+              {
+                isFetchingNextPage || isFetching ? 
+                'Загрузка...' : 
+                hasNextPage ?
+                'Показать еще' :
+                'Больше нет данных'
+              }
+            </Button>
+          </div>
+        )
+      }
+    </>
   )
 }
