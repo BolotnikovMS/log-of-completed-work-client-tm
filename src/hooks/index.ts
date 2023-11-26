fb16e3566e4f0f89c024a8f1d05676085b677ed7
BolotnikovMS <bolotnikovms@yandex.ru>
2023-11-26 19:49:11 +0500

    feat: Добавлен экспорт

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,6 +1,7 @@
 import { useDistrictSubstations } from './useDistrictSubstations'
 import { useDistricts } from './useDistricts'
 import { useInfiniteDistricts } from './useInfiniteDistricts'
+import { useInfiniteTypesKp } from './useInfiniteTypesKp'
 import { useInfiniteVoltageClasses } from './useInfiniteVoltageClasses'
 
-export { useDistricts, useDistrictSubstations, useInfiniteDistricts, useInfiniteVoltageClasses }
+export { useDistricts, useDistrictSubstations, useInfiniteDistricts, useInfiniteVoltageClasses, useInfiniteTypesKp }
