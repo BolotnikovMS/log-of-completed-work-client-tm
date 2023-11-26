cc11dbbea6f8981fdd31d8ef457123e29679231f
BolotnikovMS <bolotnikovms@yandex.ru>
2023-11-26 17:34:34 +0500

    feat: :zap: Добавлена мутация для удаления

--- src/views/district/components/districts-cards/DistrictsCards.tsx
+++ src/views/district/components/districts-cards/DistrictsCards.tsx
@@ -1,11 +1,23 @@
 import { Button, Error, InfoMessage, Loader, SmallCard } from '../../../../components'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
 
+import { DistrictService } from '../../../../services/district/district.service'
 import React from 'react'
 import { isAxiosError } from 'axios'
 import { useInfiniteDistricts } from '../../../../hooks'
 
 export const DistrictsCards: React.FC = () => {
   const { data, error, fetchNextPage, hasNextPage, isError, isFetching, isFetchingNextPage } = useInfiniteDistricts({ limit: 10 })
+  const queryClient = useQueryClient()
+  const deleteDistrict = useMutation({
+    mutationFn: (id: number) => DistrictService.deleteDistrict(id),
+    onSuccess: async () => {
+      await queryClient.cancelQueries({queryKey: ['districts']})
+    },
+    onSettled: async () => {
+      await queryClient.invalidateQueries({queryKey: ['districts']})
+    }
+  })
 
   return (
     <>
@@ -18,6 +30,12 @@ export const DistrictsCards: React.FC = () => {
                 key={district.id}
                 cardText={district.name}
                 path={`/districts/${district.id}/substations`}
+                childrenControl={
+                  <>
+                    <Button>Edit</Button>              
+                    <Button classBtn='btn-bg_red' onClick={() => deleteDistrict.mutate(district.id)}>Delete</Button>              
+                  </>
+                }
               />
             ))
           ))}
@@ -25,21 +43,18 @@ export const DistrictsCards: React.FC = () => {
       )}
       {isFetching && <Loader />}
       {(!data?.pages?.length && !isFetching && !isError) && <InfoMessage text='Районов или ГП пока не добавлено...' />}
-      {
-        hasNextPage && (
-          <div className="districts__btns">
-            <Button classBtn='btn-bg_blue' type='button' onClick={() => fetchNextPage()} disabled={!hasNextPage || isFetchingNextPage}>
-              {
-                isFetchingNextPage || isFetching ? 
-                'Загрузка...' : 
-                hasNextPage ?
-                'Показать еще' :
-                'Больше нет данных'
-              }
-            </Button>
-          </div>
-        )
-      }
+      {hasNextPage && (
+        <div className="districts__btns">
+          <Button classBtn='btn-bg_blue' type='button' onClick={() => fetchNextPage()} disabled={!hasNextPage || isFetchingNextPage}>
+            {isFetchingNextPage || isFetching ? 
+              'Загрузка...' : 
+              hasNextPage ?
+              'Показать еще' :
+              'Больше нет данных'
+            }
+          </Button>
+        </div>
+      )}
     </>
   )
 }
