ecf2a29edcdfab67fc8b10ee8c10ff06621430b6
BolotnikovMS <bolotnikovms@yandex.ru>
2023-11-28 21:30:38 +0500

    refactor: :recycle: Удаление вынесено в хук

--- src/views/typesKp/components/types-kp-cards/TypesKpCards.tsx
+++ src/views/typesKp/components/types-kp-cards/TypesKpCards.tsx
@@ -1,24 +1,20 @@
 import { Button, Error, Loader, SmallCard } from '../../../../components'
 import { Pencil, Trash2 } from 'lucide-react'
-import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { useDeleteTypeKp, useInfiniteTypesKp } from '../../../../hooks'
 
 import React from 'react'
-import { TypeKpService } from '../../../../services/types-kp/type-kp.service'
 import { isAxiosError } from 'axios'
-import { useInfiniteTypesKp } from '../../../../hooks'
 
 export const TypesKpCards: React.FC = () => {
   const { data, error, fetchNextPage, hasNextPage, isError, isFetching, isFetchingNextPage } = useInfiniteTypesKp({ limit: 10 })
-  const queryClient = useQueryClient()
-  const deleteTypeKp = useMutation({
-    mutationFn: (id: number) => TypeKpService.deleteTypeKp(id),
-    onSuccess: async () => {
-      await queryClient.cancelQueries({queryKey: ['typesKp']})
-    },
-    onSettled: async () => {
-      await queryClient.invalidateQueries({queryKey: ['typesKp']})
-    }
-  })
+  const { deleteTypeKp } = useDeleteTypeKp()
+  const handleDelete = (id: number) => {
+    const answer = confirm('Are you sure?')
+
+    if (!answer) return null
+
+    return deleteTypeKp.mutate(id)
+  }
 
   return (
     <>
@@ -35,7 +31,7 @@ export const TypesKpCards: React.FC = () => {
                     <Button>
                       <Pencil />
                     </Button>              
-                    <Button classBtn='btn-bg_red' onClick={() => deleteTypeKp.mutate(typeKp.id)}>
+                    <Button classBtn='btn-bg_red' onClick={() => handleDelete(typeKp.id)}>
                       <Trash2 />
                     </Button>              
                   </>
@@ -47,7 +43,7 @@ export const TypesKpCards: React.FC = () => {
       )}
       {isFetching && <Loader />}
       {hasNextPage && (
-        <div className="voltage__btns">
+        <div className="typeKp__btns">
           <Button classBtn='btn-bg_blue' type='button' onClick={() => fetchNextPage()} disabled={!hasNextPage || isFetchingNextPage}>
             {isFetchingNextPage || isFetching ? 
               'Загрузка...' : 
