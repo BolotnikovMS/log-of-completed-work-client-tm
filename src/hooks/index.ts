4c3e0b895e05c2c2dfd8d6661db27fe3b1db689b
BolotnikovMS <bolotnikovms@yandex.ru>
2023-12-03 17:26:40 +0500

    feat: Добавлены экспорты

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,5 +1,7 @@
+import { useDeleteDistrict } from './distrcts/useDeleteDistrict'
 import { useDeleteHeadController } from './head-controllers/useDeleteHeadController'
 import { useDeleteTypeKp } from './types-kp/useDeleteTypeKp'
+import { useDeleteVoltageClass } from './voltage-classes/useDeleteVoltageClass'
 import { useDistrictSubstations } from './distrcts/useDistrictSubstations'
 import { useDistricts } from './distrcts/useDistricts'
 import { useInfiniteDistricts } from './distrcts/useInfiniteDistricts'
@@ -8,4 +10,4 @@ import { useInfiniteTypesKp } from './types-kp/useInfiniteTypesKp'
 import { useInfiniteVoltageClasses } from './voltage-classes/useInfiniteVoltageClasses'
 import { useTypesKp } from './types-kp/useTypesKp'
 
-export { useDistricts, useDistrictSubstations, useInfiniteDistricts, useInfiniteVoltageClasses, useInfiniteTypesKp, useTypesKp, useDeleteTypeKp, useInfiniteHeadControllers, useDeleteHeadController }
+export { useDistricts, useDistrictSubstations, useInfiniteDistricts, useInfiniteVoltageClasses, useInfiniteTypesKp, useTypesKp, useDeleteTypeKp, useInfiniteHeadControllers, useDeleteHeadController, useDeleteDistrict, useDeleteVoltageClass }
