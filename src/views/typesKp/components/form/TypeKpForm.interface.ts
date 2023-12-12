5348875a3ea6ab158389ea9643419075e7b232f1
BolotnikovMS <bolotnikovms@yandex.ru>
2023-12-12 21:38:22 +0500

    feat: :sparkles: Интерфейс с описанием пропсов формы

--- src/views/typesKp/components/form/TypeKpForm.interface.ts
+++ src/views/typesKp/components/form/TypeKpForm.interface.ts
@@ -1,3 +1,12 @@
+import { ITypeKp } from '../../../../interfaces'
+
 export interface ITypeKpFields {
   name: string
+}
+
+export interface IPropsTypeKpForm {
+  typeKp?: ITypeKp | null
+  isEdited?: boolean | null
+  setIsModal: (val: boolean) => void
+  setIsEdited?: (val: boolean) => void
 }
--- src/views/voltageClasses/components/form/voltageClassForm.interface.ts
+++ src/views/voltageClasses/components/form/voltageClassForm.interface.ts
@@ -1,3 +1,12 @@
+import { IVoltageClass } from '../../../../interfaces'
+
 export interface IVoltageClassFields {
   name: string
+}
+
+export interface IPropsVoltageClassForm {
+  voltageClass?: IVoltageClass | null
+  isEdited?: boolean | null
+  setIsModal: (val: boolean) => void
+  setIsEdited?: (val: boolean) => void
 }
