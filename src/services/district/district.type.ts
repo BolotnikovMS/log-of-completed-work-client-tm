65f06c9e3822f6d4aadcb3ca5c817a3b9ac9e061
BolotnikovMS <bolotnikovms@yandex.ru>
2023-12-17 18:51:36 +0500

    style: Фикс

--- src/services/district/district.type.ts
+++ src/services/district/district.type.ts
@@ -2,4 +2,4 @@ import { IDistrict, IMeta } from '../../interfaces'
 
 export type TDistrictData = Omit<IDistrict, 'id' | 'slug' | 'userId'>
 
-export type TRespDistricts = { meta: IMeta, data: IDistrict[]}
+export type TRespDistricts = { meta: IMeta, data: IDistrict[] }
