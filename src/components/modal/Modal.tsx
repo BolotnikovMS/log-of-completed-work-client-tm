9c1d8a8984bbb80ba39ca59b93fd2c29f72b9bdc
BolotnikovMS <bolotnikovms@yandex.ru>
2023-12-17 17:19:03 +0500

    refactor: Убрано закрытие по нажатию клавиши

--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -1,32 +1,18 @@
 import './modal.scss'
 
-import React, { useEffect } from 'react'
-
 import { IPropsModal } from './modal.interface'
+import React from 'react'
+import { createPortal } from 'react-dom'
 
-export const Modal: React.FC<IPropsModal> = ({ visible, title, content, footer, onClose }) => {
-  const onKeydown = ({ key }: KeyboardEvent) => {
-    switch (key) {
-      case 'Escape':
-        onClose()
-        break
-    }
-  }
-
-  useEffect(() => {
-    document.addEventListener('keydown', onKeydown)
-
-    return () => document.removeEventListener('keydown', onKeydown)
-  })
-
+export const Modal: React.FC<IPropsModal> = ({ visible, title, content, footer, onToggle }) => {
   if (!visible) return null
 
-  return (
-    <div className='modal' onClick={onClose}>
+  return createPortal(
+    <div className='modal' onClick={onToggle}>
       <div className="modal__dialog" onClick={e => e.stopPropagation()}>
         <div className="modal__header">
           <h3 className="modal__title">{title}</h3>
-          <span className="modal__close" onClick={onClose}>&times;</span>
+          <span className="modal__close" onClick={onToggle}>&times;</span>
         </div>
         <div className="modal__body">
           <div className="modal__content">{content}</div>
@@ -35,6 +21,7 @@ export const Modal: React.FC<IPropsModal> = ({ visible, title, content, footer, 
           <div className="modal__footer">{footer}</div>
         )}
       </div>
-    </div>
+    </div>,
+    document.body
   )
 }
