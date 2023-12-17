911d894fc0b4fd7325825ad3650c801be9a263a9
BolotnikovMS <bolotnikovms@yandex.ru>
2023-12-17 17:20:04 +0500

    feat: Добавлен экспорт интерфейса IHeadController

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,8 +1,9 @@
 import { IDistrict } from './district.interface'
+import { IHeadController } from './head-controller.interface'
 import { IMeta } from './meta.interface'
 import { IQueryParams } from './qparams.interface'
 import { ISubstation } from './substation.interface'
 import { ITypeKp } from './type-kp'
 import { IVoltageClass } from './voltage-class.interface'
 
-export type { IDistrict, ISubstation, IQueryParams, IMeta, IVoltageClass, ITypeKp }
+export type { IDistrict, ISubstation, IQueryParams, IMeta, IVoltageClass, ITypeKp, IHeadController }
