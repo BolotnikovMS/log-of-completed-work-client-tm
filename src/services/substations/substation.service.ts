86f8d3707a2f2a71ee42e4650a34ce5f6a5d6dab
BolotnikovMS <bolotnikovms@yandex.ru>
2023-12-19 21:16:45 +0500

    feat: Добавлен метод для удаления

--- src/services/substations/substation.service.ts
+++ src/services/substations/substation.service.ts
@@ -1,11 +1,19 @@
+import { IQueryParams, ISubstation } from '../../interfaces'
+
 import { TRespSubstations } from './substation.type'
 import axios from 'axios'
 import { url } from '../../constants'
 
 export const SubstationService = {
-  async getSubstations() {
-    const response = await axios<TRespSubstations>(`${url}/substations`)
+  async getSubstations({ limit, page }: IQueryParams) {
+    const response = await axios<TRespSubstations>(`${url}/substations`, {
+      params: { page, limit }
+    })
 
     return response.data
+  },
+
+  async deleteSubstation(id: number) {
+    return axios.delete<ISubstation>(`${url}/substations/${id}`)
   }
 }
