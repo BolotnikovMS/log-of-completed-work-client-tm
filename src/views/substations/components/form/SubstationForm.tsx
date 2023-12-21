6caf60d3a55d3399e8bab082739f99c001c109ce
BolotnikovMS <bolotnikovms@yandex.ru>
2023-12-21 21:32:13 +0500

    feat: :sparkles: Начата работа над формой ПС

--- src/views/substations/components/form/SubstationForm.tsx
+++ src/views/substations/components/form/SubstationForm.tsx
@@ -1,47 +1,81 @@
 import { Button, CustomInput, FormGroup } from '../../../../components'
 import { IPropsSubstationForm, ISubstationFields } from './substationForm.interface'
+import { SubmitHandler, useController, useForm } from 'react-hook-form'
+import { useDistricts, useGsmOperators, useHeadControllers, useTypesKp, useVoltageClasses } from '../../../../hooks'
 
+import AsyncSelect from 'react-select'
 import React from 'react'
-import { useForm } from 'react-hook-form'
 import { useQueryClient } from '@tanstack/react-query'
 
 export const SubstationForm: React.FC<IPropsSubstationForm> = ({ substation, isEdited, setIsEdited, toggleModal }) => {
-  const { register, handleSubmit, formState: { errors, isValid }, reset } = useForm<ISubstationFields>({
+  const { register, handleSubmit, formState: { errors, isValid }, reset, control } = useForm<ISubstationFields>({
     mode: 'onBlur'
   })
-  const queryClient = useQueryClient()
+  const { field: {value: districtValue, onChange: districtOnChange, ...restDistrictField} } = useController({ name: 'district', control, rules: {required: {value: true, message: 'Поле является обязательным!'}}})
+  const { field: {value: voltageClassValue, onChange: voltageClassOnChange, ...restVoltageClass} } = useController({name: 'voltageClass', control, rules: {required: {value: true, message: 'Поле является обязательным!'}}})
+  const { field: {value: typeKpValue, onChange: typeKpOnChange, ...restTypeKp} } = useController({name: 'typeKp', control,rules: {required: {value: true, message: 'Поле является обязательным!'}}})
+  const { field: {value: headControllerValue, onChange: headControllerOnChange, ...restHeadController} } = useController({name: 'headController', control, rules: {required: {value: true, message: 'Поле является обязательным!'}}})
+  const { field: {value: gsmOperatorValue, onChange: gsmOperatorOnChange, ...restGsmOperator} } = useController({name: 'gsm', control, rules: {required: false} })
+  const { districts, isLoading: isLoadingDistricts, isError: isErrorDistricts } = useDistricts({})
+  const { voltageClasses, isError: isErrorVoltageClasses, isLoading: isLoadingVoltageClasses } = useVoltageClasses()
+  const { typesKp, isError: isErrorTypesKp, isLoading: isLoadingTypesKp } = useTypesKp({})
+  const { headControllers, isError: isErrorHeadControllers, isLoading: isLoadingHeadControllers } = useHeadControllers()
+  const { data: gsmOperators, isError: isErrorGsmOperators, isLoading: isLoadingGsmOperators } = useGsmOperators()
+  // const queryClient = useQueryClient()
+  
+  const submit: SubmitHandler<ISubstationFields> = (data) => {
+    console.log(data);
+    reset()
+  }
   
   return (
     <>
       <div className="work-log__form">
-        <form className="form form-col">
+        <form className="form form-col" onSubmit={handleSubmit(submit)}>
           <div className="form__content form__content-col">
             <FormGroup>
-              <CustomInput
-                label='Выберите район'
-                name='district'
-                register={register}
+              <AsyncSelect
+                classNamePrefix='form__custom-select'
+                options={districts?.data}
+                getOptionValue={option => option.id.toString()}
+                getOptionLabel={option => option.name}
+                value={districtValue ? districts?.data.find(d => d.id === districtValue) : null}
+                onChange={option => districtOnChange(option ? option.id : option)}
+                isLoading={isLoadingDistricts}
+                isDisabled={isErrorDistricts}
+                isClearable
+                placeholder="Выберите район..."
+                {...restDistrictField}
               />
             </FormGroup>
             <FormGroup>
-              <CustomInput
-                label='Класс U'
-                name='voltageClass'
-                register={register}
+              <AsyncSelect
+                classNamePrefix='form__custom-select'
+                options={typesKp?.data}
+                getOptionValue={option => option.id.toString()}
+                getOptionLabel={option => option.name}
+                value={typeKpValue ? typesKp?.data.find(d => d.id === typeKpValue) : null}
+                onChange={option => typeKpOnChange(option ? option.id : option)}
+                isLoading={isLoadingTypesKp}
+                isDisabled={isErrorTypesKp}
+                isClearable
+                placeholder="Выберите КП..."
+                {...restTypeKp}
               />
             </FormGroup>
             <FormGroup>
-              <CustomInput
-                label='Тип КП'
-                name='typeKp'
-                register={register}
-              />
-            </FormGroup>
-            <FormGroup>
-              <CustomInput
-                label='Головной контроллер'
-                name='headController'
-                register={register}
+              <AsyncSelect
+                classNamePrefix='form__custom-select'
+                options={headControllers?.data}
+                getOptionValue={option => option.id.toString()}
+                getOptionLabel={option => option.name}
+                value={headControllerValue ? headControllers?.data.find(d => d.id === headControllerValue) : null}
+                onChange={option => headControllerOnChange(option ? option.id : option)}
+                isLoading={isLoadingHeadControllers}
+                isDisabled={isErrorHeadControllers}
+                isClearable
+                placeholder="Выберите головной контроллер..."
+                {...restHeadController}
               />
             </FormGroup>
             <FormGroup>
@@ -66,10 +100,18 @@ export const SubstationForm: React.FC<IPropsSubstationForm> = ({ substation, isE
               />
             </FormGroup>
             <FormGroup>
-              <CustomInput
-                label='GSM оператор'
-                name='gsm'
-                register={register}
+              <AsyncSelect
+                classNamePrefix='form__custom-select'
+                options={gsmOperators}
+                getOptionValue={option => option.id.toString()}
+                getOptionLabel={option => option.name}
+                value={gsmOperatorValue ? gsmOperators?.find(d => d.id === gsmOperatorValue) : null}
+                onChange={option => gsmOperatorOnChange(option ? option.id : option)}
+                isLoading={isLoadingGsmOperators}
+                isDisabled={isErrorGsmOperators}
+                isClearable
+                placeholder="Выберите GSM оператора..."
+                {...restGsmOperator}
               />
             </FormGroup>
             <FormGroup>
@@ -77,6 +119,29 @@ export const SubstationForm: React.FC<IPropsSubstationForm> = ({ substation, isE
                 label='Название ПС'
                 name='name'
                 register={register}
+                error={errors.name?.message}
+                validation={{
+                  required: {value: true, message: 'Поле является обязательным!'},
+                  minLength: {value: 3, message: 'Минимальная длина поля 3 символа!'},
+                  maxLength: {value: 200, message: 'Максимальная длина поля 200 символов!'}
+                }}
+                placeholder='Введите название ПС...'
+                defaultValue={substation?.name}
+              />
+            </FormGroup>
+            <FormGroup>
+              <AsyncSelect
+                classNamePrefix='form__custom-select'
+                options={voltageClasses?.data}
+                getOptionValue={option => option.id.toString()}
+                getOptionLabel={option => option.name}
+                value={voltageClassValue ? voltageClasses?.data.find(d => d.id === voltageClassValue) : null}
+                onChange={option => voltageClassOnChange(option ? option.id : option)}
+                isLoading={isLoadingVoltageClasses}
+                isDisabled={isErrorVoltageClasses}
+                isClearable
+                placeholder="Выберите класс U..."
+                {...restVoltageClass}
               />
             </FormGroup>
             <FormGroup>
@@ -84,6 +149,9 @@ export const SubstationForm: React.FC<IPropsSubstationForm> = ({ substation, isE
                 label='ip основного канала'
                 name='mainChannelIp'
                 register={register}
+                error={errors.mainChannelIp?.message}
+                placeholder='Введите ip основного канала...'
+                defaultValue={substation?.mainChannelIp}
               />
             </FormGroup>
             <FormGroup>
@@ -91,6 +159,9 @@ export const SubstationForm: React.FC<IPropsSubstationForm> = ({ substation, isE
                 label='ip резервного канала'
                 name='backupChannelIp'
                 register={register}
+                error={errors.backupChannelIp?.message}
+                placeholder='Введите ip резервного канала...'
+                defaultValue={substation?.backupChannelIp}
               />
             </FormGroup>
             <FormGroup className='form__group-row'>
