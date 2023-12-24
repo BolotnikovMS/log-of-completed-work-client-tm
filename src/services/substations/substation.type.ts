2d06a81aeac8aa985650b7f27885e151b0becccb
BolotnikovMS <bolotnikovms@yandex.ru>
2023-12-24 20:23:23 +0500

    feat: :sparkles: Добавлен тип

--- src/services/substations/substation.type.ts
+++ src/services/substations/substation.type.ts
@@ -1,3 +1,5 @@
 import { IMeta, ISubstation } from '../../interfaces'
 
+export type TSubstationData = Omit<ISubstation, 'id' | 'slug' | 'userId' | 'fullNameSubstation'>
+
 export type TRespSubstations = { meta: IMeta, data: ISubstation[] }
