475042b89b2e86b936bcc59a82dbc8b3c5f5ed15
BolotnikovMS <bolotnikovms@yandex.ru>
2023-12-28 21:58:22 +0500

    refactor: Добавлен боди и футер

--- src/components/cards/small-card/SmallCard.tsx
+++ src/components/cards/small-card/SmallCard.tsx
@@ -1,17 +1,24 @@
 import './small-card.scss'
 
-import { IPropsSmallCard } from './smallCard.interface'
-import { Link } from 'react-router-dom'
-import React from 'react'
 import cx from 'classnames'
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { IPropsSmallCard } from './smallCard.interface'
 
-export const SmallCard: React.FC<IPropsSmallCard> = ({ childrenContent, childrenControl, className, cardText, path }) => {
+export const SmallCard: React.FC<IPropsSmallCard> = ({ childrenContent, childrenFooter, childrenControl, className, cardText, path }) => {
   return (
     <div className={cx('card', className)}>
       {path && <Link to={path} className='card__link'/>}
       <div className="card__content">
-        {childrenContent}
-        <p className='card__text'>{cardText}</p>
+				<div className="card__body">
+					{childrenContent}
+					<p className='card__text'>{cardText}</p>
+				</div>
+				{childrenFooter && 
+					(<div className="card__footer">
+						<p>{childrenFooter}</p>
+					</div>)
+				}
       </div>
       <div className="card__control card__control-row">
         {childrenControl}
