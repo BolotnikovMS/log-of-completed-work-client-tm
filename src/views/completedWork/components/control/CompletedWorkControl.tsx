30335676e96012f0b8e6bfd1f53b0402bb871f4b
BolotnikovMS <bolotnikovms@yandex.ru>
2024-01-04 18:19:05 +0500

    feat: Добавлен фун-л с модалкой

--- src/views/completedWork/components/control/CompletedWorkControl.tsx
+++ src/views/completedWork/components/control/CompletedWorkControl.tsx
@@ -1,6 +1,23 @@
+import { Plus } from 'lucide-react'
+import { type FC } from 'react'
+import { CompletedWorkForm } from '..'
+import { Button, Modal } from '../../../../components'
+import { useModal } from '../../../../hooks'
 
-export const CompletedWorkControl = () => {
+export const CompletedWorkControl: FC = () => {
+	const { isModal, toggleModal } = useModal()
+
 	return (
-		<div>CompletedWorksControl</div>
+		<div className="work-log__control">
+			<Button classBtn='btn-bg_green' onClick={() => toggleModal()}>
+				<Plus />
+			</Button>
+			<Modal
+				visible={isModal}
+				title='Форма добавления выполненной работы'
+				content={<CompletedWorkForm toggleModal={toggleModal} />}
+				onToggle={toggleModal}
+			/>
+		</div>
 	)
 }
