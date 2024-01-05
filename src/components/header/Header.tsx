351b62bb708e24bc9c24b5ef892e8fe26b04cacb
BolotnikovMS <bolotnikovms@yandex.ru>
2024-01-05 21:50:56 +0500

    feat: Добавлена иконка

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,14 +1,17 @@
 import './header.scss'
 
-import { NavBar } from '../navBar/NavBar'
+import { BookCheck } from 'lucide-react'
 import React from 'react'
+import { NavBar } from '../navBar/NavBar'
 
 export const Header: React.FC = () => {
   return (
     <header className='header'>
       <div className="container">
         <div className='header__content'>
-          <div className='header__logo'>Test task</div>
+          <div className='header__logo'>
+						<BookCheck />
+					</div>
           <NavBar />
         </div>
       </div>
