f8c8d7f62da99bef3b611dfc7b50582b932d61d6
BolotnikovMS <bolotnikovms@yandex.ru>
2024-01-05 20:50:48 +0500

    feat: :zap: Добавлена react-toastify

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,14 @@
-import './styles/reset.scss'
+import 'react-toastify/dist/ReactToastify.css'
 import './styles/main.scss'
+import './styles/reset.scss'
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
-import { App } from './App'
-import { BrowserRouter } from 'react-router-dom'
-import ReactDOM from 'react-dom/client'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
+import { App } from './App'
 
 const client = new QueryClient({
   defaultOptions: {
@@ -20,6 +22,19 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <QueryClientProvider client={client}>
     <BrowserRouter>
       <App />
+			<ToastContainer
+				position="bottom-right"
+				autoClose={5000}
+				hideProgressBar={false}
+				newestOnTop={false}
+				closeOnClick={false}
+				rtl={false}
+				pauseOnFocusLoss
+				draggable={false}
+				pauseOnHover
+				theme="light"
+				style={{ fontSize: '17px' }}
+			/>
     </BrowserRouter>
     <ReactQueryDevtools initialIsOpen={false} />
   </QueryClientProvider>
