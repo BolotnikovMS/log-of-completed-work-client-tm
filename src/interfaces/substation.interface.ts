a73977f1cc72d7a18a7c327ee241469f4f105f67
BolotnikovMS <bolotnikovms@yandex.ru>
2024-01-14 18:04:00 +0500

    feat: Добавлены свойства

--- src/interfaces/substation.interface.ts
+++ src/interfaces/substation.interface.ts
@@ -15,4 +15,37 @@ export interface ISubstation {
   gsmId: number | null
   mainChannelIp: string | null
   backupChannelIp?: string | null
+	numberCompletedWorks?: number
+	voltage_class?: {
+		id: number
+		name: string
+	}
+	district?: {
+		id: number
+		name: string
+	}
+	type_kp?: {
+		id: number
+		name: string
+	}
+	head_controller?: {
+		id: number
+		name: string
+	}
+	main_channel?: {
+		id: number
+		name: string
+	}
+	backup_channel?: {
+		id: number
+		name: string
+	}
+	additional_channel?: {
+		id: number
+		name: string
+	}
+	gsm?: {
+		id: number
+		name: string
+	}
 }
