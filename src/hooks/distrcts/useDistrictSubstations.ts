d8851b4118f95bede8c5570e4c4a76b5b7604858
BolotnikovMS <bolotnikovms@yandex.ru>
2024-01-14 18:03:29 +0500

    fix: Fix

--- src/hooks/distrcts/useDistrictSubstations.ts
+++ src/hooks/distrcts/useDistrictSubstations.ts
@@ -1,13 +1,14 @@
-import { DistrictService } from '../../services/district/district.service'
 import { useQuery } from '@tanstack/react-query'
+import { DistrictService } from '../../services/district/district.service'
 
 export const useDistrictSubstations = (id: string | undefined) => {
-  const { data: substations, error, isError, isLoading } = useQuery({
+  const { data, error, isError, isLoading } = useQuery({
     queryKey: ['district-substations', id],
     queryFn: () => DistrictService.getSubstationsRelatedDistrict(id || ''),
     staleTime: 1000 * 10,
     enabled: !!id,
   })
+	const substations = data?.data
 
   return { substations, error, isError, isLoading }
 }
