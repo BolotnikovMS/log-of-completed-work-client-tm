9e47513740786b91cda6dd4eca13ddcf26ee207d
BolotnikovMS <bolotnikovms@yandex.ru>
2024-01-14 18:05:17 +0500

    feat: Добавлен экспорт

--- src/views/substations/components/index.ts
+++ src/views/substations/components/index.ts
@@ -3,6 +3,7 @@ import { lazy } from 'react'
 const SubstationControl = lazy(() => import('./control/SubstationControl'))
 const SubstationsCards = lazy(() => import('./substations-cards/SubstationsCards'))
 const SubstationForm = lazy(() => import('./form/SubstationForm'))
+const SubstationInfo = lazy(() => import('./info/SubstationInfo'))
 
-export { SubstationControl, SubstationForm, SubstationsCards }
+export { SubstationControl, SubstationForm, SubstationInfo, SubstationsCards }
 
