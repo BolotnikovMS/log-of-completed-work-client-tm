3e638832cedf4fc5b5efb2459000ffea38fc7ad3
BolotnikovMS <bolotnikovms@yandex.ru>
2024-01-21 20:04:07 +0500

    feat: Добавлено использование комопнента с фильтрами

--- src/views/completedWork/components/control/CompletedWorkControl.tsx
+++ src/views/completedWork/components/control/CompletedWorkControl.tsx
@@ -1,6 +1,6 @@
 import { Plus } from 'lucide-react'
 import { type FC } from 'react'
-import { CompletedWorkForm } from '..'
+import { CompletedWorkFilters, CompletedWorkForm } from '..'
 import { Button, Modal } from '../../../../components'
 import { useModal } from '../../../../hooks'
 
@@ -8,16 +8,19 @@ const CompletedWorkControl: FC = () => {
 	const { isModal, toggleModal } = useModal()
 
 	return (
-		<div className="work-log__control">
-			<Button classBtn='btn-bg_green' onClick={() => toggleModal()}>
-				<Plus />
-			</Button>
-			<Modal
-				visible={isModal}
-				title='Форма добавления выполненной работы'
-				content={<CompletedWorkForm toggleModal={toggleModal} />}
-				onToggle={toggleModal}
-			/>
+		<div className="work-log__control control">
+			<div className="control__wrapper">
+				<Button classBtn='btn-bg_green' onClick={() => toggleModal()}>
+					<Plus />
+				</Button>
+				<Modal
+					visible={isModal}
+					title='Форма добавления выполненной работы'
+					content={<CompletedWorkForm toggleModal={toggleModal} />}
+					onToggle={toggleModal}
+				/>
+				<CompletedWorkFilters />
+			</div>
 		</div>
 	)
 }
