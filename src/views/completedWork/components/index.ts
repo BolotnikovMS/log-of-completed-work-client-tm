3057c08d190a55d5e7586bfe9c178d747afc9975
BolotnikovMS <bolotnikovms@yandex.ru>
2024-01-21 20:03:16 +0500

    feat: Компонент с фильтрами

--- src/views/completedWork/components/index.ts
+++ src/views/completedWork/components/index.ts
@@ -3,6 +3,7 @@ import { lazy } from 'react'
 const CompletedWorkControl = lazy(() => import('./control/CompletedWorkControl'))
 const CompletedWorksCards = lazy(() => import('./cards/CompletedWorksCards'))
 const CompletedWorkForm = lazy(() => import('./form/CompletedWorkForm'))
+const CompletedWorkFilters = lazy(() => import('./filters/CompletedWorkFilters'))
 
-export { CompletedWorkControl, CompletedWorkForm, CompletedWorksCards }
+export { CompletedWorkControl, CompletedWorkFilters, CompletedWorkForm, CompletedWorksCards }
 
