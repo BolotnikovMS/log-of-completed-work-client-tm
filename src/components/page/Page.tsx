e1017cabab969a55ef91332bfdd33d733d6e29f6
BolotnikovMS <bolotnikovms@yandex.ru>
2024-03-12 21:24:12 +0500

    feat: Добавлен кастомный класс

--- src/components/page/Page.tsx
+++ src/components/page/Page.tsx
@@ -1,14 +1,15 @@
 import './work-log.scss'
 
+import cx from 'classnames'
 import { type FC } from 'react'
 import { IPropsPage } from './page.interface'
 
-export const Page: FC<IPropsPage> = ({ title, children }) => {
+export const Page: FC<IPropsPage> = ({ title, children, classTitle }) => {
   return (
     <div className="work-log">
       <div className="work-log__content">
         <div className="work-log__titles">
-          <h2 className="title">{title}</h2>
+          <h2 className={cx('title', classTitle)}>{title}</h2>
         </div>
         {children}
       </div>
