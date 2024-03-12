94007b5484df4c74b1d14e11abb962eec2336d10
BolotnikovMS <bolotnikovms@yandex.ru>
2024-03-12 21:27:11 +0500

    feat: Сервис с авторизацией

---
+++ src/services/auth/auth.service.ts
@@ -0,0 +1,23 @@
+import { instance } from '../../api/axios.api'
+import { IUserDataLogin } from '../../interfaces'
+
+interface IUserLogin {
+	id: number
+	username: string
+	email: string
+	role: {
+		name: string
+	}
+	fullName: string
+	shortName: string
+	type: string
+	token: string
+}
+
+export const AuthService = {
+	async login(userDate: IUserDataLogin): Promise<IUserLogin | undefined> {
+		const { data } = await instance.post<IUserLogin>('/login', userDate)
+
+		return data
+	}
+}
