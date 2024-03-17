b1a707e342e32daa8ca478b85f354867fe8a3b68
BolotnikovMS <bolotnikovms@yandex.ru>
2024-03-17 16:45:48 +0500

    feat: Добавлена возможность выйти

--- src/components/navBar/NavBar.tsx
+++ src/components/navBar/NavBar.tsx
@@ -1,10 +1,14 @@
 import './navbar.scss'
 
-import { MenuItems } from './MenuItems'
 import React from 'react'
+import { Button } from '..'
 import { menuItemData } from '../../constants'
+import { useLogout } from '../../hooks'
+import { MenuItems } from './MenuItems'
 
 export const NavBar: React.FC = () => {
+	const { logout } = useLogout()
+	const logoutHandel = () => logout.mutate()
 
   return (
     <nav className='nav'>
@@ -12,6 +16,11 @@ export const NavBar: React.FC = () => {
         {
           menuItemData.map((menu, i) => <MenuItems key={i} title={menu.title} url={menu.url} submenus={menu.submenu} />)
         }
+				<li>
+					<Button onClick={logoutHandel}>
+						Выход
+					</Button>
+				</li>
       </ul>
     </nav>
   )
