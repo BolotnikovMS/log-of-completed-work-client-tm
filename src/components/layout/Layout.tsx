b46772d4dbd4f771b67847c9b9b5e7991c55c038
BolotnikovMS <bolotnikovms@yandex.ru>
2024-03-19 22:01:30 +0500

    feat: Состояние загрузки

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,10 +1,15 @@
 import { Suspense, type FC } from 'react'
 
 import { Outlet } from 'react-router-dom'
+import { useAuthStore } from '../../store/auth'
 import { Header } from '../header/Header'
 import { Loader } from '../loader/Loader'
 
 export const Layout: FC = () => {
+	const userAuthStore = useAuthStore()
+
+	if (userAuthStore.requestLoading) return <Loader />	
+
   return (
     <>
       <Header />
