6607edbde5b02c2e3dc56f26fb79ea71b7327f4d
BolotnikovMS <bolotnikovms@yandex.ru>
2024-03-21 21:27:17 +0500

    feat: Вывод профиля пользователя

--- src/views/auth/Profile.tsx
+++ src/views/auth/Profile.tsx
@@ -1,8 +1,23 @@
 import { type FC } from 'react'
+import { useAuthStore } from '../../store/auth'
 
 const Profile: FC = () => {
+	const { authUser } = useAuthStore()
+
 	return (
-		<div></div>
+		<div>
+			<div className="profile__content">
+				<p>
+					Ф.И.О: { authUser?.fullName }
+				</p>
+				<p>
+					Должность: { authUser?.role.name }
+				</p>
+				<p>
+					Email: { authUser?.email }
+				</p>
+			</div>
+		</div>
 	)
 }
 
