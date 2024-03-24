3449f257604212c454820cbe2dc9c4d01c26da08
BolotnikovMS <bolotnikovms@yandex.ru>
2024-03-24 19:11:11 +0500

    refactor: Реф

--- src/App.tsx
+++ src/App.tsx
@@ -3,40 +3,43 @@
 
 import { getTokenFromLocalStorage, removeTokenFromLocalStorage } from './helpers/localstorege.helper'
 
-import { AuthService } from './services/auth/auth.service'
-import { Router } from './components/routing/Router'
-import { useAuthStore } from './store/auth'
-import { useEffect } from 'react'
 import { useQueryClient } from '@tanstack/react-query'
+import { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { Router } from './components/routing/Router'
+import { AuthService } from './services/auth/auth.service'
+import { useAuthStore } from './store/auth'
 
 export const App = () => {
 	const userAuthStore = useAuthStore()
 	const queryClient = useQueryClient()
+	const navigate = useNavigate()
 	
 	const checkAuth = async () => {
 		const token = getTokenFromLocalStorage()
 		
+		userAuthStore.setRequestLoading(true)
+		
 		try {
 			if (token && Date.now() < Date.parse(token.expiresIn)) {
-				userAuthStore.setRequestLoading(true)
 				const data = await AuthService.getProfile()
 
 				if (data) {
 					userAuthStore.setAuthUser(data)
-					userAuthStore.setRequestLoading(false)
 				} else {
 					console.log('Error auth');
 					userAuthStore.setAuthUser(null)
-					userAuthStore.setRequestLoading(false)
 				}
 			} else {
 				userAuthStore.setAuthUser(null)
-				userAuthStore.setRequestLoading(false)
 				removeTokenFromLocalStorage('user_token')
 				queryClient.removeQueries()
+				navigate('/sign-in')
 			}
 		} catch (error) {
 			console.log(error)
+		} finally {
+			userAuthStore.setRequestLoading(false)
 		}
 	}
 
