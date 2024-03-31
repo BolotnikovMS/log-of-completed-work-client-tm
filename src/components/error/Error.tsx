cfa73dbe6641b2bdc3a12cb415d26027c0f4f3cd
BolotnikovMS <bolotnikovms@yandex.ru>
2024-03-31 20:10:08 +0500

    refactor: Поправлена обработка ошибок

--- src/components/error/Error.tsx
+++ src/components/error/Error.tsx
@@ -2,15 +2,16 @@ import './error.scss'
 
 import { AxiosError } from 'axios'
 import { type FC } from 'react'
+import { errorHandler } from '../../helpers/errorHandler.helper'
 
 interface IPropsError {
-  error: Error | AxiosError<unknown> | null
+  error: Error | AxiosError<unknown>
 }
 
 const Error: FC<IPropsError> = ({ error }) => {
   return (
     <div className='errors'>
-      <div className='error__message'>{error?.message} :(</div>
+      <div className='error__message'>{ errorHandler(error) } :(</div>
     </div>
   )
 }
