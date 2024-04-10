2fd83226cea1109a2d777134e3205f7b83fedb25
BolotnikovMS <bolotnikovms@yandex.ru>
2024-04-10 18:21:05 +0500

    feat: Добавлена проверка прав

--- src/views/completedWork/components/cards/CompletedWorksCards.tsx
+++ src/views/completedWork/components/cards/CompletedWorksCards.tsx
@@ -1,14 +1,16 @@
-import { format } from 'date-fns'
 import { Pencil, Trash2 } from 'lucide-react'
 import { useState, type FC } from 'react'
 import { Link, useSearchParams } from 'react-router-dom'
 import { CompletedWorkForm } from '..'
-import { Button, Card, Error, InfoMessage, LoadMore, Loader, Modal } from '../../../../components'
+import { Button, Card, Error, InfoMessage, Loader, LoadMore, Modal } from '../../../../components'
+import { checkRole, ERoles } from '../../../../helpers/checkRole.helper'
 import { useDeleteCompletedWork, useModal } from '../../../../hooks'
 import { useInfiniteCompletedWork } from '../../../../hooks/completed-works/useInfiniteCompletedWork'
 import { ICompletedWork } from '../../../../interfaces'
+import { useAuthStore } from '../../../../store/auth'
 
 const CompletedWorksCards: FC = () => {
+	const { authUser } = useAuthStore()
 	const [searchParams] = useSearchParams()
 	const substationParam = searchParams.get('substation')
 	const { data, error, fetchNextPage, hasNextPage, isError, isFetching, isFetchingNextPage } = useInfiniteCompletedWork({ limit: 15, substation: substationParam })
@@ -24,35 +26,44 @@ const CompletedWorksCards: FC = () => {
 		return deleteCompletedWork.mutate(id)
   }
 
+	if (isError && error) return <Error error={error}/>
+	
+	if (isFetching) return <Loader />
+
 	return (
 		<>
-      {(isError) && <Error error={error}/>}
-      {isFetching ? (<Loader />) : 
-        (!!data?.pages[0].data.length && (
-          <div className="cards">
-            {data.pages.map(completedWorks => (
-              completedWorks.data.map(completedWork => (
-								<Card
-									key={completedWork.id}
-									childrenHeader={<p className='card__text card__text-bold'><Link to={`/substations/${completedWork.substation.id}`}>{completedWork.substation.fullNameSubstation}</Link></p>}
-									childrenBody={<p>{completedWork.description}</p>}
-									childrenFooter={<p>Дата работ: {format(completedWork.dateCompletion, 'dd.MM.yyyy')}. Выполнил: {completedWork.work_producer.shortUserName} </p>}
-									childrenControl={
-										<>
-											<Button onClick={() => {toggleModal(), setCompetedWork(completedWork), setIsEdited(!isEdited)}}>
-												<Pencil />
-											</Button>
-											<Button classBtn='btn-bg_red' onClick={() => handleDelete(completedWork.id)}>
-												<Trash2 />
-											</Button>
-										</>
-									}
-								/>
-              ))
-            ))}
-          </div>
-        ))
-      }
+      {!!data?.pages[0].data.length && (
+				<div className="cards">
+					{data.pages.map(completedWorks => (
+						completedWorks.data.map(completedWork => (
+							<Card
+								key={completedWork.id}
+								childrenHeader={<p className='card__text card__text-bold'><Link to={`/substations/${completedWork.substation.id}`}>{completedWork.substation.fullNameSubstation}</Link></p>}
+								childrenBody={<p>{completedWork.description}</p>}
+								childrenFooter={<p>Дата работ: {completedWork.dateCompletion.toString()}. Выполнил: {completedWork.work_producer.shortName} </p>}
+								childrenControl={
+									<>
+										{
+											checkRole(authUser, [ERoles.Admin, ERoles.Moderator]) && (
+												<Button onClick={() => {toggleModal(), setCompetedWork(completedWork), setIsEdited(!isEdited)}}>
+													<Pencil />
+												</Button>
+											)
+										}
+										{
+											checkRole(authUser, [ERoles.Admin, ERoles.Moderator]) && (
+												<Button classBtn='btn-bg_red' onClick={() => handleDelete(completedWork.id)}>
+													<Trash2 />
+												</Button>
+											)
+										}
+									</>
+								}
+							/>
+						))
+					))}
+				</div>
+			)}
 			{(!data?.pages[0].meta.total && !isFetching && !isError) && <InfoMessage text='Пока нет выполненных работ по ПС...' />}
 			{hasNextPage && <LoadMore hasNextPage={hasNextPage} isFetching={isFetching} isFetchingNextPage={isFetchingNextPage} fetchNextPage={fetchNextPage} />}
 			<Modal visible={isModal} title='Редактирование записи' onToggle={() => {toggleModal(), setIsEdited(false)}} content={<CompletedWorkForm completedWork={completedWork} isEdited={isEdited} setIsEdited={setIsEdited} toggleModal={toggleModal} />}/>
