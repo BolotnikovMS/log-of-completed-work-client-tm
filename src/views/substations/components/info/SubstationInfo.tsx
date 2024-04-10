242165aa6cd78162bf23477f0f21ae0a20db6066
BolotnikovMS <bolotnikovms@yandex.ru>
2024-04-10 18:22:05 +0500

    refactor: Вынесены лоадер и сообщение об ошибках

--- src/views/substations/components/info/SubstationInfo.tsx
+++ src/views/substations/components/info/SubstationInfo.tsx
@@ -10,42 +10,39 @@ const SubstationInfo: FC = () => {
 	const { substation, error, isError, isLoading } = useSubstation(id)
 	const navigate = useNavigate()
 
+	if (isError && error) return <Error error={error}/>
+
+	if (isLoading) return <Loader />
+
 	return (
 		<>
-			{isLoading ? (<Loader />) :
-				isError ? (<Error error={error}/>) :
-					(
-						<>
-							<h1 className='title'>{substation?.fullNameSubstation}</h1>
-							<div className="info info-mt">
-								<div className="info__wrapper">
-									<div className="info__content">
-										<p>РЭС/ГП: <span className='text__sub'>{substation?.district?.name}</span></p>
-										<p>РДУ: <span className='text__sub'>{substation?.rdu ? 'Да' : 'Нет'}</span></p>
-										<p>Тип КП: <span className='text__sub'>{substation?.type_kp?.name}</span></p>
-										<p>Головной контроллер: <span className='text__sub'>{substation?.head_controller?.name}</span></p>
-										<p>Основной канал: <span className='text__sub'>{substation?.main_channel?.name}</span></p>
-										{substation?.backup_channel &&  <p>Резервный канал: <span className='text__sub'>{substation?.backup_channel ? substation?.backup_channel.name : '-' }</span></p>}
-										{substation?.additional_channel && <p>Дополнительный канал: <span className='text__sub'>{substation?.additional_channel.name}</span></p>}
-										{substation?.gsm && <p>Gsm оператор: <span className='text__sub'>{substation?.gsm?.name}</span></p>}
-										{substation?.mainChannelIp && <p>IP основного канала: <span className='text__sub'>{substation?.mainChannelIp}</span></p>}
-										{substation?.backupChannelIp && <p>IP резервного канала: <span className='text__sub'>{substation?.backupChannelIp}</span></p>}
-										<p>Всего выполнено работ: <span className="text__sub">{substation?.numberCompletedWorks}</span></p>
-									</div>
-									<div className="info__imgs">
-										<Image width={400} height={400}/>
-									</div>
-								</div>
-								<div className="info__btns info__btns-mt">
-									<Button classBtn='btn-bg_blue' onClick={() => navigate(-1)}><ArrowLeft />Обратно</Button>
-									<Link to={`/completed-works?substation=${substation?.id}`} className='btn btn-bg_green'>
-										<BookTextIcon />Работы
-									</Link>
-								</div>
-							</div>
-						</>
-					)
-			}
+			<h1 className='title'>{substation?.fullNameSubstation}</h1>
+			<div className="info info-mt">
+				<div className="info__wrapper">
+					<div className="info__content">
+						<p>РЭС/ГП: <span className='text__sub'>{substation?.district?.name}</span></p>
+						<p>РДУ: <span className='text__sub'>{substation?.rdu ? 'Да' : 'Нет'}</span></p>
+						<p>Тип КП: <span className='text__sub'>{substation?.type_kp?.name}</span></p>
+						<p>Головной контроллер: <span className='text__sub'>{substation?.head_controller?.name}</span></p>
+						<p>Основной канал: <span className='text__sub'>{substation?.main_channel?.name}</span></p>
+						{substation?.backup_channel &&  <p>Резервный канал: <span className='text__sub'>{substation?.backup_channel ? substation?.backup_channel.name : '-' }</span></p>}
+						{substation?.additional_channel && <p>Дополнительный канал: <span className='text__sub'>{substation?.additional_channel.name}</span></p>}
+						{substation?.gsm && <p>Gsm оператор: <span className='text__sub'>{substation?.gsm?.name}</span></p>}
+						{substation?.mainChannelIp && <p>IP основного канала: <span className='text__sub'>{substation?.mainChannelIp}</span></p>}
+						{substation?.backupChannelIp && <p>IP резервного канала: <span className='text__sub'>{substation?.backupChannelIp}</span></p>}
+						<p>Всего выполнено работ: <span className="text__sub">{substation?.numberCompletedWorks}</span></p>
+					</div>
+					<div className="info__imgs">
+						<Image width={400} height={400}/>
+					</div>
+				</div>
+				<div className="info__btns info__btns-mt">
+					<Button classBtn='btn-bg_blue' onClick={() => navigate(-1)}><ArrowLeft />Обратно</Button>
+					<Link to={`/completed-works?substation=${substation?.id}`} className='btn btn-bg_green'>
+						<BookTextIcon />Работы
+					</Link>
+				</div>
+			</div>
 		</>
 	)
 }
