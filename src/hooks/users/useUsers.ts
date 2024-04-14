5a842f530370c7efe66ef9ebd56f87368e979682
BolotnikovMS <bolotnikovms@yandex.ru>
2024-04-14 13:47:14 +0500

    feat: Комонент формы добавления пользователя

--- src/hooks/users/useUsers.ts
+++ src/hooks/users/useUsers.ts
@@ -2,11 +2,11 @@ import { UserService } from '../../services/user/user.service'
 import { useQuery } from '@tanstack/react-query'
 
 export const useUsers = () => {
-	const { data, error, isError, isLoading, isFetching } = useQuery({
-		queryKey: ['users', 'all'],
-		queryFn: () => UserService.getUsers(),
-		staleTime: 1000 * 10,
-	})
+  const { data, error, isError, isLoading, isFetching } = useQuery({
+    queryKey: ['users', 'all'],
+    queryFn: () => UserService.getUsers(),
+    staleTime: 1000 * 10,
+  })
 
-	return { data, error, isError, isLoading, isFetching }
+  return { data, error, isError, isLoading, isFetching }
 }
---
+++ src/views/user/components/form/UserForm.tsx
@@ -0,0 +1,11 @@
+import { type FC } from "react";
+
+const UserForm: FC = () => {
+  return (
+    <>
+      <div className="work-log__form"></div>
+    </>
+  )
+}
+
+export default UserForm
