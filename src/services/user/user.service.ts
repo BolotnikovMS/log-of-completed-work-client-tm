e1d45a4b4ae7116b338d1fa9343630ce36317381
BolotnikovMS <bolotnikovms@yandex.ru>
2024-04-14 15:38:47 +0500

    refactor: Добавлена типизация

--- src/services/user/user.service.ts
+++ src/services/user/user.service.ts
@@ -1,10 +1,13 @@
-import { IUser } from '../../interfaces'
 import { instance } from '../../api/axios.api'
 import { url } from '../../constants'
+import { IQueryParams } from '../../interfaces'
+import { TRespUsers } from './user.type'
 
 export const UserService = {
-	async getUsers() {
-		const { data } = await instance.get<IUser[]>(`${url}/users`)
+	async getUsers({ limit, page }: IQueryParams): Promise<TRespUsers> {
+		const { data } = await instance.get<TRespUsers>(`${url}/users`, {
+			params: { limit, page },
+		})
 
 		return data
 	}
