666019969be48dd8c624fa3a128c52697ff3f637
BolotnikovMS <bolotnikovms@yandex.ru>
2024-04-17 22:00:04 +0500

    feat: Экспорт интерфейса роли

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -8,9 +8,10 @@ import { IGsmOperator } from './gsm-operator.interface'
 import { IHeadController } from './head-controller.interface'
 import { IMeta } from './meta.interface'
 import { IQueryParams } from './qparams.interface'
+import { IRole } from './role.interface'
 import { ISubstation } from './substation.interface'
 import { ITypeKp } from './type-kp'
 import { IVoltageClass } from './voltage-class.interface'
 
-export type { IChannelType, ICompletedWork, IDistrict, IGenericResponseLogout, IGsmOperator, IHeadController, IMeta, IQueryParams, ISignInFields, ISubstation, ITypeKp, IUser, IUserDataLogin, IUserLogin, IVoltageClass }
+export type { IChannelType, ICompletedWork, IDistrict, IGenericResponseLogout, IGsmOperator, IHeadController, IMeta, IQueryParams, IRole, ISignInFields, ISubstation, ITypeKp, IUser, IUserDataLogin, IUserLogin, IVoltageClass }
 
