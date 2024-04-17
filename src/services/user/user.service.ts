6f99437e4b1a6048fe2c5c577c4dda1bd150ee77
BolotnikovMS <bolotnikovms@yandex.ru>
2024-04-17 22:00:33 +0500

    feat: Добавлен метод для создания пользователя

--- src/services/user/user.service.ts
+++ src/services/user/user.service.ts
@@ -1,7 +1,9 @@
+import { IQueryParams, IUser } from '../../interfaces'
+import { TRespUsers, TUserData } from './user.type'
+
+import { AxiosResponse } from 'axios'
 import { instance } from '../../api/axios.api'
 import { url } from '../../constants'
-import { IQueryParams } from '../../interfaces'
-import { TRespUsers } from './user.type'
 
 export const UserService = {
 	async getUsers({ limit, page }: IQueryParams): Promise<TRespUsers> {
@@ -10,5 +12,9 @@ export const UserService = {
 		})
 
 		return data
+	},
+
+	async createUser(data: TUserData): Promise<AxiosResponse<IUser>> {
+		return instance.post<IUser>(`${url}/users/create-account`, data)
 	}
 }
