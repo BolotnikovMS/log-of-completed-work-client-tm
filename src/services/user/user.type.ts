4016f20afc536b693821810c2d66f5a71125a9f8
BolotnikovMS <bolotnikovms@yandex.ru>
2024-04-17 22:01:03 +0500

    fix: Исключены поля

--- src/services/user/user.type.ts
+++ src/services/user/user.type.ts
@@ -1,5 +1,5 @@
 import { IMeta, IUser } from '../../interfaces'
 
-export type TUserData = Omit<IUser, 'id'>
+export type TUserData = Omit<IUser, 'id' | 'fullName' | 'shortName' | 'role'>
 
 export type TRespUsers = { meta: IMeta, data: IUser[] }
