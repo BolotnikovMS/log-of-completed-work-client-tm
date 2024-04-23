87d3cf7ca598bb878019a5d2aef968c3345a7d3f
BolotnikovMS <bolotnikovms@yandex.ru>
2024-04-23 21:06:00 +0500

    feat: Добавлены поля

--- src/components/input/input.interface.ts
+++ src/components/input/input.interface.ts
@@ -1,3 +1,8 @@
+import { ReactNode } from 'react'
+
 export interface IPropsInput extends React.InputHTMLAttributes<HTMLInputElement> {
   className?: string
+	iconLeft?: ReactNode
+	iconRight?: ReactNode
+	error?:  boolean
 }
