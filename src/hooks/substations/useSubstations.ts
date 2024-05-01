60dff7441a22118b6a1fce5f6ca89c270897e92d
BolotnikovMS <bolotnikovms@yandex.ru>
2024-05-01 18:11:16 +0500

    feat: Добален page и limit

--- src/hooks/substations/useSubstations.ts
+++ src/hooks/substations/useSubstations.ts
@@ -1,10 +1,11 @@
 import { useQuery } from '@tanstack/react-query'
+import { IQueryParams } from '../../interfaces'
 import { SubstationService } from '../../services/substations/substation.service'
 
-export const useSubstations = () => {
+export const useSubstations = ({page, limit}: IQueryParams) => {
   const { data: substations, error, isError, isLoading } = useQuery({
     queryKey: ['substations', 'all'],
-    queryFn: () => SubstationService.getSubstations({}),
+    queryFn: () => SubstationService.getSubstations({ page, limit }),
     staleTime: 1000 * 10,
   })
 
