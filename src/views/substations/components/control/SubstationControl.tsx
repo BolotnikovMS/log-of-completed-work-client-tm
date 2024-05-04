5b4b835447c6f0b622bfc38bcfc0aa353659d0de
BolotnikovMS <bolotnikovms@yandex.ru>
2024-05-04 16:31:57 +0500

    feat: Добавлена обертка

--- src/views/substations/components/control/SubstationControl.tsx
+++ src/views/substations/components/control/SubstationControl.tsx
@@ -2,7 +2,7 @@ import { Button, Modal } from '../../../../components'
 
 import { Plus } from 'lucide-react'
 import { type FC } from 'react'
-import { SubstationForm } from '..'
+import { SubstationFilters, SubstationForm } from '..'
 import { checkRole, ERoles } from '../../../../helpers/checkRole.helper'
 import { useModal } from '../../../../hooks'
 import { useAuthStore } from '../../../../store/auth'
@@ -13,19 +13,22 @@ const SubstationControl: FC = () => {
 
   return (
     <div className="work-log__control">
-			{
-				checkRole(authUser, [ERoles.Admin, ERoles.Moderator]) && (
-					<Button classBtn='btn-bg_green' onClick={() => toggleModal()}>
-						<Plus />
-					</Button>
-				)
-			}
-      <Modal
-        visible={isModal} 
-        title='Форма добавления подстанций' 
-        content={<SubstationForm toggleModal={toggleModal} />}
-        onToggle={toggleModal} 
-      />
+			<div className="control__wrapper">
+				{
+					checkRole(authUser, [ERoles.Admin, ERoles.Moderator]) && (
+						<Button classBtn='btn-bg_green' onClick={() => toggleModal()}>
+							<Plus />
+						</Button>
+					)
+				}
+				<Modal
+					visible={isModal} 
+					title='Форма добавления подстанций' 
+					content={<SubstationForm toggleModal={toggleModal} />}
+					onToggle={toggleModal} 
+				/>
+				<SubstationFilters />
+			</div>
     </div>
   )
 }
