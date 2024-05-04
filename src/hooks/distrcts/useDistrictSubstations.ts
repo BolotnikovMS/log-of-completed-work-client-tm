dcd28a07414082fd61d516844edba533f2f7ed9d
BolotnikovMS <bolotnikovms@yandex.ru>
2024-05-04 16:28:18 +0500

    refactor: Добавлен интерфейс, добавлен параметр поиска

--- src/hooks/distrcts/useDistrictSubstations.ts
+++ src/hooks/distrcts/useDistrictSubstations.ts
@@ -1,10 +1,15 @@
 import { useQuery } from '@tanstack/react-query'
 import { DistrictService } from '../../services/district/district.service'
 
-export const useDistrictSubstations = (id: string | undefined) => {
+interface IPropsDistrictSubstation {
+	id: string | undefined
+	search?: string
+}
+
+export const useDistrictSubstations = ({ id, search }: IPropsDistrictSubstation) => {
   const { data, error, isError, isLoading } = useQuery({
-    queryKey: ['district-substations', id],
-    queryFn: () => DistrictService.getSubstationsRelatedDistrict(id || ''),
+    queryKey: ['district-substations', id, search],
+    queryFn: () => DistrictService.getSubstationsRelatedDistrict(id || '', { search }),
     staleTime: 1000 * 10,
     enabled: !!id,
   })
