26ab6f40d77ba7d204ebb0cff859b296e3b8c0c0
BolotnikovMS <bolotnikovms@yandex.ru>
2024-05-06 19:52:20 +0500

    refactor: Использована иконка

--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -1,5 +1,6 @@
 import './modal.scss'
 
+import { X } from 'lucide-react'
 import { type FC } from 'react'
 import { createPortal } from 'react-dom'
 import { IPropsModal } from './modal.interface'
@@ -12,7 +13,9 @@ export const Modal: FC<IPropsModal> = ({ visible, title, content, footer, onTogg
       <div className="modal__dialog" onClick={e => e.stopPropagation()}>
         <div className="modal__header">
           <h3 className="modal__title">{title}</h3>
-          <span className="modal__close" onClick={onToggle}>&times;</span>
+          <span className="modal__close" onClick={onToggle}>
+						<X />
+					</span>
         </div>
         <div className="modal__body">
           <div className="modal__content">{content}</div>
