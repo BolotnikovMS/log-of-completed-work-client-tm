abeee43520156dcbc150d3d5cc6e938b41a65a57
BolotnikovMS <bolotnikovms@yandex.ru>
2024-05-11 18:23:21 +0500

    feat: Поправлены стили, добавлена таблица бэкапов

--- src/views/substations/components/info/SubstationInfo.tsx
+++ src/views/substations/components/info/SubstationInfo.tsx
@@ -3,6 +3,7 @@ import { type FC } from 'react'
 import { Link, useNavigate, useParams } from 'react-router-dom'
 import { Button, Error, Loader } from '../../../../components'
 import { useSubstation } from '../../../../hooks'
+import BackupTable from '../backupTable/BackupTable'
 import './info.scss'
 
 const SubstationInfo: FC = () => {
@@ -20,22 +21,26 @@ const SubstationInfo: FC = () => {
 			<div className="info info-mt">
 				<div className="info__wrapper">
 					<div className="info__content">
-						<p>РЭС/ГП: <span className='text__sub'>{substation?.district?.name}</span></p>
-						<p>РДУ: <span className='text__sub'>{substation?.rdu ? 'Да' : 'Нет'}</span></p>
-						<p>Тип КП: <span className='text__sub'>{substation?.type_kp?.name}</span></p>
-						<p>Головной контроллер: <span className='text__sub'>{substation?.head_controller?.name}</span></p>
-						<p>Основной канал: <span className='text__sub'>{substation?.main_channel?.name}</span></p>
-						{substation?.backup_channel &&  <p>Резервный канал: <span className='text__sub'>{substation?.backup_channel ? substation?.backup_channel.name : '-' }</span></p>}
-						{substation?.additional_channel && <p>Дополнительный канал: <span className='text__sub'>{substation?.additional_channel.name}</span></p>}
-						{substation?.gsm && <p>Gsm оператор: <span className='text__sub'>{substation?.gsm?.name}</span></p>}
-						{substation?.mainChannelIp && <p>IP основного канала: <span className='text__sub'>{substation?.mainChannelIp}</span></p>}
-						{substation?.backupChannelIp && <p>IP резервного канала: <span className='text__sub'>{substation?.backupChannelIp}</span></p>}
-						<p>Всего выполнено работ: <span className="text__sub">{substation?.numberCompletedWorks}</span></p>
+						<p className='text'>РЭС/ГП: <span className='sub-text'>{substation?.district?.name}</span></p>
+						<p className='text'>РДУ: <span className='sub-text'>{substation?.rdu ? 'Да' : 'Нет'}</span></p>
+						<p className='text'>Тип КП: <span className='sub-text'>{substation?.type_kp?.name}</span></p>
+						<p className='text'>Головной контроллер: <span className='sub-text'>{substation?.head_controller?.name}</span></p>
+						<p className='text'>Основной канал: <span className='sub-text'>{substation?.main_channel?.name}</span></p>
+						{substation?.backup_channel &&  <p className='text'>Резервный канал: <span className='sub-text'>{substation?.backup_channel ? substation?.backup_channel.name : '-' }</span></p>}
+						{substation?.additional_channel && <p className='text'>Дополнительный канал: <span className='sub-text'>{substation?.additional_channel.name}</span></p>}
+						{substation?.gsm && <p className='text'>Gsm оператор: <span className='sub-text'>{substation?.gsm?.name}</span></p>}
+						{substation?.mainChannelIp && <p>IP основного канала: <span className='sub-text'>{substation?.mainChannelIp}</span></p>}
+						{substation?.backupChannelIp && <p className='text'>IP резервного канала: <span className='sub-text'>{substation?.backupChannelIp}</span></p>}
+						<p className='text'>Всего выполнено работ: <span className="sub-text">{substation?.numberCompletedWorks}</span></p>
 					</div>
 					<div className="info__imgs">
 						<Image width={400} height={400}/>
 					</div>
 				</div>
+
+				{substation?.files?.length ? (
+					<BackupTable backupFiles={substation?.files} />
+				) : null}
 				<div className="info__btns info__btns-mt">
 					<Button classBtn='btn-bg_blue' onClick={() => navigate(-1)}><ArrowLeft />Обратно</Button>
 					<Link to={`/completed-works?substation=${substation?.id}`} className='btn btn-bg_green'>
