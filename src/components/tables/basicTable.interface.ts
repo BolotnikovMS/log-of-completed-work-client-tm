6e7a0bf491186bcb4aa2598d57301c9b96763342
BolotnikovMS <bolotnikovms@yandex.ru>
2024-05-11 18:17:11 +0500

    feat: Добавлены свойства

--- src/components/tables/basicTable.interface.ts
+++ src/components/tables/basicTable.interface.ts
@@ -3,4 +3,7 @@ export interface IPropsBasicTable {
 	data: unknown[]
 	// eslint-disable-next-line @typescript-eslint/no-explicit-any
 	columns: any[]
+	search?: boolean
+	size?: number
+	title?: string
 }
