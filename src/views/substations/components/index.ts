f5ec276560d7e3e5ecd54580b42f4cf1270f1800
BolotnikovMS <bolotnikovms@yandex.ru>
2024-05-14 20:14:23 +0500

    feat: Обновлены экспорты

--- src/views/substations/components/index.ts
+++ src/views/substations/components/index.ts
@@ -1,11 +1,12 @@
 import { lazy } from 'react'
+import { UploadSubstationFile } from './forms/uploadFile/UploadSubstationFile'
 
 const SubstationControl = lazy(() => import('./control/SubstationControl'))
 const SubstationsCards = lazy(() => import('./substations-cards/SubstationsCards'))
-const SubstationForm = lazy(() => import('./form/SubstationForm'))
+const SubstationForm = lazy(() => import('./forms/createSubstation/SubstationForm'))
 const SubstationInfo = lazy(() => import('./info/SubstationInfo'))
 const SubstationFilters = lazy(() => import('./filters/SubstationFilters'))
 const BackupTable = lazy(() => import('./backupTable/BackupTable'))
 
-export { BackupTable, SubstationControl, SubstationFilters, SubstationForm, SubstationInfo, SubstationsCards }
+export { BackupTable, SubstationControl, SubstationFilters, SubstationForm, SubstationInfo, SubstationsCards, UploadSubstationFile }
 
