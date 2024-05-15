0d5a52ecf1ef962fcc2628b2146716aa559ec8da
BolotnikovMS <bolotnikovms@yandex.ru>
2024-05-15 20:25:51 +0500

    fix: Поправлено

--- src/hooks/modal/useModal.ts
+++ src/hooks/modal/useModal.ts
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 
 export const useModal = () => {
-  const [isModal, setIsModal] = useState<boolean>(true)
+  const [isModal, setIsModal] = useState<boolean>(false)
   const toggleModal = () => setIsModal(!isModal)
 
   return { isModal, toggleModal }
