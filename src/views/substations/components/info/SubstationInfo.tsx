271e519eeb9dd5b4d0c8b7e96f28978a19ea77dc
BolotnikovMS <bolotnikovms@yandex.ru>
2024-05-15 20:28:47 +0500

    refactor: Рефакторинг

--- src/views/substations/components/info/SubstationInfo.tsx
+++ src/views/substations/components/info/SubstationInfo.tsx
@@ -1,9 +1,8 @@
-import { ArrowLeft, BookTextIcon, Image, Paperclip } from 'lucide-react'
+import { ArrowLeft, BookTextIcon, Image } from 'lucide-react'
 import { type FC } from 'react'
 import { Link, useNavigate, useParams } from 'react-router-dom'
-import { UploadSubstationFile } from '..'
-import { Button, Error, Loader, Modal } from '../../../../components'
-import { useModal, useSubstation } from '../../../../hooks'
+import { Button, Error, Loader } from '../../../../components'
+import { useSubstation } from '../../../../hooks'
 import BackupTable from '../backupTable/BackupTable'
 import './info.scss'
 
@@ -11,58 +10,43 @@ const SubstationInfo: FC = () => {
 	const { id } = useParams()
 	const { substation, error, isError, isLoading } = useSubstation(id)
 	const navigate = useNavigate()
-	const { isModal, toggleModal } = useModal()
 
 	if (isError && error) return <Error error={error}/>
 
 	if (isLoading) return <Loader />
 
 	return (
-		<>
-			<div className="info-control info-control-mb-15">
-				<Button onClick={() => toggleModal()}>
-					<Paperclip />
-				</Button>
-			</div>	
+		<div className="info info-mt">
 			<h1 className='title'>{substation?.fullNameSubstation}</h1>
-			<div className="info info-mt">
-				<div className="info__wrapper">
-					<div className="info__content">
-						<p className='text'>РЭС/ГП: <span className='sub-text'>{substation?.district?.name}</span></p>
-						<p className='text'>РДУ: <span className='sub-text'>{substation?.rdu ? 'Да' : 'Нет'}</span></p>
-						<p className='text'>Тип КП: <span className='sub-text'>{substation?.type_kp?.name}</span></p>
-						<p className='text'>Головной контроллер: <span className='sub-text'>{substation?.head_controller?.name}</span></p>
-						<p className='text'>Основной канал: <span className='sub-text'>{substation?.main_channel?.name}</span></p>
-						{substation?.backup_channel &&  <p className='text'>Резервный канал: <span className='sub-text'>{substation?.backup_channel ? substation?.backup_channel.name : '-' }</span></p>}
-						{substation?.additional_channel && <p className='text'>Дополнительный канал: <span className='sub-text'>{substation?.additional_channel.name}</span></p>}
-						{substation?.gsm && <p className='text'>Gsm оператор: <span className='sub-text'>{substation?.gsm?.name}</span></p>}
-						{substation?.mainChannelIp && <p>IP основного канала: <span className='sub-text'>{substation?.mainChannelIp}</span></p>}
-						{substation?.backupChannelIp && <p className='text'>IP резервного канала: <span className='sub-text'>{substation?.backupChannelIp}</span></p>}
-						<p className='text'>Всего выполнено работ: <span className="sub-text">{substation?.numberCompletedWorks}</span></p>
-					</div>
-					<div className="info__imgs">
-						<Image width={400} height={400}/>
-					</div>
+			<div className="info__wrapper">
+				<div className="info__content">
+					<p className='text'>РЭС/ГП: <span className='sub-text'>{substation?.district?.name}</span></p>
+					<p className='text'>РДУ: <span className='sub-text'>{substation?.rdu ? 'Да' : 'Нет'}</span></p>
+					<p className='text'>Тип КП: <span className='sub-text'>{substation?.type_kp?.name}</span></p>
+					<p className='text'>Головной контроллер: <span className='sub-text'>{substation?.head_controller?.name}</span></p>
+					<p className='text'>Основной канал: <span className='sub-text'>{substation?.main_channel?.name}</span></p>
+					{substation?.backup_channel &&  <p className='text'>Резервный канал: <span className='sub-text'>{substation?.backup_channel ? substation?.backup_channel.name : '-' }</span></p>}
+					{substation?.additional_channel && <p className='text'>Дополнительный канал: <span className='sub-text'>{substation?.additional_channel.name}</span></p>}
+					{substation?.gsm && <p className='text'>Gsm оператор: <span className='sub-text'>{substation?.gsm?.name}</span></p>}
+					{substation?.mainChannelIp && <p>IP основного канала: <span className='sub-text'>{substation?.mainChannelIp}</span></p>}
+					{substation?.backupChannelIp && <p className='text'>IP резервного канала: <span className='sub-text'>{substation?.backupChannelIp}</span></p>}
+					<p className='text'>Всего выполнено работ: <span className="sub-text">{substation?.numberCompletedWorks}</span></p>
 				</div>
-
-				{substation?.files?.length ? (
-					<BackupTable backupFiles={substation?.files} />
-				) : null}
-				<div className="info__btns info__btns-mt">
-					<Button classBtn='btn-bg_blue' onClick={() => navigate(-1)}><ArrowLeft />Обратно</Button>
-					<Link to={`/completed-works?substation=${substation?.id}`} className='btn btn-bg_green'>
-						<BookTextIcon />Работы
-					</Link>
+				<div className="info__imgs">
+					<Image width={400} height={400}/>
 				</div>
 			</div>
-			
-			<Modal
-				visible={isModal}
-				title='Добавление файла'
-				content={<UploadSubstationFile toggleModal={toggleModal} />}
-				onToggle={toggleModal}
-			/>
-		</>
+
+			{substation?.files_backups?.length ? (
+				<BackupTable backupFiles={substation?.files_backups} />
+			) : null}
+			<div className="info__btns info__btns-mt">
+				<Button classBtn='btn-bg_blue' onClick={() => navigate(-1)}><ArrowLeft />Обратно</Button>
+				<Link to={`/completed-works?substation=${substation?.id}`} className='btn btn-bg_green'>
+					<BookTextIcon />Работы
+				</Link>
+			</div>
+		</div>
 	)
 }
 
