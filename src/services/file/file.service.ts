3ba0079e6d610f1cf2673948501965630ce8c3a3
BolotnikovMS <bolotnikovms@yandex.ru>
2024-05-19 18:32:28 +0500

    feat: Добавлен метод для скачивания
    Перемещение в папку file и добавление метода

---
+++ src/services/file/file.service.ts
@@ -0,0 +1,32 @@
+import { AxiosResponse } from 'axios'
+import fileDownload from 'js-file-download'
+import { toast } from 'react-toastify'
+import { instance } from '../../api/axios.api'
+import { url } from '../../constants'
+import { errorHandler } from '../../helpers'
+import { IFile } from '../../interfaces/file.interface'
+import { TNewFileUpload } from './file.type'
+
+export const FileService = {
+	async upload(data: TNewFileUpload): Promise<AxiosResponse<string>> {
+		return instance.post(`${url}/files/upload`, data, {
+			headers: {
+				'Content-Type': 'multipart/form-data'
+			}
+		})
+	},
+
+	async download(file: IFile): Promise<void> {
+		await instance.get(`${url}/files/download/${file.id}`, {
+			responseType: 'blob'
+		}).then(downloadFile => {
+			fileDownload(downloadFile.data, file.clientName)
+		}).catch(e => {
+			toast.error(errorHandler(e))
+		})
+	},
+
+	async delete(id: number): Promise<void> {
+		return instance.delete(`${url}/files/${id}`)
+	}
+}
