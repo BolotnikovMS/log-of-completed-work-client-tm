2508fc4fc8472e6a9b59aa2b12103964aad1a085
BolotnikovMS <bolotnikovms@yandex.ru>
2024-06-10 14:25:32 +0500

    refactor:  Добавлена проверка на автора записи

--- src/helpers/checkRole.helper.ts
+++ src/helpers/checkRole.helper.ts
@@ -6,8 +6,23 @@ export enum ERoles {
 	User = 'User'
 }
 
-export const checkRole = (user: IUser | null, allowedRole: ERoles[]): boolean => {
+/**
+ * Description
+ * @param {IUser|null} Авторизованный пользователь.
+ * @param {ERoles[]} Массив ролей которым доступны действия.
+ * @param {boolean=false} Флаг, который указывает на то, что автор записи может выполнять действие над записью.
+ * @param {unknown} Текущая запись.
+ * @returns {any} Возвращает true или false
+ */
+
+export const checkRole = (user: IUser | null, allowedRole: ERoles[], author: boolean = false, record?: unknown): boolean => {
 	if (!user) return false
 
-	return !!allowedRole.find(role => role.includes(user.role.name))
+	if (author && record && user.id === record.userId) {
+		return true
+	} else {
+		return !!allowedRole.find(role => role.includes(user.role.name))
+	}
+
+	// return !!allowedRole.find(role => role.includes(user.role.name))
 }
