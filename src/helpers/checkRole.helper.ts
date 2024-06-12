71ed24c569caf56a2fb76dcc808af65056845f9f
BolotnikovMS <bolotnikovms@yandex.ru>
2024-06-12 14:29:41 +0500

    refactor: Рефакторинг
    Улучшена типизация, описание параметров фун-ий

--- src/helpers/checkRole.helper.ts
+++ src/helpers/checkRole.helper.ts
@@ -1,28 +1,21 @@
+import { ERoles } from '../enums/roles.enum'
 import { IUser } from '../interfaces'
-
-export enum ERoles {
-	Admin = 'Admin',
-	Moderator = 'Moderator',
-	User = 'User'
-}
 
 /**
  * Description
- * @param {IUser|null} Авторизованный пользователь.
- * @param {ERoles[]} Массив ролей которым доступны действия.
- * @param {boolean=false} Флаг, который указывает на то, что автор записи может выполнять действие над записью.
- * @param {unknown} Текущая запись.
- * @returns {any} Возвращает true или false
+ * @param {IUser|null} user - Авторизованный пользователь.
+ * @param {ERoles[]} allowedRoles - Массив ролей которым доступны действия.
+ * @param {boolean} isAuthor - Флаг, который указывает на то, что автор записи может выполнять действие над записью.
+ * @param {unknown} record - Текущая запись.
+ * @returns {boolean}  Возвращает true или false
  */
 
-export const checkRole = (user: IUser | null, allowedRole: ERoles[], author: boolean = false, record?: unknown): boolean => {
+export const checkRole = <T extends { userId: number}>(user: IUser | null, allowedRoles: ERoles[], isAuthor: boolean = false, record?: T): boolean => {
 	if (!user) return false
 
-	if (author && record && user.id === record.userId) {
-		return true
-	} else {
-		return !!allowedRole.find(role => role.includes(user.role.name))
-	}
+	if (isAuthor && record && user.id === record.userId) return true
 
-	// return !!allowedRole.find(role => role.includes(user.role.name))
+	if (allowedRoles.length === 0) return true
+	
+	return allowedRoles.some(role => role.includes(user.role.name))
 }
