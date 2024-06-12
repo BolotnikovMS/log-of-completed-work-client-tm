d10fcb75eb53f2200842cbb68a0a2f043057ac9a
BolotnikovMS <bolotnikovms@yandex.ru>
2024-06-12 19:21:16 +0500

    refactor: Рефакторинг профиля

--- src/views/auth/Profile.tsx
+++ src/views/auth/Profile.tsx
@@ -1,20 +1,36 @@
 import { type FC } from 'react'
 import { useAuthStore } from '../../store/auth'
+import styles from './profile.module.scss'
 
 const Profile: FC = () => {
 	const { authUser } = useAuthStore()
 
 	return (
 		<div>
-			<div className="profile__content">
-				<p>
-					Ф.И.О: { authUser?.fullName }
+			<div className={styles['profile-content']}>
+				<p className='text'>
+					Ф.И.О: 
+					<span className="sub-text">
+						{ authUser?.fullName }
+					</span>
 				</p>
-				<p>
-					Должность: { authUser?.role.name }
+				<p className='text'>
+					Должность: 
+					<span className="sub-text">
+						{ authUser?.position }
+					</span>
 				</p>
-				<p>
-					Email: { authUser?.email }
+				<p className='text'>
+					Роль в системе: 
+					<span className="sub-text">
+						{ authUser?.role.name }
+					</span>
+				</p>
+				<p className='text'>
+					Email: 
+					<span className="sub-text">
+						{ authUser?.email }
+					</span>
 				</p>
 			</div>
 		</div>
