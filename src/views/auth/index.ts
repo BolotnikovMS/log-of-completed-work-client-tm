fbeadff489645c9867ebaffbd094bf63c7d1dde5
BolotnikovMS <bolotnikovms@yandex.ru>
2024-06-12 19:22:23 +0500

    feat: Добавлены импорты компонентов

--- src/views/auth/index.ts
+++ src/views/auth/index.ts
@@ -2,5 +2,8 @@ import { lazy } from 'react'
 import { SignIn } from './SignIn'
 
 const Profile = lazy(() => import('./Profile'))
+const ProfileControl = lazy(() => import('./components/control/ProfileControl'))
+const ChangePasswordForm = lazy(() => import('./components/form/ChangePasswordForm'))
 
-export { Profile, SignIn }
+export { ChangePasswordForm, Profile, ProfileControl, SignIn }
+
