af9d04b304676e462898bb26c69c4e8b039daa1f
BolotnikovMS <bolotnikovms@yandex.ru>
2024-06-12 19:19:49 +0500

    refactor: Рефакторинг
    Использован метод join, также  приведение к строке

--- src/helpers/errorHandler.helper.ts
+++ src/helpers/errorHandler.helper.ts
@@ -1,13 +1,13 @@
 import { AxiosError, isAxiosError } from 'axios'
 
-export const errorHandler = (errors: Error | AxiosError<unknown>) => {
+export const errorHandler = (errors: Error | AxiosError<unknown>): string => {
 	let errorMessage: string = ''
 
 	if (isAxiosError(errors)) {
-		if (Array.isArray(errors.response?.data?.errors)) {
-			errors.response.data.errors.map((errData: { message: string, field: string })  => errorMessage += `${errData.message} `)
+		if (errors.response && Array.isArray(errors.response?.data?.errors)) {
+			errorMessage = errors.response.data.errors.map(({ message }: { message: string }) => message).join('; ')
 		} else if (errors.response?.data.message) {
-			errorMessage = errors.response?.data.message
+			errorMessage = errors.response?.data.message as string
 		} else {
 			errorMessage = errors.message
 		}
