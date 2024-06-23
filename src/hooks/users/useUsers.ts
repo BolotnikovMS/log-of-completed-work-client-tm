17501b946a8cf7c25769d9057988ad3c9baaf92b
BolotnikovMS <bolotnikovms@yandex.ru>
2024-06-23 18:13:06 +0500

    feat: Добавлены параметры для выборки

--- src/hooks/users/useUsers.ts
+++ src/hooks/users/useUsers.ts
@@ -1,10 +1,12 @@
 import { useQuery } from '@tanstack/react-query'
+import { IQueryParams } from '../../interfaces'
 import { UserService } from '../../services/user/user.service'
 
-export const useUsers = () => {
+export const useUsers = ({ active, cleanUser }: IQueryParams) => {
+	const isParams = active ? `active:${active}` : cleanUser ? `clean:${cleanUser}` : 'all'
   const { data, error, isError, isLoading, isFetching } = useQuery({
-    queryKey: ['users', 'all'],
-    queryFn: () => UserService.getUsers({}),
+    queryKey: ['users', isParams],
+    queryFn: () => UserService.getUsers({ active, cleanUser}),
     staleTime: 1000 * 10,
   })
 
