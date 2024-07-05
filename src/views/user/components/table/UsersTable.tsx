c297bd5467a7ccec31ca6c8db921322270aa43d8
BolotnikovMS <bolotnikovms@yandex.ru>
2024-07-05 16:19:20 +0500

    feat: Добавлена форма для изменения статуса аккаунта

--- src/views/user/components/table/UsersTable.tsx
+++ src/views/user/components/table/UsersTable.tsx
@@ -1,71 +1,74 @@
-import { type FC, useMemo, useState } from 'react'
-import { BasicTable, Button, ChangePasswordForm, Error, InfoMessage, Loader, Modal } from '../../../../components'
-
 import { ColumnDef } from '@tanstack/react-table'
 import { KeyRound } from 'lucide-react'
+import { type FC, useMemo, useState } from 'react'
+import { ChangeStatusAccountForm } from '..'
+import { BasicTable, Button, ChangePasswordForm, Error, InfoMessage, Loader, Modal } from '../../../../components'
 import { useModal, useUsers } from '../../../../hooks'
 import { IUser } from '../../../../interfaces'
 
 const UsersTable: FC = () => {
-	const { data, error, isError, isLoading } = useUsers({})
-	const { isModal, toggleModal } = useModal()
-	const [currentUser, setCurrentUser] = useState<IUser | undefined>(undefined)
-	const columns = useMemo<ColumnDef<IUser>[]>(() => [
-		{
-			header: 'Ф.И.О.',
-			accessorKey: 'shortName',
-		},
-		{
-			header: 'Должность',
-			accessorKey: 'position',
-		},
-		{
-			header: 'УЗ активна',
-			accessorFn: row => row.active ? '✅' : '⛔️'
-		},
-		{
-			header: 'Username',
-			accessorKey: 'username',
-		},
-		{
-			header: 'email',
-			accessorKey: 'email',
-		},
-		{
-			header: 'Роль',
-			accessorFn: row => row.role.name
-		},
-		{
-			header: '⚙️',
-			enableSorting: false,
-			accessorKey: 'setting',
-			cell: ({row}) =>  (
-				<div className='table-cell-row'>
-					<Button title='Изменить пароль' onClick={() => { toggleModal(), setCurrentUser(row.original) }}>
-						<KeyRound />
-					</Button>
-				</div>
-			)
-		}
-	], [])
+  const { data, error, isError, isLoading } = useUsers({})
+  const { isModal, toggleModal } = useModal()
+  const [currentUser, setCurrentUser] = useState<IUser | undefined>(undefined)
+  const columns = useMemo<ColumnDef<IUser>[]>(() => [
+    {
+      header: 'Ф.И.О.',
+      accessorKey: 'shortName',
+    },
+    {
+      header: 'Должность',
+      accessorKey: 'position',
+    },
+    {
+      header: 'УЗ активна',
+      accessorKey: 'active',
+      cell: ({ row }) => (
+        <ChangeStatusAccountForm active={row.original.active} userId={row.original.id} />
+      )
+    },
+    {
+      header: 'Username',
+      accessorKey: 'username',
+    },
+    {
+      header: 'email',
+      accessorKey: 'email',
+    },
+    {
+      header: 'Роль',
+      accessorFn: row => row.role.name
+    },
+    {
+      header: '⚙️',
+      enableSorting: false,
+      accessorKey: 'setting',
+      cell: ({ row }) => (
+        <div className='table-cell-row'>
+          <Button title='Изменить пароль' onClick={() => { toggleModal(), setCurrentUser(row.original) }}>
+            <KeyRound />
+          </Button>
+        </div>
+      )
+    }
+  ], [])
 
-	if (isError && error) return <Error error={error}/>
+  if (isError && error) return <Error error={error} />
 
-	if (isLoading) return <Loader />
+  if (isLoading) return <Loader />
 
-	if (!data?.data) return <InfoMessage text='Пока нет данных для отображения!' />
+  if (!data?.data) return <InfoMessage text='Пока нет данных для отображения!' />
 
-	return (
-		<>
-			<BasicTable data={data.data} columns={columns} search />
-			<Modal 
-				visible={isModal} 
-				title='Форма сброса пароля' 
-				content={<ChangePasswordForm toggleModal={toggleModal} isResetPassword user={currentUser} />} 
-				onToggle={toggleModal}
-			/>
-		</>
-	)
+  return (
+    <>
+      <BasicTable data={data.data} columns={columns} search />
+      <Modal
+        visible={isModal}
+        title='Форма сброса пароля'
+        content={<ChangePasswordForm toggleModal={toggleModal} isResetPassword user={currentUser} />}
+        onToggle={toggleModal}
+      />
+    </>
+  )
 }
 
 export default UsersTable
