a2e093e362191a8255b56f49b9523b13880ec74f
BolotnikovMS <bolotnikovms@yandex.ru>
2024-07-05 16:18:26 +0500

    feat: Добавлен импорт

--- src/views/user/components/index.ts
+++ src/views/user/components/index.ts
@@ -3,5 +3,6 @@ import { lazy } from 'react'
 const UserForm = lazy(() => import('./form/UserForm'))
 const UserControl = lazy(() => import('./control/UserControl'))
 const UsersTable = lazy(() => import('./table/UsersTable'))
+const ChangeStatusAccountForm = lazy(() => import('./form/blockUser/ChangeStatusAccountForm'))
 
-export { UserControl, UserForm, UsersTable }
+export { UserControl, UserForm, UsersTable, ChangeStatusAccountForm }
