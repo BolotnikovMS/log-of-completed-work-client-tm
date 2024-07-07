3b0c13235bf76e9bcef94d48018d0beefea03946
BolotnikovMS <bolotnikovms@yandex.ru>
2024-07-07 19:15:46 +0500

    feat: Добавлены параметры

--- src/hooks/distrcts/useDistrictSubstations.ts
+++ src/hooks/distrcts/useDistrictSubstations.ts
@@ -1,15 +1,15 @@
 import { useQuery } from '@tanstack/react-query'
+import { IQueryParams } from '../../interfaces'
 import { DistrictService } from '../../services/district/district.service'
 
-interface IPropsDistrictSubstation {
+interface IPropsDistrictSubstation extends IQueryParams {
 	id: string | undefined
-	search?: string
 }
 
-export const useDistrictSubstations = ({ id, search }: IPropsDistrictSubstation) => {
+export const useDistrictSubstations = ({ id, search, sort, order }: IPropsDistrictSubstation ) => {
   const { data, error, isError, isLoading } = useQuery({
-    queryKey: ['district-substations', id, search],
-    queryFn: () => DistrictService.getSubstationsRelatedDistrict(id || '', { search }),
+    queryKey: ['district-substations', id, search, sort, order],
+    queryFn: () => DistrictService.getSubstationsRelatedDistrict(id || '', { search, sort, order }),
     staleTime: 1000 * 10,
     enabled: !!id,
   })
