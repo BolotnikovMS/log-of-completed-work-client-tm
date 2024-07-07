da5083240baa570a7dab808f5d7fd34dc1ce452d
BolotnikovMS <bolotnikovms@yandex.ru>
2024-07-07 19:16:47 +0500

    style: Поправены отступы

--- src/views/substations/components/forms/uploadFile/typeFileOptions.ts
+++ src/views/substations/components/forms/uploadFile/typeFileOptions.ts
@@ -1,7 +1,7 @@
 import { EUploadFileType } from '../../../../../enums/upload-file-type.enum'
 
 export const typeFileOptions = [
-	{ value: EUploadFileType.photoPs, label: 'Фото объекта' },
-	{ value: EUploadFileType.backup, label: 'Бэкап оборудования' },
-	{ value: EUploadFileType.otherFiles, label: 'Другие файлы' },
+  { value: EUploadFileType.photoPs, label: 'Фото объекта' },
+  { value: EUploadFileType.backup, label: 'Бэкап оборудования' },
+  { value: EUploadFileType.otherFiles, label: 'Другие файлы' },
 ]
