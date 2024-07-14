ea19cca31730dfe684e500e64a474b392f0368b2
BolotnikovMS <bolotnikovms@yandex.ru>
2024-07-14 16:53:23 +0500

    refacor: Переработано добавление/обновления записи

--- src/views/channelType/components/form/ChannelTypeForm.tsx
+++ src/views/channelType/components/form/ChannelTypeForm.tsx
@@ -1,14 +1,11 @@
-import { useMutation, useQueryClient } from '@tanstack/react-query'
 import { SubmitHandler, useForm } from 'react-hook-form'
 import { Button, CustomInput, Error, Group, Loader } from '../../../../components'
 import { IChannelTypeFields, IPropsChannelTypeForm } from './channelTypeForm.interface'
 
 import { yupResolver } from '@hookform/resolvers/yup'
-import { AxiosError, isAxiosError } from 'axios'
 import { type FC } from 'react'
-import { toast } from 'react-toastify'
-import { ChannelTypeService } from '../../../../services/channel-type/channel-type.service'
-import { TChannelTypeData } from '../../../../services/channel-type/channel-type.type'
+import { useCreateChannelType } from '../../../../hooks/channel-types/useCreateChannelType'
+import { useUpdateChannelType } from '../../../../hooks/channel-types/useUpdateChannelType'
 import { validationSchema } from './channelType.validation'
 
 const ChannelTypeForm: FC<IPropsChannelTypeForm> = ({ channelType, isEdited, setIsEdited, toggleModal }) => {
@@ -19,65 +16,49 @@ const ChannelTypeForm: FC<IPropsChannelTypeForm> = ({ channelType, isEdited, set
       name: channelType?.name
     }
   })
-  const queryClient = useQueryClient()
-  const { mutateAsync, isError: isErrorMutate, error: errorMutate, isPending } = useMutation({
-    mutationFn: isEdited ? (data: TChannelTypeData) => ChannelTypeService.updateChannelType({id: channelType!.id, data}) : (data: TChannelTypeData) => ChannelTypeService.create(data),
-    onSuccess: async () => {
-      await queryClient.invalidateQueries({queryKey: ['channelTypes']})
-
-			if (channelType !== undefined && channelType !== null && setIsEdited) {
-				setIsEdited(false)
-				toast.success('Запись успешно обновлена!')
-			} else {
-				toast.success('Запись успешно добавлена!')
-			}
-			reset()
-			toggleModal()
-    },
-		onError: (errors) => {
-			if(isAxiosError(errors)) {
-				if (Array.isArray(errors.response?.data)) {
-					errors.response?.data.map((errData: AxiosError) => {
-						toast.error(errData.message)
-					})
-				}
-			}
-		}
-  })
-
-  const submit: SubmitHandler<IChannelTypeFields> = data => {
-    mutateAsync(data)
+	const { mutateAsync: createMutate, isError: isErrorCreate, error: errorCreate, isPending } = useCreateChannelType()
+	const { mutateAsync: updateMutate, isError: isErrorUpdate, error: errorUpdate } = useUpdateChannelType()
+  const submitCreate: SubmitHandler<IChannelTypeFields> = data => {
+		createMutate(data)
+		reset()
+		toggleModal()
   }
+	const submitUpdate: SubmitHandler<IChannelTypeFields> = data => {
+		if (!channelType?.id) return null
+
+		updateMutate({id: channelType?.id, data})
+		reset()
+		toggleModal()
+
+		if (isEdited && setIsEdited) setIsEdited(false)
+	}
 
   return (
-    <>
-      <div className="work-log__form">
-      {(isErrorMutate) && <Error error={errorMutate} />}
-      {isPending ? 
-        (<Loader />)
-      : (
-        <form className="form form-col" onSubmit={handleSubmit(submit)}>
-          <div className="form__content form__content-w-55 form__content-mt">
-            <Group className='group-col group-str'>
-              <CustomInput
-                label='Наименование канала'
-                name='name'
-                register={register}
-                errorMessage={errors.name?.message}
+		<div className="work-log__form">
+			{(isErrorCreate) || (isErrorUpdate) && <Error error={errorCreate || errorUpdate} />}
+			{isPending ? 
+				(<Loader />) : (
+				<form className="form form-col" onSubmit={handleSubmit(isEdited ? submitUpdate : submitCreate)}>
+					<div className="form__content form__content-w-55 form__content-mt">
+						<Group className='group-col group-str'>
+							<CustomInput
+								label='Наименование канала'
+								name='name'
+								register={register}
+								errorMessage={errors.name?.message}
 								mandatory={true}
-                placeholder='Введите наименование...'
-              />
-            </Group>
-          </div>
-          <div className="form__btns">
-            <Button disabled={!isValid} classBtn='btn-bg_green'>
-              {isEdited ? 'Сохранить' : 'Добавить'}
-            </Button>
-          </div>
-        </form>
-      )}
-      </div>
-    </>
+								placeholder='Введите наименование...'
+							/>
+						</Group>
+					</div>
+					<div className="form__btns">
+						<Button disabled={!isValid} classBtn='btn-bg_green'>
+							{isEdited ? 'Сохранить' : 'Добавить'}
+						</Button>
+					</div>
+				</form>
+			)}
+		</div>
   )
 }
 
