c4053d0073eae4b3ce0d78f4bd4ad4e981356a20
BolotnikovMS <bolotnikovms@yandex.ru>
2024-07-14 16:52:04 +0500

    fix: Поправлена передача аргументов

--- src/hooks/channel-types/useUpdateChannelType.ts
+++ src/hooks/channel-types/useUpdateChannelType.ts
@@ -5,19 +5,18 @@ import { errorHandler } from '../../helpers'
 import { ChannelTypeService } from '../../services/channel-type/channel-type.service'
 import { TChannelTypeData } from '../../services/channel-type/channel-type.type'
 
-export const useUpdateChannelType = (channelTypeId: number, data: TChannelTypeData) => {
+export const useUpdateChannelType = () => {
   const queryClient = useQueryClient()
 
 	return useMutation({
-		mutationFn: () => ChannelTypeService.updateChannelType({id: channelTypeId, data}),
+		mutationFn: ({id, data}: {id: number, data: TChannelTypeData}) => ChannelTypeService.updateChannelType(id, data),
 		onSuccess: async () => {
 			await queryClient.invalidateQueries({queryKey: ['channelTypes']})
-
+			
 			toast.success('Запись успешно обновлена!')
 		},
 		onError: (errors) => {
 			toast.error(errorHandler(errors))
 		}
-
 	})
 }
