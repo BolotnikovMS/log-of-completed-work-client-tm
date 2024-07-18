e01e5f99da0edc5abb4e04ca7a07abe20271e1e5
BolotnikovMS <bolotnikovms@yandex.ru>
2024-07-18 20:53:21 +0500

    style: Поправлены отступы в коде

--- src/hooks/channel-types/useCreateChannelType.ts
+++ src/hooks/channel-types/useCreateChannelType.ts
@@ -1,22 +1,21 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query'
-
+import { toast } from 'react-toastify'
+import { errorHandler } from '../../helpers'
 import { ChannelTypeService } from '../../services/channel-type/channel-type.service'
 import { TChannelTypeData } from '../../services/channel-type/channel-type.type'
-import { errorHandler } from '../../helpers'
-import { toast } from 'react-toastify'
 
 export const useCreateChannelType = () => {
-	const queryClient = useQueryClient()
+  const queryClient = useQueryClient()
 
-	return useMutation({
-		mutationFn: (data: TChannelTypeData) => ChannelTypeService.create(data),
-		onSuccess: async () => {
-			await queryClient.invalidateQueries({queryKey: ['channelTypes']})
+  return useMutation({
+    mutationFn: (data: TChannelTypeData) => ChannelTypeService.create(data),
+    onSuccess: async () => {
+      await queryClient.invalidateQueries({ queryKey: ['channelTypes'] })
 
-			toast.success('Запись успешно добавлена!')
-		},
-		onError: (errors) => {
-			toast.error(errorHandler(errors))
-		}
-	})
+      toast.success('Запись успешно добавлена!')
+    },
+    onError: (errors) => {
+      toast.error(errorHandler(errors))
+    }
+  })
 }
--- src/hooks/channel-types/useUpdateChannelType.ts
+++ src/hooks/channel-types/useUpdateChannelType.ts
@@ -1,5 +1,4 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query'
-
 import { toast } from 'react-toastify'
 import { errorHandler } from '../../helpers'
 import { ChannelTypeService } from '../../services/channel-type/channel-type.service'
@@ -8,15 +7,15 @@ import { TChannelTypeData } from '../../services/channel-type/channel-type.type'
 export const useUpdateChannelType = () => {
   const queryClient = useQueryClient()
 
-	return useMutation({
-		mutationFn: ({id, data}: {id: number, data: TChannelTypeData}) => ChannelTypeService.updateChannelType(id, data),
-		onSuccess: async () => {
-			await queryClient.invalidateQueries({queryKey: ['channelTypes']})
-			
-			toast.success('Запись успешно обновлена!')
-		},
-		onError: (errors) => {
-			toast.error(errorHandler(errors))
-		}
-	})
+  return useMutation({
+    mutationFn: ({ id, data }: { id: number, data: TChannelTypeData }) => ChannelTypeService.updateChannelType(id, data),
+    onSuccess: async () => {
+      await queryClient.invalidateQueries({ queryKey: ['channelTypes'] })
+
+      toast.success('Запись успешно обновлена!')
+    },
+    onError: (errors) => {
+      toast.error(errorHandler(errors))
+    }
+  })
 }
