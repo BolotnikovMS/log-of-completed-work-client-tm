9361503f12f5963d24bdcbc4e992a5f48a7d1544
BolotnikovMS <bolotnikovms@yandex.ru>
2024-07-18 20:59:05 +0500

    refactor: Поправлен вывод ошибок

--- src/views/channelType/components/form/ChannelTypeForm.tsx
+++ src/views/channelType/components/form/ChannelTypeForm.tsx
@@ -3,7 +3,7 @@ import { Button, CustomInput, Error, Group, Loader } from '../../../../component
 import { IChannelTypeFields, IPropsChannelTypeForm } from './channelTypeForm.interface'
 
 import { yupResolver } from '@hookform/resolvers/yup'
-import { useState, type FC } from 'react'
+import { type FC } from 'react'
 import { useCreateChannelType, useUpdateChannelType } from '../../../../hooks'
 import { IPropsMutation } from '../../../../interfaces'
 import { validationSchema } from './channelType.validation'
@@ -11,59 +11,54 @@ import { validationSchema } from './channelType.validation'
 const ChannelTypeForm: FC<IPropsChannelTypeForm> = ({ channelType, isEdited, setIsEdited, toggleModal }) => {
   const { register, handleSubmit, formState: { errors, isValid }, reset } = useForm<IChannelTypeFields>({
     mode: 'onBlur',
-		resolver: yupResolver(validationSchema),
+    resolver: yupResolver(validationSchema),
     defaultValues: {
       name: channelType?.name
     }
   })
-	const [error, setError] = useState<Error | null>(null)
-	const { mutateAsync: createMutate, isError: isErrorCreate, isPending: isPendingCreate } = useCreateChannelType()
-	const { mutateAsync: updateMutate, isError: isErrorUpdate, isPending: isPendingUpdate } = useUpdateChannelType()
-	const handleMutation = async ({ data, mutateFn, id}: IPropsMutation<IChannelTypeFields>) => {
-    try {
-      await mutateFn(id ? { id, data } : data)
+  const { mutateAsync: createChannelType, isError: isErrorCreate, error: errorCreate, isPending: isPendingCreate } = useCreateChannelType()
+  const { mutateAsync: updateChannelType, isError: isErrorUpdate, error: errorUpdate, isPending: isPendingUpdate } = useUpdateChannelType()
+  const handleMutation = async ({ data, mutateFn, id }: IPropsMutation<IChannelTypeFields>) => {
+    await mutateFn(id ? { id, data } : data)
 
-      reset()
-      toggleModal()
-      if (isEdited && setIsEdited) setIsEdited(false)
-    } catch (error) {
-      setError(error as Error)
-    }
+    reset()
+    toggleModal()
+    if (isEdited && setIsEdited) setIsEdited(false)
   }
-  const submitCreate: SubmitHandler<IChannelTypeFields> = data => handleMutation({data, mutateFn: createMutate})
+  const submitCreate: SubmitHandler<IChannelTypeFields> = data => handleMutation({ data, mutateFn: createChannelType })
   const submitUpdate: SubmitHandler<IChannelTypeFields> = data => {
     if (!channelType?.id) return null
-		
-    handleMutation({data, mutateFn: updateMutate, id: channelType.id})
+
+    handleMutation({ data, mutateFn: updateChannelType, id: channelType.id })
   }
-	const errorMessage = ((isErrorCreate || isErrorUpdate) && error !== null) && <Error error={error} />
+  const errorMessage = (isErrorCreate || isErrorUpdate && errorCreate && errorUpdate !== null) && <Error error={errorCreate || errorUpdate} />
 
   return (
-		<div className="work-log__form">
-			{errorMessage}
-			{isPendingCreate || isPendingUpdate ? 
-				(<Loader />) : (
-				<form className="form form-col" onSubmit={handleSubmit(isEdited ? submitUpdate : submitCreate)}>
-					<div className="form__content form__content-w-55 form__content-mt">
-						<Group className='group-col group-str'>
-							<CustomInput
-								label='Наименование канала'
-								name='name'
-								register={register}
-								errorMessage={errors.name?.message}
-								mandatory={true}
-								placeholder='Введите наименование...'
-							/>
-						</Group>
-					</div>
-					<div className="form__btns">
-						<Button disabled={!isValid} classBtn='btn-bg_green'>
-							{isEdited ? 'Сохранить' : 'Добавить'}
-						</Button>
-					</div>
-				</form>
-			)}
-		</div>
+    <div className="work-log__form">
+      {errorMessage}
+      {isPendingCreate || isPendingUpdate ?
+        (<Loader />) : (
+          <form className="form form-col" onSubmit={handleSubmit(isEdited ? submitUpdate : submitCreate)}>
+            <div className="form__content form__content-w-55 form__content-mt">
+              <Group className='group-col group-str'>
+                <CustomInput
+                  label='Наименование канала'
+                  name='name'
+                  register={register}
+                  errorMessage={errors.name?.message}
+                  mandatory={true}
+                  placeholder='Введите наименование...'
+                />
+              </Group>
+            </div>
+            <div className="form__btns">
+              <Button disabled={!isValid} classBtn='btn-bg_green'>
+                {isEdited ? 'Сохранить' : 'Добавить'}
+              </Button>
+            </div>
+          </form>
+        )}
+    </div>
   )
 }
 
