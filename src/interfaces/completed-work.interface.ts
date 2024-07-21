26b11875c203efe9e308f272938b9a2cfb5ba453
BolotnikovMS <bolotnikovms@yandex.ru>
2024-07-21 18:32:52 +0500

    style: Поправлены отступы в коде

--- src/interfaces/completed-work.interface.ts
+++ src/interfaces/completed-work.interface.ts
@@ -1,14 +1,14 @@
 import { ISubstation, IUser } from '.'
 
 export interface ICompletedWork {
-	id: number
-	userId: number
-	description: string
-	shortText: string
-	note?: string | null
-	dateCompletion: string
-	createdAt: Date
-	substation?: Pick<ISubstation, 'id' | 'fullNameSubstation'>
-	work_producer?: Pick<IUser, 'id' | 'shortName'>
-	author?: Pick<IUser, 'id' | 'shortName'>
+  id: number
+  userId: number
+  description: string
+  shortText: string
+  note?: string | null
+  dateCompletion: string
+  createdAt: Date
+  substation?: Pick<ISubstation, 'id' | 'fullNameSubstation'>
+  work_producer?: Pick<IUser, 'id' | 'shortName'>
+  author?: Pick<IUser, 'id' | 'shortName'>
 }
