365c84173486ee6fffb2b1107a3e0390384b8f76
BolotnikovMS <bolotnikovms@yandex.ru>
2024-07-24 20:31:26 +0500

    feat: Добавлено условие для обработки ошибок валидации с сервера

--- src/helpers/errorHandler.helper.ts
+++ src/helpers/errorHandler.helper.ts
@@ -1,19 +1,21 @@
 import { AxiosError, isAxiosError } from 'axios'
 
 export const errorHandler = (errors: Error | AxiosError<unknown>): string => {
-	let errorMessage: string = ''
+  let errorMessage: string = ''
 
-	if (isAxiosError(errors)) {
-		if (errors.response && Array.isArray(errors.response?.data?.errors)) {
-			errorMessage = errors.response.data.errors.map(({ message }: { message: string }) => message).join('; ')
-		} else if (errors.response?.data.message) {
-			errorMessage = errors.response?.data.message as string
-		} else {
-			errorMessage = errors.message
-		}
-	} else {
-		errorMessage = errors.message
-	}
+  if (isAxiosError(errors)) {
+    if (errors.response && Array.isArray(errors.response?.data?.errors)) {
+      errorMessage = errors.response.data.errors.map(({ message }: { message: string }) => message).join('; ')
+    } else if (errors.response?.status === 422 && Array.isArray(errors.response.data)) {
+      errorMessage = errors.response.data.map(({ message }: { message: string }) => message).join('; ')
+    } else if (errors.response?.data.message) {
+      errorMessage = errors.response?.data.message as string
+    } else {
+      errorMessage = errors.message
+    }
+  } else {
+    errorMessage = errors.message
+  }
 
-	return errorMessage
+  return errorMessage
 }
