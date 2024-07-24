37c2f14a5a408912c90d79abaa98433a4c81ee21
BolotnikovMS <bolotnikovms@yandex.ru>
2024-07-24 20:38:07 +0500

    fix: Убрано поле active тк по дефолту true

--- src/views/user/components/form/user.validation.ts
+++ src/views/user/components/form/user.validation.ts
@@ -1,20 +1,16 @@
 import * as yup from 'yup'
-
 import { email, numberCheck, text20, text30 } from '../../../../validations/rules'
 
 export const validationSchema = yup.object().shape({
-	username: text30,
-	surname: text20,
-	name: text20,
-	patronymic: text20,
-	position: text30,
-	email: email,
-	password: yup
-		.string()
-		.required('Поле является обязательным!')
-		.min(6, 'Минимальная длина поля 6 символа!'),
-	roleId: numberCheck,
-	active: yup
-		.boolean()
-		.default(true)
+  username: text30,
+  surname: text20,
+  name: text20,
+  patronymic: text20,
+  position: text30,
+  email: email,
+  password: yup
+    .string()
+    .required('Поле является обязательным!')
+    .min(6, 'Минимальная длина поля 6 символа!'),
+  roleId: numberCheck,
 })
--- src/views/user/components/form/userForm.interface.ts
+++ src/views/user/components/form/userForm.interface.ts
@@ -1,20 +1,19 @@
 import { IUser } from '../../../../interfaces'
 
 export interface IUserFields {
-	active: boolean
-	roleId: number
-	username: string
-	name: string
-	surname: string
-	patronymic:string
-	position: string
-	email: string
-	password: string
+  roleId: number
+  username: string
+  name: string
+  surname: string
+  patronymic: string
+  position: string
+  email: string
+  password: string
 }
 
 export interface IPropsUserForm {
-	user?: IUser | null
-	isEdited?: boolean | null
-	toggleModal: () => void
-	setIsEdited?: (val: boolean) => void
+  user?: IUser | null
+  isEdited?: boolean | null
+  toggleModal: () => void
+  setIsEdited?: (val: boolean) => void
 }
