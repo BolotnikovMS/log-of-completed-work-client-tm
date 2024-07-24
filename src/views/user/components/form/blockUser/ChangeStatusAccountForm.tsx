e7ae9c650b2abd9a16dcdb87ac00727ff0586001
BolotnikovMS <bolotnikovms@yandex.ru>
2024-07-24 20:39:03 +0500

    fix: Добавлено подтверждение действия

--- src/views/user/components/form/blockUser/ChangeStatusAccountForm.tsx
+++ src/views/user/components/form/blockUser/ChangeStatusAccountForm.tsx
@@ -1,32 +1,43 @@
-import { useEffect, type FC } from 'react'
+import { type FC, useEffect, useState } from 'react'
 import { SubmitHandler, useForm } from 'react-hook-form'
 import { LoaderLine, Toggle } from '../../../../../components'
 import { useChangeStatusAccountMutation } from '../../../../../hooks'
+import styles from './change-status.module.scss'
 import { IPropChangeStatusAccountFields, IPropsChangeStatusAccountForm } from './changeStatus.interface'
 
 const ChangeStatusAccountForm: FC<IPropsChangeStatusAccountForm> = ({ active, userId }) => {
   const { register, setValue, handleSubmit } = useForm<IPropChangeStatusAccountFields>({
     mode: 'onChange',
   })
+  const [isActive, setIsActive] = useState(active)
 
   useEffect(() => {
     setValue('active', active)
-  }, [active, setValue])
+  }, [active, isActive, setValue])
 
   const { mutateAsync, isPending } = useChangeStatusAccountMutation(userId)
-  const submit: SubmitHandler<IPropChangeStatusAccountFields> = data => mutateAsync(data)
+  const submit: SubmitHandler<IPropChangeStatusAccountFields> = data => {
+    const answer = confirm('Подтвердтие изменение статуса УЗ.')
+
+    if (!answer) {
+      setIsActive(prevActive => !prevActive)
+
+      return
+    }
+
+    mutateAsync(data)
+  }
+
+  if (isPending) return <LoaderLine />
 
   return (
-    isPending ?
-      <LoaderLine />
-      :
-      <form onChange={handleSubmit(submit)}>
-        <Toggle
-          idToggle={userId.toString()}
-          defaultChecked={active}
-          {...register('active')}
-        />
-      </form >
+    <form className={styles.changeStatusForm} onChange={handleSubmit(submit)}>
+      <Toggle
+        idToggle={userId.toString()}
+        defaultChecked={isActive}
+        {...register('active')}
+      />
+    </form >
   )
 }
 
