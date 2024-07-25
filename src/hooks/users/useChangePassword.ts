a5330fa2833fcf198eea20ce0f1900f21dc8d45a
BolotnikovMS <bolotnikovms@yandex.ru>
2024-07-25 18:25:46 +0500

    feat: Хуки мутации

---
+++ src/hooks/users/useChangePassword.ts
@@ -0,0 +1,17 @@
+import { useMutation } from "@tanstack/react-query"
+import { IChangePassword } from "../../interfaces"
+import { AuthService } from "../../services/auth/auth.service"
+import { toast } from "react-toastify"
+import { errorHandler } from "../../helpers"
+
+export const useChangePassword = () => {
+  return useMutation({
+    mutationFn: (data: IChangePassword) => AuthService.changePassword(data),
+    onSuccess: (data) => {
+      toast.success(data.data)
+    },
+    onError: (errors) => {
+      toast.error(errorHandler(errors))
+    }
+  })
+}
---
+++ src/hooks/users/useResetPassword.ts
@@ -0,0 +1,17 @@
+import { useMutation } from "@tanstack/react-query"
+import { toast } from "react-toastify"
+import { errorHandler } from "../../helpers"
+import { IChangePassword } from "../../interfaces"
+import { UserService } from "../../services/user/user.service"
+
+export const useResetPassword = () => {
+  return useMutation({
+    mutationFn: ({ id, data }: { id: number, data: IChangePassword }) => UserService.resetPassword(id, data),
+    onSuccess: (data) => {
+      toast.success(data.data)
+    },
+    onError: (errors) => {
+      toast.error(errorHandler(errors))
+    }
+  })
+}
