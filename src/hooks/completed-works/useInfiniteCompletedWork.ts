6077feec9f4b9ea1c1621b320a9cea686844d3c2
BolotnikovMS <bolotnikovms@yandex.ru>
2024-07-28 19:46:21 +0500

    feat: Добавлены параметры для запроса

--- src/hooks/completed-works/useInfiniteCompletedWork.ts
+++ src/hooks/completed-works/useInfiniteCompletedWork.ts
@@ -1,26 +1,27 @@
 import { useInfiniteQuery } from '@tanstack/react-query'
+import { IQueryParams } from '../../interfaces'
 import { CompletedWorkService } from '../../services/completed-work/completed-work.service'
 
-export const useInfiniteCompletedWork = ({ limit, substation }: { limit: number, substation?: string | null } ) => {
-	const {
-		data,
+export const useInfiniteCompletedWork = ({ limit, substation, executor, dateStart, dateEnd }: IQueryParams) => {
+  const {
+    data,
     error,
     isError,
     fetchNextPage,
     hasNextPage,
     isFetching,
     isFetchingNextPage,
-	} = useInfiniteQuery({
-		queryKey: ['completedWork', 'infinity', substation],
-		queryFn: ({ pageParam = 1 }) => CompletedWorkService.getAll({page: pageParam, limit, substation}),
-		initialPageParam: 1,
+  } = useInfiniteQuery({
+    queryKey: ['completedWork', 'infinity', substation, executor, dateStart, dateEnd],
+    queryFn: ({ pageParam = 1 }) => CompletedWorkService.getAll({ page: pageParam, limit, substation, executor, dateStart, dateEnd }),
+    initialPageParam: 1,
     getNextPageParam: (lastPage, allPages) => {
       const nextPage = allPages.length + 1
-      const totalPages = Math.ceil(allPages[0].meta.total / limit)
+      const totalPages = Math.ceil(allPages[0].meta.total / limit!)
 
       return totalPages >= nextPage && lastPage.data.length !== 0 ? nextPage : undefined
     }
-	})
-	
-	return { data, error, isError, fetchNextPage, hasNextPage, isFetching, isFetchingNextPage }
+  })
+
+  return { data, error, isError, fetchNextPage, hasNextPage, isFetching, isFetchingNextPage }
 }
