b45860656ed3aeb44872d4e9b559666bb5ff3084
BolotnikovMS <bolotnikovms@yandex.ru>
2024-07-28 19:51:27 +0500

    feat: Добавлен вызов модалки с фильтрами

--- src/views/completedWork/components/control/CompletedWorkControl.tsx
+++ src/views/completedWork/components/control/CompletedWorkControl.tsx
@@ -1,28 +1,37 @@
-import { Plus } from 'lucide-react'
+import { Filter, Plus } from 'lucide-react'
 import { type FC } from 'react'
 import { CompletedWorkFilters, CompletedWorkForm } from '..'
 import { Button, Modal } from '../../../../components'
 import { useModal } from '../../../../hooks'
 
 const CompletedWorkControl: FC = () => {
-	const { isModal, toggleModal } = useModal()
+  const { isModal, toggleModal } = useModal()
+  const { isModal: isModalFilters, toggleModal: toggleModalFilters } = useModal()
 
-	return (
-		<div className="work-log__control control">
-			<div className="control__wrapper">
-				<Button classBtn='btn-bg_green' onClick={() => toggleModal()}>
-					<Plus />
-				</Button>
-				<Modal
-					visible={isModal}
-					title='Форма добавления выполненной работы'
-					content={<CompletedWorkForm toggleModal={toggleModal} />}
-					onToggle={toggleModal}
-				/>
-				<CompletedWorkFilters />
-			</div>
-		</div>
-	)
+  return (
+    <div className="work-log__control control">
+      <div className="control__wrapper">
+        <Button classBtn='btn-bg_green' onClick={() => toggleModal()}>
+          <Plus />
+        </Button>
+        <Button classBtn='btn-bg_trnt' onClick={() => toggleModalFilters()}>
+          <Filter />
+        </Button>
+        <Modal
+          visible={isModal}
+          title='Форма добавления выполненной работы'
+          content={<CompletedWorkForm toggleModal={toggleModal} />}
+          onToggle={toggleModal}
+        />
+        <Modal
+          visible={isModalFilters}
+          title='Фильтры'
+          content={<CompletedWorkFilters toggleModal={toggleModalFilters} />}
+          onToggle={toggleModalFilters}
+        />
+      </div>
+    </div>
+  )
 }
 
 export default CompletedWorkControl
