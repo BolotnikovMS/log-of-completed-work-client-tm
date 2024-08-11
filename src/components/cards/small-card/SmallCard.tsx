1978f5154d9eca2e09249940fb8a58265ba79a9a
BolotnikovMS <bolotnikovms@yandex.ru>
2024-08-11 12:33:40 +0500

    fix: Добавлена проверка, отступы
    Добавлдена проверка на передаваймый control. Поправлены отступы в коде

--- src/components/cards/small-card/SmallCard.tsx
+++ src/components/cards/small-card/SmallCard.tsx
@@ -8,21 +8,23 @@ import { IPropsSmallCard } from './smallCard.interface'
 export const SmallCard: React.FC<IPropsSmallCard> = ({ childrenContent, childrenFooter, childrenControl, className, cardText, path }) => {
   return (
     <div className={cx('card', className)}>
-      {path && <Link to={path} className='card__link'/>}
+      {path && <Link to={path} className='card__link' />}
       <div className="card__content">
-				<div className="card__body">
-					{childrenContent}
-					<p className='card__text'>{cardText}</p>
-				</div>
-				{childrenFooter && 
-					(<div className="card__footer">
-						<p>{childrenFooter}</p>
-					</div>)
-				}
+        <div className="card__body">
+          {childrenContent}
+          <p className='card__text'>{cardText}</p>
+        </div>
+        {childrenFooter &&
+          (<div className="card__footer">
+            <p>{childrenFooter}</p>
+          </div>)
+        }
       </div>
-      <div className="card__control card__control-row">
-        {childrenControl}
-      </div>
+      {childrenControl && (
+        <div className="card__control card__control-row">
+          {childrenControl}
+        </div>
+      )}
     </div>
   )
 }
