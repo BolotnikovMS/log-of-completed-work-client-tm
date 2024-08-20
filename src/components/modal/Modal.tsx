b9c13dd5c7e3c7cc3554a24fc6c395c5a08c9959
BolotnikovMS <bolotnikovms@yandex.ru>
2024-08-20 20:34:30 +0500

    style: Обновлены стили

--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -1,27 +1,26 @@
-import './modal.scss'
-
 import { X } from 'lucide-react'
-import { type FC } from 'react'
+import { FC } from 'react'
 import { createPortal } from 'react-dom'
 import { IPropsModal } from './modal.interface'
+import './modal.scss'
 
 export const Modal: FC<IPropsModal> = ({ visible, title, content, footer, onToggle }) => {
   if (!visible) return null
 
   return createPortal(
-    <div className='modal' onClick={onToggle}>
-      <div className="modal__dialog" onClick={e => e.stopPropagation()}>
-        <div className="modal__header">
-          <h3 className="modal__title">{title}</h3>
-          <span className="modal__close" onClick={onToggle}>
-						<X />
-					</span>
+    <div className='mModal' onClick={onToggle}>
+      <div className="mModal__dialog" onClick={e => e.stopPropagation()}>
+        <div className="mModal__header">
+          <h3 className="mModal__title">{title}</h3>
+          <span className="mModal__close" onClick={onToggle}>
+            <X />
+          </span>
         </div>
-        <div className="modal__body">
-          <div className="modal__content">{content}</div>
+        <div className="overflow-y-auto">
+          <div className="p-1">{content}</div>
         </div>
         {footer && (
-          <div className="modal__footer">{footer}</div>
+          <div className="mModal__footer">{footer}</div>
         )}
       </div>
     </div>,
