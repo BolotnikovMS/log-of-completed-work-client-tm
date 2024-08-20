ff80c51e5b5a658c825617a399e10ea46bf2a53c
BolotnikovMS <bolotnikovms@yandex.ru>
2024-08-20 20:30:37 +0500

    feat: Добавлены анимации

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,32 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        appear: {
+          '0%': { opacity: 0 },
+          '100%': { opacity: 1 },
+        },
+        slideIn: {
+          'from': {
+            transform: { translateY: '-150px' }
+          },
+          'to': {
+            transform: { translateY: '0' },
+          }
+        }
+      },
+      animation: {
+        appear: 'appear 0.5s',
+        slideIn: 'slideIn 0.2s'
+      },
+      top: {
+        'custom': '30px',
+      },
+      right: {
+        '25p': '25px'
+      }
+    },
   },
   daisyui: {
     themes: ['light']
