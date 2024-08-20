0cf0b9486545343e22b8e934414301fc354d5816
BolotnikovMS <bolotnikovms@yandex.ru>
2024-08-20 20:39:57 +0500

    style: Обновлены стили

--- src/components/tab/Tab.tsx
+++ src/components/tab/Tab.tsx
@@ -2,32 +2,33 @@ import cx from 'classnames'
 import { useState, type FC } from 'react'
 import Error from '../error/Error'
 import { IPropsTab } from './tab.interface'
-import styles from './tab.module.scss'
+import './tab.scss'
 
-const Tab: FC<IPropsTab> = ({tabs}) => {
-	const [toggleState, setToggleState] = useState<number>(0)
-	const toggleTab = (index: number) => setToggleState(index)
+const Tab: FC<IPropsTab> = ({ tabs }) => {
+  const [toggleState, setToggleState] = useState<number>(0)
+  const toggleTab = (index: number) => setToggleState(index)
 
-	if (!tabs.length) return <Error error={{message: 'Данные для табов не переданы!!'} as Error} />
+  if (!tabs.length) return <Error error={{ message: 'Данные для табов не переданы!!' } as Error} />
 
-	return (
-		<div className={styles.wrapper}>
-			<ul className={styles.tabs}>
-				{tabs.map((tab, index) => (
-					<li key={tab.id} className={cx(styles.tab, toggleState === index && styles.active)} onClick={() => toggleTab(index)}>
-						<span className='text'>{tab.label}</span>
-					</li>
-				))}
+  return (
+    <div>
+      <ul className='mTab__menu'>
+        {tabs.map((tab, index) => (
+          <li key={tab.id} className={cx('mTab__menu-item', toggleState === index && '!border-black')} onClick={() => toggleTab(index)} aria-label={tab.label}>
+            {tab.icon && tab.icon}
+            <span className='text'>{tab.label}</span>
+          </li>
+        ))}
       </ul>
-      <div className="content-container">
-				{tabs.map((tab, index) => (
-					<div key={tab.id} className={cx(styles['tab-content'], toggleState === index && styles.active)}>
-						{tab.content}
-					</div>
-				))}
+      <div className="mTab-container">
+        {tabs.map((tab, index) => (
+          <div key={tab.id} className={cx('py-2', toggleState === index ? 'block' : 'hidden')}>
+            {tab.content}
+          </div>
+        ))}
       </div>
     </div>
-	)
+  )
 }
 
 export default Tab
