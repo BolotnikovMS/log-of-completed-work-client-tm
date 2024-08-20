e0805ea3deb621eb98be9f81a22ee40abfc0de78
BolotnikovMS <bolotnikovms@yandex.ru>
2024-08-20 20:33:01 +0500

    style: Обновлены стили

--- src/components/input/Input.tsx
+++ src/components/input/Input.tsx
@@ -1,14 +1,14 @@
 import cx from 'classnames'
 import { forwardRef, type FC } from 'react'
-import { joinClasses } from '../../helpers/joinClasses.helper'
 import { IPropsInput } from './input.interface'
-import styles from './input.module.scss'
+import './input.scss'
 
 const Input: FC<IPropsInput> = forwardRef<HTMLInputElement, IPropsInput>(({ className, error, iconLeft, iconRight, ...attributes }, ref) => {
   return (
-    <div className={cx(styles['input-wrapper'], joinClasses(styles, className), error && styles['input-wrapper-error'],  attributes.type === 'checkbox' && styles['input-wrapper-no_border'])}>
+    <div className={cx('input-wrapper', error && 'border-red-500', attributes.type === 'checkbox' &&
+      '!border-none')}>
       {iconLeft}
-      <input className={cx(styles.input)} ref={ref} {...attributes} />
+      <input className='w-full' ref={ref} {...attributes} />
       {iconRight}
     </div>
   )
