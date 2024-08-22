e74a71cba5c5a94cab4280bfdfe3b8529b02c0fd
BolotnikovMS <bolotnikovms@yandex.ru>
2024-08-22 20:29:57 +0500

    style: Пересмотрены стили

--- src/components/forms/errorMessage/ValidationMessage.tsx
+++ src/components/forms/errorMessage/ValidationMessage.tsx
@@ -1,14 +1,12 @@
-import styles from './validationMessage.module.scss'
-
 import cx from 'classnames'
 import { type FC } from 'react'
-import { joinClasses } from '../../../helpers/joinClasses.helper'
 import { IPropsValidationMessage } from './ValidationMessage.interface'
+import './validationMessage.scss'
 
 const ValidationMessage: FC<IPropsValidationMessage> = ({ children, className }) => {
   return (
-    <div className={cx(styles.error, joinClasses(styles, className))}>
-      { children }
+    <div className={cx('validation-error', className)}>
+      {children}
     </div>
   )
 }
