17ffd1e0c33445190717e5a5989c4d6d2466890a
BolotnikovMS <bolotnikovms@yandex.ru>
2024-08-22 20:27:34 +0500

    fix: Rename

--- src/components/cards/card/card.interface.ts
+++ src/components/cards/card/card.interface.ts
@@ -2,7 +2,7 @@ import { type ReactNode } from 'react'
 
 export interface IPropsCard {
 	childrenHeader?: ReactNode
-	childrenBody?: ReactNode
+	childrenContent?: ReactNode
 	childrenFooter?: ReactNode
 	childrenControl?: ReactNode
 	className?: string
