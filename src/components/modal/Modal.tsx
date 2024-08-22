b5172fbad760d857d5094e64fd7337a8a25892c2
BolotnikovMS <bolotnikovms@yandex.ru>
2024-08-22 20:33:51 +0500

    style: Добавлены стили

--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -1,23 +1,24 @@
+import cx from 'classnames'
 import { X } from 'lucide-react'
 import { FC } from 'react'
 import { createPortal } from 'react-dom'
 import { IPropsModal } from './modal.interface'
 import './modal.scss'
 
-export const Modal: FC<IPropsModal> = ({ visible, title, content, footer, onToggle }) => {
+export const Modal: FC<IPropsModal> = ({ visible, title, content, footer, classDialog, onToggle }) => {
   if (!visible) return null
 
   return createPortal(
     <div className='mModal' onClick={onToggle}>
-      <div className="mModal__dialog" onClick={e => e.stopPropagation()}>
+      <div className={cx('mModal__dialog min-w-[650px]', classDialog)} onClick={e => e.stopPropagation()}>
         <div className="mModal__header">
           <h3 className="mModal__title">{title}</h3>
           <span className="mModal__close" onClick={onToggle}>
             <X />
           </span>
         </div>
-        <div className="overflow-y-auto">
-          <div className="p-1">{content}</div>
+        <div className="overflow-y-auto p-4">
+          {content}
         </div>
         {footer && (
           <div className="mModal__footer">{footer}</div>
