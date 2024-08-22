c71e4b5713d07b0e1bce399efab434391497a82b
BolotnikovMS <bolotnikovms@yandex.ru>
2024-08-22 20:28:29 +0500

    refactor: Добавлены стили, логика
    Пересмотрены стили карточки. Добавлена логика для рендера.

--- src/components/cards/card/Card.tsx
+++ src/components/cards/card/Card.tsx
@@ -1,26 +1,34 @@
 import cx from 'classnames'
 import { type FC } from 'react'
 import { Link } from 'react-router-dom'
-import './../card.css'
+import './../card.scss'
 import { IPropsCard } from './card.interface'
 
-export const Card: FC<IPropsCard> = ({ childrenHeader, childrenBody, childrenFooter, childrenControl, className, path }) => {
+export const Card: FC<IPropsCard> = ({ childrenHeader, childrenContent, childrenFooter, childrenControl, className, path }) => {
   return (
-    <div className={cx('card', className)}>
+    <div className={cx('mCard !w-full', className)}>
       {path && <Link to={path} className='card__link' />}
-      <div className="card__content">
-        <div className="card__header">
-          {childrenHeader}
-        </div>
-        <div className="card__body">
-          {childrenBody}
-        </div>
-        <div className="card__footer">
-          {childrenFooter}
-        </div>
-      </div>
-      <div className="card__control card__control-row">
-        {childrenControl}
+      <div className="mCard__body">
+        {childrenHeader && (
+          <div className="card__header">
+            {childrenHeader}
+          </div>
+        )}
+        {childrenContent && (
+          <div className="mCard__content">
+            {childrenContent}
+          </div>
+        )}
+        {childrenFooter && (
+          <div className="card__footer">
+            {childrenFooter}
+          </div>
+        )}
+        {childrenControl && (
+          <div className="mCard__actions">
+            {childrenControl}
+          </div>
+        )}
       </div>
     </div>
   )
