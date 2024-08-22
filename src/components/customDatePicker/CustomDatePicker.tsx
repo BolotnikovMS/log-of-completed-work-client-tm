80086060244f76e7263efbbf43bb6c32e0af97f9
BolotnikovMS <bolotnikovms@yandex.ru>
2024-08-22 20:29:12 +0500

    style: Пересмотрены стили

--- src/components/customDatePicker/CustomDatePicker.tsx
+++ src/components/customDatePicker/CustomDatePicker.tsx
@@ -3,23 +3,24 @@ import { forwardRef, type FC } from 'react'
 import { default as DatePicker } from 'react-datepicker'
 import { ValidationMessage } from '..'
 import { IPropsCustomDatePicker } from './customDatePicker.interface'
-import styles from './customDatePicker.module.scss'
+import './customDatePicker.scss'
 
-const CustomDatePicker: FC<IPropsCustomDatePicker> = forwardRef<unknown, IPropsCustomDatePicker>(({className, errorMessage, iconLeft, iconRight, ...props}, ref) => {
-	return (
-		<>
-			<div className={cx(styles['date-picker-wrapper'], className, errorMessage && styles['date-picker-wrapper-error'])}>
-				{iconLeft}
-				<DatePicker
-					className={styles['date-picker-input']}
-					ref={ref}
-					{...props}
-				/>
-				{iconRight}
-				{errorMessage && <ValidationMessage className='error-bottom-23' children={errorMessage} />}
-			</div>
-		</>
-	)
+const CustomDatePicker: FC<IPropsCustomDatePicker> = forwardRef<unknown, IPropsCustomDatePicker>(({ className, errorMessage, iconLeft, iconRight, ...props }, ref) => {
+  return (
+    <div className={cx('date-picker-wrapper',
+      className,
+      errorMessage && '!border-red-500')}
+    >
+      {iconLeft}
+      <DatePicker
+        className='w-full'
+        ref={ref}
+        {...props}
+      />
+      {iconRight}
+      {errorMessage && <ValidationMessage className='bottom-[-32px]' children={errorMessage} />}
+    </div>
+  )
 })
 
 export default CustomDatePicker
