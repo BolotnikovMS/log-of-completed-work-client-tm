7db89fe517df49bca9c8ef0919f26d9da9589fcc
BolotnikovMS <bolotnikovms@yandex.ru>
2024-08-22 20:39:24 +0500

    style: Пересмотрены стили

--- src/views/completedWork/components/info/CompletedWorkInfo.tsx
+++ src/views/completedWork/components/info/CompletedWorkInfo.tsx
@@ -1,56 +1,55 @@
-import cx from 'classnames'
 import moment from 'moment'
 import { type FC } from 'react'
 import { Group } from '../../../../components'
 import { IPropsCompletedWorkInfo } from './CompletedWorkInfo.interface'
-import styles from './completedWork.module.scss'
+import './completedWork.scss'
 
 const CompletedWorkInfo: FC<IPropsCompletedWorkInfo> = ({ completedWork }) => {
   return (
-    <div className={styles.workInfo}>
-      <div className={styles.workInfoContent}>
+    <div className='work-info'>
+      <div className='work-info__content'>
         <Group>
-          <p className='text'>Объект:
-            <span className='sub-text'>
+          <p className='work-info__text'>Объект:
+            <span className='text-content'>
               {completedWork?.substation?.fullNameSubstation}
             </span>
           </p>
         </Group>
         <Group>
-          <p className='text'>Дата проведения работ:
-            <span className='sub-text'>
+          <p className='work-info__text'>Дата проведения работ:
+            <span className='text-content'>
               {moment(completedWork.dateCompletion, 'YYYY-MM-DD').format('DD.MM.yyyy')}
             </span>
           </p>
         </Group>
         <Group>
-          <p className='text'>
+          <p className='work-info__text'>
             Производитель работ:
-            <span className='sub-text'>
+            <span className='text-content'>
               {completedWork?.work_producer?.shortName}
             </span>
           </p>
         </Group>
         <Group>
-          <p className='text'>
+          <p className='work-info__text'>
             Автор записи:
-            <span className='sub-text'>
+            <span className='text-content'>
               {completedWork?.author?.shortName}
             </span>
           </p>
         </Group>
       </div>
-      <div className={styles.workInfoContent}>
-        <div className={styles.workInfoTitles}>
-          <h2 className="title-1">Описание выполненной работы</h2>
+      <div className='work-info__content'>
+        <div className='work-info__titles'>
+          <h2 className="text-title font-bold">Описание выполненной работы</h2>
         </div>
-        <p className='text text-mt-5'>{completedWork.description}</p>
+        <p className='text-content'>{completedWork.description}</p>
         {completedWork.note && (
           <>
-            <div className={cx(styles.workInfoTitles, styles['workInfoTitles-mt-18'])}>
-              <h2 className="title-1">Примечание</h2>
+            <div className='work-info__titles mt-5'>
+              <h2 className="text-title font-bold">Примечание</h2>
             </div>
-            <p className='text text-mt-5'>{completedWork.note}</p>
+            <p className='text-content'>{completedWork.note}</p>
           </>
         )}
       </div>
