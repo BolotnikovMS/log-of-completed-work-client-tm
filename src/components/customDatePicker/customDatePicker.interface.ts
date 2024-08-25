a5c365a67187ecb4c8528010b93729aac5af07e4
BolotnikovMS <bolotnikovms@yandex.ru>
2024-08-25 19:08:00 +0500

    fix: Свойство register сделано не обязательным

--- src/components/customDatePicker/customDatePicker.interface.ts
+++ src/components/customDatePicker/customDatePicker.interface.ts
@@ -2,11 +2,11 @@ import { ReactNode } from 'react'
 import { ReactDatePickerProps } from 'react-datepicker'
 
 export interface IPropsCustomDatePicker extends ReactDatePickerProps {
-	className?: string 
-	iconLeft?: ReactNode
-	iconRight?: ReactNode
-	// eslint-disable-next-line @typescript-eslint/no-explicit-any
-	errorMessage?: string
-	register: any
-	validation?: { [key: string]: unknown }
+  className?: string
+  iconLeft?: ReactNode
+  iconRight?: ReactNode
+  errorMessage?: string
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  register?: any
+  validation?: { [key: string]: unknown }
 }
