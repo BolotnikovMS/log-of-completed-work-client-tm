9e31e1c4d091d92697132932b394083443145032
BolotnikovMS <bolotnikovms@yandex.ru>
2024-08-27 22:08:57 +0500

    refactor: Стили перенесены в компонент

--- src/components/loadMore/LoadMore.tsx
+++ src/components/loadMore/LoadMore.tsx
@@ -1,11 +1,10 @@
 import { type FC } from 'react'
 import { Button } from '..'
-import './load-more.css'
 import { IPropsLoadMore } from './loadMore.interface'
 
 const LoadMore: FC<IPropsLoadMore> = ({ hasNextPage, isFetching, isFetchingNextPage, fetchNextPage }) => {
   return (
-    <div className='load-more m-4'>
+    <div className='flex items-center justify-center m-4'>
       <Button className='mBtn_primary' type='button' onClick={() => fetchNextPage()} disabled={!hasNextPage || isFetchingNextPage}>
         {isFetchingNextPage || isFetching ?
           <span className="loading loading-spinner"></span> :
