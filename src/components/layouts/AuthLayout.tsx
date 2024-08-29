ab446d96e982e5017fe509f06c5e37517b391f36
BolotnikovMS <bolotnikovms@yandex.ru>
2024-08-29 15:07:03 +0500

    feat: Добавлена подпись

--- src/components/layouts/AuthLayout.tsx
+++ src/components/layouts/AuthLayout.tsx
@@ -6,8 +6,9 @@ import { LogoBe } from '../../icons'
 export const AuthLayout: FC = () => {
   return (
     <div className='flex items-center h-screen'>
-      <div className='!text-black w-2/5 flex justify-center'>
+      <div className='!text-black w-2/5 flex flex-col items-center justify-center gap-5'>
         <LogoBe className='!text-black w-1/2' />
+        <p className='text-4xl text-sky-500'>ПО "ИТиС" ООО "Башкирэнерго"</p>
       </div>
       <main className='flex-1 p-4'>
         <div className="container mx-auto max-w-3xl">
