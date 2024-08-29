93108a033701d533bc2567fd31fd8c7139c9dacd
BolotnikovMS <bolotnikovms@yandex.ru>
2024-08-29 20:23:06 +0500

    style: Поправлены отступы

--- src/hooks/file/useDeleteFile.ts
+++ src/hooks/file/useDeleteFile.ts
@@ -1,21 +1,20 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query'
-
 import { toast } from 'react-toastify'
 import { errorHandler } from '../../helpers'
 import { FileService } from '../../services/file/file.service'
 
 export const useDeleteFile = () => {
   const queryClient = useQueryClient()
-	const deleteFile = useMutation({
-		mutationFn: (id: number) => FileService.delete(id),
-		onSuccess: async () => {
-      await queryClient.invalidateQueries({queryKey: ['substation']})
-			toast.success('Файл успешно удален!')
+  const deleteFile = useMutation({
+    mutationFn: (id: number) => FileService.delete(id),
+    onSuccess: async () => {
+      await queryClient.invalidateQueries({ queryKey: ['substation'] })
+      toast.success('Файл успешно удален!')
     },
-		onError: async (error) => {
-			toast.error(errorHandler(error))
-		}
-	})
+    onError: async (error) => {
+      toast.error(errorHandler(error))
+    }
+  })
 
-	return { deleteFile }
+  return { deleteFile }
 }
