8ddde3682908ee01c7dfa2b5297b1e224542bd0b
BolotnikovMS <bolotnikovms@yandex.ru>
2024-09-05 17:04:09 +0500

    feat: Добавлен класс для копки, также добавлен обработчик

--- src/components/dropdown/Dropdown.tsx
+++ src/components/dropdown/Dropdown.tsx
@@ -2,10 +2,10 @@ import cx from 'classnames'
 import { Button } from '..'
 import { IPropsDropdown } from './dropdown.interface'
 
-const Dropdown = ({ children, menuItems, classMenu }: IPropsDropdown) => {
+const Dropdown = ({ children, menuItems, classMenu, classBtnTrigger }: IPropsDropdown) => {
   return (
     <div className={cx('dropdown', classMenu ?? 'dropdown-top dropdown-end')}>
-      <Button tabIndex={0} className="mBtn" >
+      <Button tabIndex={0} className={cx("mBtn", classBtnTrigger)} onClick={(e) => {e.stopPropagation()}}>
         {children}
       </Button>
       <ul tabIndex={0} className="dropdown-content menu bg-base-100 rounded-box z-[1] w-52 p-2 m-2 gap-1 shadow">
