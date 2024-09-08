28c145ef80e5716f425204688665239b5b644021
BolotnikovMS <bolotnikovms@yandex.ru>
2024-09-08 16:53:55 +0500

    fix: Исправление полей

--- src/interfaces/completed-work.interface.ts
+++ src/interfaces/completed-work.interface.ts
@@ -8,7 +8,7 @@ export interface ICompletedWork {
   note?: string | null
   dateCompletion: string
   createdAt: Date
-  substation?: Pick<ISubstation, 'id' | 'fullNameSubstation'>
-  work_producer?: Pick<IUser, 'id' | 'shortName'>
-  author?: Pick<IUser, 'id' | 'shortName'>
+  substation: Pick<ISubstation, 'id' | 'fullNameSubstation'>
+  work_producer: Pick<IUser, 'id' | 'shortName'>
+  author: Pick<IUser, 'id' | 'shortName'>
 }
