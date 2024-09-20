6c4168e6514996181d59a1f36f65802eeb67530c
BolotnikovMS <bolotnikovms@yandex.ru>
2024-09-20 19:07:34 +0500

    feat: Добавлена модалка и кнопка
    Добавлено модальное окно с формой для изменения роли пользователя, кнопка для вызова окна

--- src/views/user/components/table/UsersTable.tsx
+++ src/views/user/components/table/UsersTable.tsx
@@ -7,12 +7,14 @@ import { checkRole } from '../../../../helpers'
 import { useModal, useUsers } from '../../../../hooks'
 import { IUser } from '../../../../interfaces'
 import { useAuthStore } from '../../../../store/auth'
+import ChangeUserRole from '../form/changeUserRole/ChangeUserRole'
 
 const UsersTable: FC = () => {
   const { authUser } = useAuthStore()
   const isAdmin = checkRole(authUser, [ERoles.Admin])
   const { data, error, isError, isLoading } = useUsers({})
   const { isModal, toggleModal } = useModal()
+  const { isModal: isModalChangeRole, toggleModal: toggleModalChangeRole } = useModal()
   const [currentUser, setCurrentUser] = useState<IUser | undefined>(undefined)
   const columns = useMemo<ColumnDef<IUser>[]>(() => [
     {
@@ -40,7 +42,16 @@ const UsersTable: FC = () => {
     },
     {
       header: 'Роль',
-      accessorFn: row => row.role.name
+      cell: ({ row }) => (
+        isAdmin && (
+          <div className='table-cell-row'>
+            {row.original.role.name}
+            <Button title='Изменить роль пользователя' onClick={() => { toggleModalChangeRole(), setCurrentUser(row.original) }}>
+              <Icon id='user-switch' />
+            </Button>
+          </div>
+        )
+      )
     },
     {
       header: () => <Icon id='setting' />,
@@ -72,6 +83,13 @@ const UsersTable: FC = () => {
         title='Форма сброса пароля'
         content={<ChangePasswordForm toggleModal={toggleModal} isResetPassword user={currentUser} />}
         onToggle={toggleModal}
+      />
+      <Modal
+        visible={isModalChangeRole}
+        title='Форма изменения роли пользователя'
+        classDialog='!h-[350px]'
+        content={<ChangeUserRole toggleModal={toggleModalChangeRole} user={currentUser} />}
+        onToggle={toggleModalChangeRole}
       />
     </>
   )
