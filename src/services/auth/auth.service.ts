4855a2b79816586a0dba48df3d7023d1730c1964
BolotnikovMS <bolotnikovms@yandex.ru>
2024-09-20 19:03:18 +0500

    style: Поправлены отступы

--- src/services/auth/auth.service.ts
+++ src/services/auth/auth.service.ts
@@ -4,22 +4,22 @@ import { AxiosResponse } from 'axios'
 import { instance } from '../../api/axios.api'
 
 export const AuthService = {
-	async login(userDate: IUserDataLogin): Promise<IUserLogin | null> {
-		const { data } = await instance.post<IUserLogin>('/login', userDate)
+  async login(userDate: IUserDataLogin): Promise<IUserLogin | null> {
+    const { data } = await instance.post<IUserLogin>('/login', userDate)
 
-		return data
-	},
-	async getProfile(): Promise<IUser | null> {
-		const { data } = await instance.get<IUser>('/profile')
+    return data
+  },
+  async getProfile(): Promise<IUser | null> {
+    const { data } = await instance.get<IUser>('/profile')
 
-		return data
-	},
-	async changePassword(data: IChangePassword): Promise<AxiosResponse<string>> {
-		return instance.patch('/change-password', data)
-	},
-	async logout(): Promise<IGenericResponseLogout> {
-		const { data } = await instance.get<IGenericResponseLogout>('/logout')
+    return data
+  },
+  async changePassword(data: IChangePassword): Promise<AxiosResponse<string>> {
+    return instance.patch('/change-password', data)
+  },
+  async logout(): Promise<IGenericResponseLogout> {
+    const { data } = await instance.get<IGenericResponseLogout>('/logout')
 
-		return data
-	}
+    return data
+  }
 }
