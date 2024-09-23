09648aa3e29425c61d0290f34beed6cb38c538a0
BolotnikovMS <bolotnikovms@yandex.ru>
2024-09-23 16:47:05 +0500

    feat: Добавлен интерфейс ICustomAxisTick

--- src/views/dashboard/components/charts/substationsTypeKpChart/substationsTypeKpChart.interface.ts
+++ src/views/dashboard/components/charts/substationsTypeKpChart/substationsTypeKpChart.interface.ts
@@ -1,6 +1,14 @@
+import { Payload } from 'recharts/types/component/DefaultLegendContent'
+
 export interface ICustomBarLabel {
   x: number
   y: number
   width: number
   value: number
+}
+
+export interface ICustomAxisTick {
+  x: number
+  y: number
+  payload: Payload
 }
