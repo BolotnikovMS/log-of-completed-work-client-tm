59899bcb92d3f5bc114f806c1da863243b4646ce
BolotnikovMS <bolotnikovms@yandex.ru>
2024-09-23 17:14:48 +0500

    feat: Добавлен CustomTooltip

--- src/views/dashboard/components/charts/substationsTypeKpChart/SubstationsTypeKpChart.tsx
+++ src/views/dashboard/components/charts/substationsTypeKpChart/SubstationsTypeKpChart.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 import { Bar, BarChart, Legend, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts'
 import { Error, Loader } from '../../../../../components'
 import { useSubstationsTypeKp } from '../../../../../hooks'
-import { ICustomAxisTick, ICustomBarLabel } from './substationsTypeKpChart.interface'
+import { ICustomAxisTick, ICustomBarLabel, ICustomTooltip } from './substationsTypeKpChart.interface'
 
 const SubstationsTypeKpChart: FC = () => {
   const { data, error, isError, isLoading } = useSubstationsTypeKp()
@@ -14,7 +14,7 @@ const SubstationsTypeKpChart: FC = () => {
   const renderCustomBarLabel = ({ x, y, width, value }: ICustomBarLabel) => {
     return <text className='recharts-text' x={x + width / 2} y={y} fill="#666" textAnchor="middle" dy={-6}>{`${value}`}</text>
   }
-  const CustomizedAxisTick = ({ x, y, payload }: ICustomAxisTick) => {
+  const CustomAxisTick = ({ x, y, payload }: ICustomAxisTick) => {
     const { value } = payload
     const [id, name] = value.split('-')
     const link = `/substations?typeKp=${id}`
@@ -29,6 +29,19 @@ const SubstationsTypeKpChart: FC = () => {
       </g>
     )
   }
+  const CustomTooltip = ({ payload, active }: ICustomTooltip) => {
+    if (active) {
+      const { name, numberSubstations } = payload[0].payload
+
+      return (
+        <div className="py-2 px-2 bg-gray-200/80 rounded-lg">
+          <p className="recharts-text font-bold">{`${name}:`} <span className='font-normal'>{numberSubstations}</span></p>
+        </div>
+      )
+    }
+
+    return null;
+  }
 
   if (isLoading) return <Loader />
   if (isError && error) return <Error error={error} />
@@ -36,9 +49,9 @@ const SubstationsTypeKpChart: FC = () => {
   return (
     <ResponsiveContainer width='100%' height={330}>
       <BarChart width={500} height={330} data={transformedData}>
-        <XAxis dataKey='combinedKey' tick={CustomizedAxisTick} />
+        <XAxis dataKey='combinedKey' tick={CustomAxisTick} />
         <YAxis />
-        <Tooltip />
+        <Tooltip content={<CustomTooltip active={false} payload={[]} />} />
         <Legend verticalAlign='top' height={55} />
         <Bar name='Количество объектов по типу КП' barSize={50} dataKey="numberSubstations" fill="#03ACEF" label={renderCustomBarLabel} />
       </BarChart>
