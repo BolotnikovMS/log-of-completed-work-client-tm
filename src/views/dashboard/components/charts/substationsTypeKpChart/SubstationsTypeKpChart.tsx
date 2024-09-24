a836f66d80efa2c01a0d21523fe55658228d9904
BolotnikovMS <bolotnikovms@yandex.ru>
2024-09-24 18:36:43 +0500

    refactor: Использованы общие компоненты графиков

--- src/views/dashboard/components/charts/substationsTypeKpChart/SubstationsTypeKpChart.tsx
+++ src/views/dashboard/components/charts/substationsTypeKpChart/SubstationsTypeKpChart.tsx
@@ -1,14 +1,13 @@
 import { type FC } from 'react'
 import { Bar, BarChart, Legend, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts'
-import { Error, Loader } from '../../../../../components'
+import { CustomAxisTickChart, CustomBarChartLabel, CustomTooltipChart, Error, Loader } from '../../../../../components'
 import { useSubstationsTypeKp } from '../../../../../hooks'
-import { CustomAxisTick, CustomBarLabel, CustomTooltip } from './chartParts'
 
 const SubstationsTypeKpChart: FC = () => {
   const { data, error, isError, isLoading } = useSubstationsTypeKp()
   const transformedData = data?.map(item => ({
     ...item,
-    combinedKey: `${item.id}-${item.name}`
+    combinedKey: `${item.id}:${item.name.slice(3)}`
   }))
 
   if (isLoading) return <Loader />
@@ -17,11 +16,22 @@ const SubstationsTypeKpChart: FC = () => {
   return (
     <ResponsiveContainer width='100%' height={330}>
       <BarChart width={500} height={330} data={transformedData}>
-        <XAxis dataKey='combinedKey' tick={CustomAxisTick} />
+        <XAxis
+          dataKey='combinedKey'
+          tick={
+            <CustomAxisTickChart x={0} y={0} payload={undefined} linkTemplate={(typeKpId) => `/substations?typeKp=${typeKpId}`} renderValue={(_, textLabel) => textLabel} valueSeparator=':' />
+          }
+        />
         <YAxis />
-        <Tooltip content={<CustomTooltip active={false} payload={[]} />} />
+        <Tooltip content={<CustomTooltipChart active={false} payload={[]} labelKey={'name'} valueKey={'numberSubstations'} labelText='в количестве: ' />} />
         <Legend verticalAlign='top' height={55} />
-        <Bar name='Количество объектов по типу КП' barSize={50} dataKey="numberSubstations" fill="#03ACEF" label={CustomBarLabel} />
+        <Bar
+          name='Количество объектов по типу КП'
+          barSize={40}
+          dataKey="numberSubstations"
+          fill="#03ACEF"
+          label={<CustomBarChartLabel x={0} y={0} width={0} value={0} labelClass='font-bold' />}
+        />
       </BarChart>
     </ResponsiveContainer>
   )
