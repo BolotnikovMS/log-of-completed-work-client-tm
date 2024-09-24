4b8bf59488c3c7e39c77e2c3dfe8a164eb5631e7
BolotnikovMS <bolotnikovms@yandex.ru>
2024-09-24 18:36:17 +0500

    feat: Добавлен компонент с графиком работ

--- src/views/dashboard/Dashboard.tsx
+++ src/views/dashboard/Dashboard.tsx
@@ -1,17 +1,23 @@
 import { type FC } from 'react'
 import { Card } from '../../components'
-import { SubstationsTypeKpChart } from './components'
+import { CompletedWorksYearChart, SubstationsTypeKpChart } from './components'
 
 const Dashboard: FC = () => {
   return (
-    <>
+    <div className='flex flex-col gap-4'>
       <Card
         classContent='justify-center'
         childrenContent={
           <SubstationsTypeKpChart />
         }
       />
-    </>
+      <Card
+        classContent='justify-center'
+        childrenContent={
+          <CompletedWorksYearChart />
+        }
+      />
+    </div>
   )
 }
 
