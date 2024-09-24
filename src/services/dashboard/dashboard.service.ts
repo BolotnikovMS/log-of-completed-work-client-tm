a381ca7676b99c2481dd78ecc8e4cc52ca3da043
BolotnikovMS <bolotnikovms@yandex.ru>
2024-09-24 18:34:11 +0500

    feat: Добавлен метод
    Добавлен метод для получения даных по кол-ву выпол-х работ по годам

--- src/services/dashboard/dashboard.service.ts
+++ src/services/dashboard/dashboard.service.ts
@@ -1,10 +1,15 @@
 import { instance } from '../../api/axios.api'
 import { url } from '../../constants'
-import { ITypeKp } from '../../interfaces'
+import { ICompletedWorkYear, ITypeKp } from '../../interfaces'
 
 export const DashboardService = {
   async getSubstationsTypeKp(): Promise<ITypeKp[]> {
     const { data } = await instance.get<ITypeKp[]>(`${url}/dashboards/statistics-type-kp`)
+
+    return data
+  },
+  async getCompletedWorksYear(): Promise<ICompletedWorkYear[]> {
+    const { data } = await instance.get<ICompletedWorkYear[]>(`${url}/dashboards/statistics-completed-works-years`)
 
     return data
   }
