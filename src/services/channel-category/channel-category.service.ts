26480372d27e4057479a5228b007df364a5b0e2d
BolotnikovMS <bolotnikovms@yandex.ru>
2024-10-02 17:21:31 +0500

    feat: Сервис с запросами к api

---
+++ src/services/channel-category/channel-category.service.ts
@@ -0,0 +1,27 @@
+import { AxiosResponse } from 'axios'
+import { instance } from '../../api/axios.api'
+import { url } from '../../constants'
+import { IChannelCategory, IQueryParams } from '../../interfaces'
+import { TChannelCategoryData, TRespChannelCategories } from './channel-category.type'
+
+export const ChannelCategoryService = {
+  async getChannelCategories({ limit, page }: IQueryParams): Promise<TRespChannelCategories> {
+    const { data } = await instance.get<TRespChannelCategories>(`${url}/channel-categories`, {
+      params: { page, limit }
+    })
+
+    return data
+  },
+
+  async create(data: TChannelCategoryData): Promise<AxiosResponse<IChannelCategory>> {
+    return instance.post<IChannelCategory>(`${url}/channel-categories`, data)
+  },
+
+  async update(id: number, data: TChannelCategoryData): Promise<AxiosResponse<IChannelCategory>> {
+    return instance.patch<IChannelCategory>(`${url}/channel-categories/${id}`, data)
+  },
+
+  async delete(id: number): Promise<AxiosResponse<void>> {
+    return instance.delete<void>(`${url}/channel-categories/${id}`)
+  }
+}
