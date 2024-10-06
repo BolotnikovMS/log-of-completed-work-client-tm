9b40c1d1547cd052867f710a251c25304d72020c
BolotnikovMS <bolotnikovms@yandex.ru>
2024-10-06 19:32:53 +0500

    feat: Компонент с карточками каналов

---
+++ src/views/channel/components/cards/ChannelCards.tsx
@@ -0,0 +1,51 @@
+import { type FC } from 'react'
+import { Link, useSearchParams } from 'react-router-dom'
+import { Error, Icon, InfoMessage, Loader, LoadMore, SmallCard } from '../../../../components'
+import { useInfiniteChannels } from '../../../../hooks/channels/useInfiniteChannels'
+import { ChannelControlMenu } from './cardParts'
+
+const ChannelCards: FC = () => {
+  const [searchParams] = useSearchParams()
+  const substationParam = searchParams.get('substation')
+  const channelTypeParam = searchParams.get('channelType')
+  const { data, error, fetchNextPage, hasNextPage, isError, isFetching, isFetchingNextPage } = useInfiniteChannels({ limit: 20, substation: substationParam, channelType: channelTypeParam })
+
+  if (isError && error) return <Error error={error} />
+
+  if (isFetching) return <Loader />
+
+  return (
+    <>
+      {!!data?.pages[0].data.length && (
+        <div className='cards'>
+          {data.pages.map(channelsData => (
+            channelsData.data.map(channel => (
+              <SmallCard
+                key={channel.id}
+                childrenContent={
+                  <p className='text-content'>
+                    {channel.channel_category?.name}
+                    {' - '}
+                    <Link to={`/substations/${channel.substationId}`} className='flex items-center gap-1 font-bold'>
+                      <Icon id='link' />
+                      {channel.substation?.fullNameSubstation}
+                    </Link>
+                  </p>
+                }
+                childrenControl={
+                  <ChannelControlMenu channel={channel} />
+                }
+              />
+            ))
+          ))}
+        </div>
+      )}
+      {(!data?.pages[0].data.length && !isFetching && !isError) && (
+        <InfoMessage text='Пока не добавлено каналов...' />
+      )}
+      {hasNextPage && <LoadMore hasNextPage={hasNextPage} isFetching={isFetching} isFetchingNextPage={isFetchingNextPage} fetchNextPage={fetchNextPage} />}
+    </>
+  )
+}
+
+export default ChannelCards
