ffe023e81a3d65e676b53d3456743ddabcf1429b
BolotnikovMS <bolotnikovms@yandex.ru>
2024-10-06 19:28:23 +0500

    feat: Сервис для работы с каналами

---
+++ src/services/channel/channel.service.ts
@@ -0,0 +1,42 @@
+import { AxiosResponse } from 'axios'
+import fileDownload from 'js-file-download'
+import { toast } from 'react-toastify'
+import { instance } from '../../api/axios.api'
+import { url } from '../../constants'
+import { errorHandler } from '../../helpers'
+import { IChannel, IQueryParams } from '../../interfaces'
+import { TChannelData } from '../../types'
+import { TRespChannels } from '../../types/channel.types'
+
+export const ChannelService = {
+  async getChannels({ limit, page, substation, channelType }: IQueryParams): Promise<TRespChannels> {
+    const { data } = await instance.get<TRespChannels>(`${url}/channels`, {
+      params: { page, limit, substation, channelType }
+    })
+
+    return data
+  },
+
+  async create(data: TChannelData): Promise<AxiosResponse<IChannel>> {
+    return instance.post<IChannel>(`${url}/channels`, data)
+  },
+
+  async update(id: number, data: TChannelData): Promise<AxiosResponse<IChannel>> {
+    return instance.patch<IChannel>(`${url}/channels/${id}`, data)
+  },
+
+  async delete(id: number): Promise<AxiosResponse<void>> {
+    return instance.delete<void>(`${url}/channels/${id}`)
+  },
+
+  async downloadExcel({ page, limit, substation, channelType }: IQueryParams): Promise<void> {
+    await instance.get(`${url}/channels/download-excel`, {
+      params: { page, limit, substation, channelType },
+      responseType: 'blob'
+    }).then(resp => {
+      fileDownload(resp.data, 'channel-repport.xlsx')
+    }).catch(e => {
+      toast.error(errorHandler(e))
+    })
+  }
+}
