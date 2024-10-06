7f5dfa5ad51dae8c5d08a055e0505e9e1a9c1515
BolotnikovMS <bolotnikovms@yandex.ru>
2024-10-06 19:27:23 +0500

    fix: Поправлены поля, добавлено поле объекта

--- src/interfaces/channel.interface.ts
+++ src/interfaces/channel.interface.ts
@@ -1,5 +1,6 @@
 import { IChannelType } from './channel-type.interface'
 import { IChannelCategory } from './channelCategory.interface'
+import { ISubstation } from './substation.interface'
 
 export interface IChannel {
   id: number
@@ -7,8 +8,9 @@ export interface IChannel {
   substationId: number
   channelCategoryId: number
   channelTypeId: number
-  ipAddress: string | null
-  note: string | null
+  ipAddress?: string | null
+  note?: string | null
   channel_category?: Pick<IChannelCategory, 'id' | 'name'>
   channel_type?: Pick<IChannelType, 'id' | 'name'>
+  substation?: Pick<ISubstation, 'id' | 'fullNameSubstation'>
 }
