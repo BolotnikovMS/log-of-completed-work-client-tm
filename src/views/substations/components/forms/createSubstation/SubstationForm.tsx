59fb0b0e7a4f35050c117c9ea91e90210ace12b5
BolotnikovMS <bolotnikovms@yandex.ru>
2024-10-08 10:44:25 +0500

    refactor: Убраны поля после переработки ввода каналов
    Поправлены интерфейсы формы и полей

--- src/views/substations/components/forms/createSubstation/SubstationForm.tsx
+++ src/views/substations/components/forms/createSubstation/SubstationForm.tsx
@@ -4,27 +4,21 @@ import { SubmitHandler, useController, useForm } from 'react-hook-form'
 import { useParams } from 'react-router-dom'
 import AsyncSelect from 'react-select'
 import { Button, Error, Group, Input, Loader, SelectWrapper } from '../../../../../components'
-import { useChannelTypes, useCreateSubstation, useDistricts, useGsmOperators, useHeadControllers, useTypesKp, useUpdateSubstation, useVoltageClasses } from '../../../../../hooks'
-import { IPropsMutation } from '../../../../../interfaces'
+import { useCreateSubstation, useDistricts, useHeadControllers, useTypesKp, useUpdateSubstation, useVoltageClasses } from '../../../../../hooks'
+import { IPropsForm, IPropsMutation, ISubstation } from '../../../../../interfaces'
+import { TSubstationData } from '../../../../../types'
 import { validationSchema } from './substation.validation'
-import { IPropsSubstationForm, ISubstationFields } from './substationForm.interface'
 
-const SubstationForm: FC<IPropsSubstationForm> = ({ substation, isEdited, setIsEdited, toggleModal }) => {
+const SubstationForm: FC<IPropsForm<ISubstation>> = ({ data: substation, isEdited, setIsEdited, toggleModal }) => {
   const { id } = useParams()
-  const { register, handleSubmit, formState: { errors, isValid }, reset, control } = useForm<ISubstationFields>({
+  const { register, handleSubmit, formState: { errors, isValid }, reset, control } = useForm<TSubstationData>({
     mode: 'onBlur',
     defaultValues: {
       districtId: substation?.districtId || (id ? +id : undefined),
       voltageClassesId: substation?.voltageClassesId,
       active: substation?.active,
       rdu: substation?.rdu,
-      additionalChannelId: substation?.additionalChannelId || null,
-      backupChannelId: substation?.backupChannelId || null,
-      backupChannelIp: substation?.backupChannelIp,
-      gsmId: substation?.gsmId || null,
       headControllerId: substation?.headControllerId,
-      mainChannelId: substation?.mainChannelId,
-      mainChannelIp: substation?.mainChannelIp,
       name: substation?.name,
       typeKpId: substation?.typeKpId,
     },
@@ -34,27 +28,21 @@ const SubstationForm: FC<IPropsSubstationForm> = ({ substation, isEdited, setIsE
   const { field: { value: voltageClassValue, onChange: voltageClassOnChange, ...restVoltageClass } } = useController({ name: 'voltageClassesId', control })
   const { field: { value: typeKpValue, onChange: typeKpOnChange, ...restTypeKp } } = useController({ name: 'typeKpId', control })
   const { field: { value: headControllerValue, onChange: headControllerOnChange, ...restHeadController } } = useController({ name: 'headControllerId', control })
-  const { field: { value: gsmOperatorValue, onChange: gsmOperatorOnChange, ...restGsmOperator } } = useController({ name: 'gsmId', control })
-  const { field: { value: mainChannelTypeValue, onChange: mainChannelTypeOnChange, ...restMainChannelType } } = useController({ name: 'mainChannelId', control })
-  const { field: { value: backupChannelTypeValue, onChange: backupChannelTypeOnChange, ...restBackupChannelType } } = useController({ name: 'backupChannelId', control })
-  const { field: { value: additionalChannelTypeValue, onChange: additionalChannelTypeOnChange, ...restAdditionalChannelType } } = useController({ name: 'additionalChannelId', control })
   const { districts, isLoading: isLoadingDistricts, isError: isErrorDistricts } = useDistricts({})
   const { voltageClasses, isError: isErrorVoltageClasses, isLoading: isLoadingVoltageClasses } = useVoltageClasses()
   const { typesKp, isError: isErrorTypesKp, isLoading: isLoadingTypesKp } = useTypesKp({})
   const { headControllers, isError: isErrorHeadControllers, isLoading: isLoadingHeadControllers } = useHeadControllers()
-  const { data: gsmOperators, isError: isErrorGsmOperators, isLoading: isLoadingGsmOperators } = useGsmOperators()
-  const { data: channelTypes, isError: isErrorChannelTypes, isLoading: isLoadingChannelTypes } = useChannelTypes()
   const { mutateAsync: createSubstation, isError: isErrorCreate, error: errorCreate, isPending: isPendingCreate } = useCreateSubstation()
   const { mutateAsync: updateSubstation, isError: isErrorUpdate, error: errorUpdate, isPending: isPendingUpdate } = useUpdateSubstation()
-  const handleMutation = async ({ data, mutateFn, id }: IPropsMutation<ISubstationFields>) => {
+  const handleMutation = async ({ data, mutateFn, id }: IPropsMutation<TSubstationData>) => {
     await mutateFn(id ? { id, data } : data)
 
     reset()
     toggleModal()
     if (isEdited && setIsEdited) setIsEdited(false)
   }
-  const submitCreate: SubmitHandler<ISubstationFields> = data => handleMutation({ data, mutateFn: createSubstation })
-  const submitUpdate: SubmitHandler<ISubstationFields> = data => {
+  const submitCreate: SubmitHandler<TSubstationData> = data => handleMutation({ data, mutateFn: createSubstation })
+  const submitUpdate: SubmitHandler<TSubstationData> = data => {
     if (!substation?.id) return null
 
     handleMutation({ data, mutateFn: updateSubstation, id: substation.id })
@@ -129,74 +117,6 @@ const SubstationForm: FC<IPropsSubstationForm> = ({ substation, isEdited, setIsE
           </SelectWrapper>
         </Group>
         <Group>
-          <SelectWrapper label='Выберите основной канал' errorMessage={errors.mainChannelId?.message} mandatory>
-            <AsyncSelect
-              classNamePrefix='form__custom-select'
-              options={channelTypes?.data}
-              getOptionValue={option => option.id.toString()}
-              getOptionLabel={option => option.name}
-              value={mainChannelTypeValue ? channelTypes?.data.find(mC => mC.id === mainChannelTypeValue) : null}
-              onChange={option => mainChannelTypeOnChange(option ? option.id : option)}
-              isLoading={isLoadingChannelTypes}
-              isDisabled={isErrorChannelTypes}
-              isClearable
-              placeholder="Выберите основной канал..."
-              {...restMainChannelType}
-            />
-          </SelectWrapper>
-        </Group>
-        <Group>
-          <SelectWrapper label='Выберите резервный канал' mandatory errorMessage={errors.backupChannelId?.message}>
-            <AsyncSelect
-              classNamePrefix='form__custom-select'
-              options={channelTypes?.data}
-              getOptionValue={option => option.id.toString()}
-              getOptionLabel={option => option.name}
-              value={backupChannelTypeValue ? channelTypes?.data.find(bC => bC.id === backupChannelTypeValue) : null}
-              onChange={option => backupChannelTypeOnChange(option ? option.id : option)}
-              isLoading={isLoadingChannelTypes}
-              isDisabled={isErrorChannelTypes}
-              isClearable
-              placeholder="Выберите резервный канал..."
-              {...restBackupChannelType}
-            />
-          </SelectWrapper>
-        </Group>
-        <Group>
-          <SelectWrapper label='Выберите дополнительный канал' mandatory errorMessage={errors.additionalChannelId?.message}>
-            <AsyncSelect
-              classNamePrefix='form__custom-select'
-              options={channelTypes?.data}
-              getOptionValue={option => option.id.toString()}
-              getOptionLabel={option => option.name}
-              value={additionalChannelTypeValue ? channelTypes?.data.find(aC => aC.id === additionalChannelTypeValue) : null}
-              onChange={option => additionalChannelTypeOnChange(option ? option.id : option)}
-              isLoading={isLoadingChannelTypes}
-              isDisabled={isErrorChannelTypes}
-              isClearable
-              placeholder="Выберите дополнительный канал..."
-              {...restAdditionalChannelType}
-            />
-          </SelectWrapper>
-        </Group>
-        <Group>
-          <SelectWrapper label='Выберите GSM оператора' mandatory errorMessage={errors.gsmId?.message}>
-            <AsyncSelect
-              classNamePrefix='form__custom-select'
-              options={gsmOperators}
-              getOptionValue={option => option.id.toString()}
-              getOptionLabel={option => option.name}
-              value={gsmOperatorValue ? gsmOperators?.find(g => g.id === gsmOperatorValue) : null}
-              onChange={option => gsmOperatorOnChange(option ? option.id : option)}
-              isLoading={isLoadingGsmOperators}
-              isDisabled={isErrorGsmOperators}
-              isClearable
-              placeholder="Выберите GSM оператора..."
-              {...restGsmOperator}
-            />
-          </SelectWrapper>
-        </Group>
-        <Group>
           <SelectWrapper label='Выберите класс U' errorMessage={errors.voltageClassesId?.message} mandatory>
             <AsyncSelect
               classNamePrefix='form__custom-select'
@@ -212,24 +132,6 @@ const SubstationForm: FC<IPropsSubstationForm> = ({ substation, isEdited, setIsE
               {...restVoltageClass}
             />
           </SelectWrapper>
-        </Group>
-        <Group>
-          <Input
-            label='ip основного канала'
-            name='mainChannelIp'
-            register={register}
-            errorMessage={errors.mainChannelIp?.message}
-            placeholder='Введите ip основного канала...'
-          />
-        </Group>
-        <Group>
-          <Input
-            label='ip резервного канала'
-            name='backupChannelIp'
-            register={register}
-            errorMessage={errors.backupChannelIp?.message}
-            placeholder='Введите ip резервного канала...'
-          />
         </Group>
         <Group className='!items-center !justify-center'>
           <Input
--- src/views/substations/components/forms/createSubstation/substation.validation.ts
+++ src/views/substations/components/forms/createSubstation/substation.validation.ts
@@ -1,19 +1,12 @@
 import * as yup from 'yup'
-
-import { ipOptional, numberCheck, text240 } from '../../../../../validations/rules'
+import { numberCheck, text240 } from '../../../../../validations/rules'
 
 export const validationSchema = yup.object().shape({
   districtId: numberCheck,
   typeKpId: numberCheck,
   headControllerId: numberCheck,
-  mainChannelId: numberCheck,
-  backupChannelId: numberCheck,
-  additionalChannelId: numberCheck,
-  gsmId: numberCheck,
   name: text240,
   voltageClassesId: numberCheck,
-  mainChannelIp: ipOptional,
-  backupChannelIp: ipOptional,
   rdu: yup
     .boolean()
     .default(false),
