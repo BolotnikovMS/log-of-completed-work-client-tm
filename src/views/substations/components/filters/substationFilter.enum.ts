7ee104d7400e09aa82560d2abc670b3e5932cad1
BolotnikovMS <bolotnikovms@yandex.ru>
2024-10-13 18:29:25 +0500

    fix: Delete

--- src/views/substations/components/filters/substationFilter.enum.ts
+++
@@ -1,7 +0,0 @@
-export enum EFilterSubstation {
-  district = 'district',
-  typeKp = 'typeKp',
-  headController = 'headController',
-  mainChannel = 'mainChannel',
-  backupChannel = 'backupChannel',
-}
