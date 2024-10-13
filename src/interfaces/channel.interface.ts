98a17d5892b1d9f9429cfe13650c122ccc487d89
BolotnikovMS <bolotnikovms@yandex.ru>
2024-10-13 18:26:40 +0500

    feat: Добавлены поля и связи

--- src/interfaces/channel.interface.ts
+++ src/interfaces/channel.interface.ts
@@ -1,5 +1,7 @@
 import { IChannelType } from './channel-type.interface'
 import { IChannelCategory } from './channelCategory.interface'
+import { IChannelEquipment } from './channelEquipment.intarface'
+import { IGsmOperator } from './gsm-operator.interface'
 import { ISubstation } from './substation.interface'
 
 export interface IChannel {
@@ -8,9 +10,13 @@ export interface IChannel {
   substationId: number
   channelCategoryId: number
   channelTypeId: number
+  channelEquipmentId?: number | null
+  gsmId?: number | null
   ipAddress?: string | null
   note?: string | null
   channel_category?: Pick<IChannelCategory, 'id' | 'name'>
   channel_type?: Pick<IChannelType, 'id' | 'name'>
   substation?: Pick<ISubstation, 'id' | 'fullNameSubstation'>
+  channel_equipment?: Pick<IChannelEquipment, 'name'>
+  gsm_operator?: Pick<IGsmOperator, 'name'>
 }
