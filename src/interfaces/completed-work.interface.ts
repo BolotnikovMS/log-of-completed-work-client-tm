916212011645593d4484c42b1b104cd007a827e0
BolotnikovMS <bolotnikovms@yandex.ru>
2024-10-20 14:44:41 +0500

    fix: Поправлены свойства

--- src/interfaces/completed-work.interface.ts
+++ src/interfaces/completed-work.interface.ts
@@ -1,14 +1,18 @@
-import { ISubstation, IUser } from '.'
+import { ISubstation, ITypeWork, IUser } from '.'
 
 export interface ICompletedWork {
   id: number
   userId: number
+  substationId: number
+  workProducerId: number
+  typeWorkId: number
   description: string
   shortText: string
   note?: string | null
-  dateCompletion: string
+  dateCompletion: Date
   createdAt: Date
   substation: Pick<ISubstation, 'id' | 'fullNameSubstation'>
   work_producer: Pick<IUser, 'id' | 'shortName'>
-  author: Pick<IUser, 'id' | 'shortName'>
+  author?: Pick<IUser, 'id' | 'shortName'>
+  type_work: Pick<ITypeWork, 'id' | 'name'>
 }
