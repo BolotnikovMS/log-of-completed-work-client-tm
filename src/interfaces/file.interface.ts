ba035c1f3b2898f06eb4419534ecbb5a2fdfb160
BolotnikovMS <bolotnikovms@yandex.ru>
2024-10-22 19:01:05 +0500

    fix: Поправлено наименование

--- src/interfaces/file.interface.ts
+++ src/interfaces/file.interface.ts
@@ -1,13 +1,13 @@
-import { IUser } from './user'
+import { IUser } from './user.interface'
 
 export interface IFile {
-	id: number
-	userId: number
-	substationId: number
-	filePath: string
-	typeFile: string
-	size: number
-	clientName: string
-	createdAt: Date
-	author?: Pick<IUser, 'id' | 'shortName'>
+  id: number
+  userId: number
+  substationId: number
+  filePath: string
+  typeFile: string
+  size: number
+  clientName: string
+  createdAt: Date
+  author?: Pick<IUser, 'id' | 'shortName'>
 }
