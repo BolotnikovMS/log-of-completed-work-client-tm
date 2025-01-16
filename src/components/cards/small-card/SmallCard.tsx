96de0cc10e574cbcd5d50a3780a28e8b17de0fd9
Mikhail <bolotnikovms@yandex.ru>
2025-01-16 21:00:53 +0500

    feat: Добавлен класс

--- src/components/cards/small-card/SmallCard.tsx
+++ src/components/cards/small-card/SmallCard.tsx
@@ -1,25 +1,25 @@
-import cx from 'classnames'
+import { default as cn, default as cx } from 'classnames'
 import React from 'react'
 import { Link } from 'react-router-dom'
 import './../card.scss'
 import { IPropsSmallCard } from './smallCard.interface'
 
-export const SmallCard: React.FC<IPropsSmallCard> = ({ childrenContent, childrenControl, path, classContent }) => {
-  return (
-    <div className='mCard'>
-      {path && <Link to={path} className='mCard__link' />}
-      <div className='mCard__body'>
-        {childrenContent && (
-          <div className={cx('mCard__content', classContent)}>
-            {childrenContent}
-          </div>
-        )}
-        {childrenControl && (
-          <div className='mCard__actions'>
-            {childrenControl}
-          </div>
-        )}
-      </div>
-    </div>
-  )
+export const SmallCard: React.FC<IPropsSmallCard> = ({ childrenContent, className, childrenControl, path, classContent }) => {
+	return (
+		<div className={cn('mCard', className)}>
+			{path && <Link to={path} className='mCard__link' />}
+			<div className='mCard__body'>
+				{childrenContent && (
+					<div className={cx('mCard__content', classContent)}>
+						{childrenContent}
+					</div>
+				)}
+				{childrenControl && (
+					<div className='mCard__actions'>
+						{childrenControl}
+					</div>
+				)}
+			</div>
+		</div>
+	)
 }
