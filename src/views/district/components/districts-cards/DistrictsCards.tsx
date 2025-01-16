867ee84158623ace3b507835045318ccfefce551
Mikhail <bolotnikovms@yandex.ru>
2025-01-16 21:01:24 +0500

    style: Поправлены отступы

--- src/views/district/components/districts-cards/DistrictsCards.tsx
+++ src/views/district/components/districts-cards/DistrictsCards.tsx
@@ -11,83 +11,83 @@ import { useAuthStore } from '../../../../store/auth'
 import { TOrderSort } from '../../../../types/order.types'
 
 const DistrictsCards: FC = () => {
-  const { authUser } = useAuthStore()
-  const isAdmin = checkRole(authUser, [ERoles.Admin])
-  const isAdminOrModerator = checkRole(authUser, [ERoles.Moderator, ERoles.Admin])
-  const [page, setPage] = useState<number>(1)
-  const [searchParams] = useSearchParams()
-  const sortParam = searchParams.get('sort') || 'name'
-  const orderParam = searchParams.get('order') || 'asc'
-  const { districts: data, error, isError, isLoading } = useDistricts({ limit: 15, page, sort: sortParam, order: orderParam as TOrderSort })
-  const { isModal, toggleModal } = useModal()
-  const [isEdited, setIsEdited] = useState<boolean>(false)
-  const [district, setDistrict] = useState<IDistrict | null>(null)
-  const { deleteDistrict } = useDeleteDistrict()
-  const handleDelete = (id: number) => {
-    const answer = confirm('Подтвердите удаление записи.')
+	const { authUser } = useAuthStore()
+	const isAdmin = checkRole(authUser, [ERoles.Admin])
+	const isAdminOrModerator = checkRole(authUser, [ERoles.Moderator, ERoles.Admin])
+	const [page, setPage] = useState<number>(1)
+	const [searchParams] = useSearchParams()
+	const sortParam = searchParams.get('sort') || 'name'
+	const orderParam = searchParams.get('order') || 'asc'
+	const { districts: data, error, isError, isLoading } = useDistricts({ limit: 15, page, sort: sortParam, order: orderParam as TOrderSort })
+	const { isModal, toggleModal } = useModal()
+	const [isEdited, setIsEdited] = useState<boolean>(false)
+	const [district, setDistrict] = useState<IDistrict | null>(null)
+	const { deleteDistrict } = useDeleteDistrict()
+	const handleDelete = (id: number) => {
+		const answer = confirm('Подтвердите удаление записи.')
 
-    if (!answer) return null
+		if (!answer) return null
 
-    return deleteDistrict.mutate(id)
-  }
-  
-  useEffect(() => {
-    if (data?.data.length === 0 && page !== 1) {
-      setPage(page - 1)
-    }
-  }, [data?.data.length, page])
+		return deleteDistrict.mutate(id)
+	}
 
-  if (isError && error) return <Error error={error} />
+	useEffect(() => {
+		if (data?.data.length === 0 && page !== 1) {
+			setPage(page - 1)
+		}
+	}, [data?.data.length, page])
 
-  if (isLoading) return <Loader />
+	if (isError && error) return <Error error={error} />
 
-  return (
-    <>
-      {!!data?.data.length && (
-        <div className='flex flex-col gap-2'>
-          <div className="cards">
-            {data.data.map(district => (
-              <SmallCard
-                key={district.id}
-                childrenContent={
-                  <p className='text-content flex items-center gap-1'>
-                    <Icon id='link' />
-                    {district.name}
-                  </p>
-                }
-                path={pageConfig.getDynamicUrl(pageConfig.districtSubstations, { id: district.id })}
-                childrenControl={
-                  isAdminOrModerator &&
-                  <Dropdown
-                    children={
-                      <Icon id='setting' />
-                    }
-                    menuItems={[
-                      isAdminOrModerator && (
-                        <Button className='!justify-start' onClick={() => { toggleModal(), setDistrict(district), setIsEdited(!isEdited) }}>
-                          <Icon id='edit' />
-                          Редактировать
-                        </Button>
-                      ),
-                      isAdmin && (
-                        <Button className='btn-error !justify-start' onClick={() => handleDelete(district.id)}>
-                          <Icon id='delete' />
-                          Удалить
-                        </Button>
-                      )
-                    ]}
-                  />
-                }
-              />
-            ))}
-          </div>
-          <Pagination page={page} meta={data.meta} setPage={setPage} />
-        </div>
-      )}
-      {(!data?.data.length && !isLoading && !isError) && <InfoMessage text='Районов или ГП пока не добавлено...' />}
-      <Modal visible={isModal} title='Редактирование записи' onToggle={() => { toggleModal(), setIsEdited(false) }} content={<DistrictForm data={district} isEdited={isEdited} setIsEdited={setIsEdited} toggleModal={toggleModal} />} />
-    </>
-  )
+	if (isLoading) return <Loader />
+
+	return (
+		<>
+			{!!data?.data.length && (
+				<div className='flex flex-col gap-2'>
+					<div className="cards">
+						{data.data.map(district => (
+							<SmallCard
+								key={district.id}
+								childrenContent={
+									<p className='text-content flex items-center gap-1'>
+										<Icon id='link' />
+										{district.name}
+									</p>
+								}
+								path={pageConfig.getDynamicUrl(pageConfig.districtSubstations, { id: district.id })}
+								childrenControl={
+									isAdminOrModerator &&
+									<Dropdown
+										children={
+											<Icon id='setting' />
+										}
+										menuItems={[
+											isAdminOrModerator && (
+												<Button className='!justify-start' onClick={() => { toggleModal(), setDistrict(district), setIsEdited(!isEdited) }}>
+													<Icon id='edit' />
+													Редактировать
+												</Button>
+											),
+											isAdmin && (
+												<Button className='btn-error !justify-start' onClick={() => handleDelete(district.id)}>
+													<Icon id='delete' />
+													Удалить
+												</Button>
+											)
+										]}
+									/>
+								}
+							/>
+						))}
+					</div>
+					<Pagination page={page} meta={data.meta} setPage={setPage} />
+				</div>
+			)}
+			{(!data?.data.length && !isLoading && !isError) && <InfoMessage text='Районов или ГП пока не добавлено...' />}
+			<Modal visible={isModal} title='Редактирование записи' onToggle={() => { toggleModal(), setIsEdited(false) }} content={<DistrictForm data={district} isEdited={isEdited} setIsEdited={setIsEdited} toggleModal={toggleModal} />} />
+		</>
+	)
 }
 
 export default DistrictsCards
