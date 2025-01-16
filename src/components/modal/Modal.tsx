00cd9fc968a46b6dbbe4a627b70f12d23b095142
Mikhail <bolotnikovms@yandex.ru>
2025-01-16 20:27:51 +0500

    fix: Убрано закрытие модалки
    Убрано закрытие модалки при клике вне области окна

--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -6,25 +6,25 @@ import { IPropsModal } from './modal.interface'
 import './modal.scss'
 
 export const Modal: FC<IPropsModal> = ({ visible, title, content, footer, classDialog, onToggle }) => {
-  if (!visible) return null
+	if (!visible) return null
 
-  return createPortal(
-    <div className='mModal' onClick={onToggle}>
-      <div className={cx('mModal__dialog min-w-[650px]', classDialog)} onClick={e => e.stopPropagation()}>
-        <div className="mModal__header">
-          <h3 className="mModal__title">{title}</h3>
-          <span className="mModal__close" onClick={onToggle}>
-            <Icon id='close' />
-          </span>
-        </div>
-        <div className="overflow-y-auto p-4 h-full">
-          {content}
-        </div>
-        {footer && (
-          <div className="mModal__footer">{footer}</div>
-        )}
-      </div>
-    </div>,
-    document.body
-  )
+	return createPortal(
+		<div className='mModal'>
+			<div className={cx('mModal__dialog min-w-[650px]', classDialog)} onClick={e => e.stopPropagation()}>
+				<div className="mModal__header">
+					<h3 className="mModal__title">{title}</h3>
+					<span className="mModal__close" onClick={onToggle}>
+						<Icon id='close' />
+					</span>
+				</div>
+				<div className="overflow-y-auto p-4 h-full">
+					{content}
+				</div>
+				{footer && (
+					<div className="mModal__footer">{footer}</div>
+				)}
+			</div>
+		</div>,
+		document.body
+	)
 }
