4e46c89f80944844d6ed6a8f01a9de55848b4a33
Mikhail <bolotnikovms@yandex.ru>
2025-01-23 20:38:31 +0500

    feat: Добавлены атрибуты

--- src/components/checkbox/Checkbox.tsx
+++ src/components/checkbox/Checkbox.tsx
@@ -1,20 +1,30 @@
 import cx from 'classnames'
 import { type FC } from 'react'
+import { ValidationMessage } from '..'
 import { IPropsCheckbox } from './checkbox.interface'
 
-const Checkbox: FC<IPropsCheckbox> = ({ classLabel, textLabel, classLabelText, classInput, mandatory, ...attributes }) => {
+const Checkbox: FC<IPropsCheckbox> = ({ classLabel, textLabel, classLabelText, classInput, mandatory, register, validation, errorMessage, ...attributes }) => {
 	return (
-		<label className={cx('label cursor-pointer', classLabel)}>
-			<span className={cx('label__text', classLabelText)}>
-				{textLabel}
-				{mandatory && <span className='text-mandatory'>*</span>}
-			</span>
-			<input
-				type="checkbox"
-				className={cx('checkbox checked:!', classInput)}
-				{...attributes}
-			/>
-		</label>
+		<div className="relative">
+			<label className={cx('label cursor-pointer flex-row', classLabel)}>
+				<span className={cx('label__text text-lg', classLabelText)}>
+					{textLabel}
+					{mandatory && <span className='text-mandatory'>*</span>}
+				</span>
+				<input
+					type="checkbox"
+					className={cx('checkbox checkbox-sm', classInput)}
+					{...attributes}
+					{...register && register(attributes.name, validation)}
+				/>
+			</label>
+			{errorMessage && 
+				<ValidationMessage
+					className='bottom-[-27px]'
+					children={errorMessage}
+				/>
+			}
+		</div>
 	)
 }
 
