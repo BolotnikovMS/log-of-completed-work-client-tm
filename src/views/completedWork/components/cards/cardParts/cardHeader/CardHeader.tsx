03bc243c52a91680febe3512439682d9ac54bf8e
Mikhail <bolotnikovms@yandex.ru>
2025-02-04 20:39:50 +0500

    style:Поправлены отступы

--- src/views/completedWork/components/cards/cardParts/cardHeader/CardHeader.tsx
+++ src/views/completedWork/components/cards/cardParts/cardHeader/CardHeader.tsx
@@ -5,24 +5,24 @@ import { pageConfig } from '../../../../../../config/pages.config'
 import { IPropsCardHeader } from './cardHeader.interface'
 
 const CardHeader: FC<IPropsCardHeader> = memo(({ substationId, substationFullName, typeWork, inControl }) => {
-  if (!substationId || !substationFullName || !typeWork) {
-    return <p className='text-title text-red-500'>Заголовок не доступен</p>
-  }
+	if (!substationId || !substationFullName || !typeWork) {
+		return <p className='text-title text-red-500'>Заголовок не доступен</p>
+	}
 
-  return (
-    <div className='flex items-center justify-between gap-2'>
-      <p className='flex text-title font-bold' >
-        <Link to={pageConfig.getDynamicUrl(pageConfig.substation, { id: substationId })} className='flex items-center gap-1'>
-          <Icon id='link' />
-          {substationFullName}
-        </Link>
-      </p>
+	return (
+		<div className='flex items-center justify-between gap-2'>
+			<p className='flex text-title font-bold' >
+				<Link to={pageConfig.getDynamicUrl(pageConfig.substation, { id: substationId })} className='flex items-center gap-1'>
+					<Icon id='link' />
+					{substationFullName}
+				</Link>
+			</p>
 			{
 				inControl && <Badge text='Контроль' className='mBadge_orange' />
 			}
-      <Badge text={typeWork} className='mBadge_blue' />
-    </div>
-  )
+			<Badge text={typeWork} className='mBadge_blue' />
+		</div>
+	)
 })
 
 export default CardHeader
