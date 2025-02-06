9718781f689618ff6f800911effe4873c13bde14
Mikhail <bolotnikovms@yandex.ru>
2025-02-06 19:50:08 +0500

    style: Поправлены отступы

--- src/components/dropdown/Dropdown.tsx
+++ src/components/dropdown/Dropdown.tsx
@@ -3,21 +3,21 @@ import { Button } from '..'
 import { IPropsDropdown } from './dropdown.interface'
 
 const Dropdown = ({ children, menuItems, classMenu, classBtnTrigger }: IPropsDropdown) => {
-  return (
-    <div className={cx('dropdown', classMenu ?? 'dropdown-top dropdown-end')}>
-      <Button tabIndex={0} className={cx('mBtn', classBtnTrigger)} onClick={(e) => {e.stopPropagation()}}>
-        {children}
-      </Button>
-      <ul tabIndex={0} className="dropdown-content menu bg-base-100 rounded-box z-[1] w-52 p-2 m-2 gap-1 shadow">
-        {menuItems.map((item, i) => (
-          item && (
-            <li key={i}>
-              {item}
-            </li>)
-        ))}
-      </ul>
-    </div>
-  )
+	return (
+		<div className={cx('dropdown', classMenu ?? 'dropdown-top dropdown-end')}>
+			<Button tabIndex={0} className={cx('mBtn', classBtnTrigger)} onClick={(e) => { e.stopPropagation() }}>
+				{children}
+			</Button>
+			<ul tabIndex={0} className="dropdown-content menu bg-base-100 rounded-box z-[1] w-52 p-2 m-2 gap-1 shadow">
+				{menuItems.map((item, i) => (
+					item && (
+						<li key={i}>
+							{item}
+						</li>)
+				))}
+			</ul>
+		</div>
+	)
 }
 
 export default Dropdown
