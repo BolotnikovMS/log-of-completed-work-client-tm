792730155a40dabc6a9f4a7133551293a1d8f7f0
Mikhail <bolotnikovms@yandex.ru>
2025-02-09 19:13:27 +0500

    feat: Добавлен компонент переключения темы

--- src/components/navBar/NavBar.tsx
+++ src/components/navBar/NavBar.tsx
@@ -1,6 +1,6 @@
 import React, { type FC } from 'react'
 import { Link } from 'react-router-dom'
-import { Button, Icon } from '..'
+import { Button, Icon, ToggleTheme } from '..'
 import { pageConfig } from '../../config/pages.config'
 import { menuItemData } from '../../constants'
 import { ERoles } from '../../enums/roles.enum'
@@ -10,111 +10,114 @@ import { useAuthStore } from '../../store/auth'
 import './navbar.scss'
 
 export const NavBar: FC = () => {
-  const userAuthStore = useAuthStore()
-  const user = userAuthStore.authUser
-  const { logout } = useLogout()
-  const logoutHandel = () => logout.mutate()
+	const userAuthStore = useAuthStore()
+	const user = userAuthStore.authUser
+	const { logout } = useLogout()
+	const logoutHandel = () => logout.mutate()
 
-  return (
-    <div className='mNavBar'>
-      <nav className='mNavBar__nav'>
-        <div className='mNavBar__logo '>
-          {/*<Group className='!flex-row !items-center'>
+	return (
+		<div className='mNavBar'>
+			<nav className='mNavBar__nav'>
+				<div className='mNavBar__logo '>
+					{/*<Group className='!flex-row !items-center'>
             <Icon id='logo-be' className='!w-12 !h-12 !z-30' />
             <span className='text-2xl font-bold text-sky-500'>ПО "ИТиС"</span>
           </Group>*/}
-          <span className='text-content text-center'>
-            Журнал выполненных работ по ТМ
-          </span>
-        </div>
-        <ul className='mNavBar__menu'>
-          <li>
-            <Link to={pageConfig.statistics}>
-              <Icon id='pie-chart' />
-              Статистика
-            </Link>
-          </li>
-          <li>
-            <details>
-              <summary>
-                <Icon id='books' />
-                Справочники
-              </summary>
-              <ul className='flex flex-col gap-1'>
-                {menuItemData.map(menu => (
-                  <React.Fragment key={menu.url}>
-                    {menu?.submenu ? (
-                      <li>
-                        <details>
-                          <summary>{menu.title}</summary>
-                          <ul className='flex flex-col gap-1'>
-                            {menu?.submenu.map(subMenu => (
-                              <li key={subMenu.url}>
-                                <Link to={subMenu.url}>{subMenu.title}</Link>
-                              </li>
-                            ))}
-                          </ul>
-                        </details>
-                      </li>
-                    ) : (
-                      <li>
-                        <Link to={menu.url}>{menu.title}</Link>
-                      </li>
-                    )}
-                  </React.Fragment>
-                ))}
-              </ul>
-            </details>
-          </li>
-          <li>
-            <Link to={pageConfig.completedWorks}>
-              <Icon id='note-done' />
-              Выполненные работы
-            </Link>
-          </li>
-          {checkRole(user, [ERoles.Admin]) && (
-            <li>
-              <Link to={pageConfig.users}>
-                <Icon id='users' />
-                Пользователи
-              </Link>
-            </li>
-          )}
-        </ul>
-      </nav>
-      {user && (
-        <nav className='mNavBar__nav'>
-          <ul className='mNavBar__menu !gap-2'>
-            <div className='flex items-center gap-3'>
-              <div className="avatar placeholder">
-                <div className="bg-neutral text-neutral-content w-12 rounded-full">
-                  <span>
-                    <Icon id='user' className='!w-7 !h-7' />
-                  </span>
-                </div>
-              </div>
-              <div className='leading-3'>
-                <p className='text-lg'>
-                  {user.shortName}
-                </p>
-                <span className='text-[11px]'>
-                  {user.position}
-                </span>
-              </div>
-            </div>
-            <li>
-              <Link to={pageConfig.profile}>
-                <Icon id='profile' />
-                Профиль
-              </Link>
-            </li>
-            <Button className='btn-error mt-5' onClick={logoutHandel}>
-              <Icon id='logout' />
-              Выход
-            </Button>
-          </ul>
-        </nav>
-      )}
-    </div>
-  )
+					<span className='text-content text-center'>
+						Журнал выполненных работ по ТМ
+					</span>
+					<div className="">
+						<ToggleTheme />
+					</div>
+				</div>
+				<ul className='mNavBar__menu'>
+					<li>
+						<Link to={pageConfig.statistics}>
+							<Icon id='pie-chart' />
+							Статистика
+						</Link>
+					</li>
+					<li>
+						<details>
+							<summary>
+								<Icon id='books' />
+								Справочники
+							</summary>
+							<ul className='flex flex-col gap-1'>
+								{menuItemData.map(menu => (
+									<React.Fragment key={menu.url}>
+										{menu?.submenu ? (
+											<li>
+												<details>
+													<summary>{menu.title}</summary>
+													<ul className='flex flex-col gap-1'>
+														{menu?.submenu.map(subMenu => (
+															<li key={subMenu.url}>
+																<Link to={subMenu.url}>{subMenu.title}</Link>
+															</li>
+														))}
+													</ul>
+												</details>
+											</li>
+										) : (
+											<li>
+												<Link to={menu.url}>{menu.title}</Link>
+											</li>
+										)}
+									</React.Fragment>
+								))}
+							</ul>
+						</details>
+					</li>
+					<li>
+						<Link to={pageConfig.completedWorks}>
+							<Icon id='note-done' />
+							Выполненные работы
+						</Link>
+					</li>
+					{checkRole(user, [ERoles.Admin]) && (
+						<li>
+							<Link to={pageConfig.users}>
+								<Icon id='users' />
+								Пользователи
+							</Link>
+						</li>
+					)}
+				</ul>
+			</nav>
+			{user && (
+				<nav className='mNavBar__nav'>
+					<ul className='mNavBar__menu !gap-2'>
+						<div className='flex items-center gap-3'>
+							<div className="avatar placeholder">
+								<div className="bg-neutral text-neutral-content w-12 rounded-full">
+									<span>
+										<Icon id='user' className='!w-7 !h-7' />
+									</span>
+								</div>
+							</div>
+							<div className='leading-3'>
+								<p className='text-lg'>
+									{user.shortName}
+								</p>
+								<span className='text-[11px]'>
+									{user.position}
+								</span>
+							</div>
+						</div>
+						<li>
+							<Link to={pageConfig.profile}>
+								<Icon id='profile' />
+								Профиль
+							</Link>
+						</li>
+						<Button className='mBtn_error mt-5' onClick={logoutHandel}>
+							<Icon id='logout' />
+							Выход
+						</Button>
+					</ul>
+				</nav>
+			)}
+		</div>
+	)
 }
