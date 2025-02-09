e93acf37408ce9236f1899c8cc2af3230b1c959f
Mikhail <bolotnikovms@yandex.ru>
2025-02-09 19:13:48 +0500

    feat: upd

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,41 +1,43 @@
 /** @type {import('tailwindcss').Config} */
-import daisyui from "daisyui"
+import daisyui from 'daisyui'
 
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}"
-  ],
-  theme: {
-    extend: {
-      keyframes: {
-        appear: {
-          '0%': { opacity: 0 },
-          '100%': { opacity: 1 },
-        },
-        slideIn: {
-          'from': {
-            transform: { translateY: '-150px' }
-          },
-          'to': {
-            transform: { translateY: '0' },
-          }
-        }
-      },
-      animation: {
-        appear: 'appear 0.5s',
-        slideIn: 'slideIn 0.2s'
-      },
-      top: {
-        'custom': '30px',
-      },
-      right: {
-        '25p': '25px'
-      }
-    },
-  },
-  daisyui: {
-    themes: ['light']
-  },
-  plugins: [daisyui],
+	content: [
+		'./index.html',
+		'./src/**/*.{js,ts,jsx,tsx}'
+	],
+	theme: {
+		extend: {
+			keyframes: {
+				appear: {
+					'0%': { opacity: 0 },
+					'100%': { opacity: 1 },
+				},
+				slideIn: {
+					'from': {
+						transform: { translateY: '-150px' }
+					},
+					'to': {
+						transform: { translateY: '0' },
+					}
+				}
+			},
+			animation: {
+				appear: 'appear 0.5s',
+				slideIn: 'slideIn 0.2s'
+			},
+			top: {
+				'custom': '30px',
+			},
+			right: {
+				'25p': '25px'
+			}
+		},
+	},
+	daisyui: {
+		themes: ['light', 'dark'],
+		styled: true,
+		utils: true,
+	},
+	plugins: [daisyui],
 }
