31886e04d277875ee3a7adfc9e61c83b6093e379
Mikhail <bolotnikovms@yandex.ru>
2025-02-09 19:12:15 +0500

    feat: Тип темы

--- src/types/index.ts
+++ src/types/index.ts
@@ -9,9 +9,10 @@ import { TGsmOperatorData } from './gsm-operator.type'
 import { THeadControllerData, TRespHeadController } from './head-controller.types'
 import { TObjectTypeData, TRespObjectTypes } from './objectType.types'
 import { TRespSubstations, TSubstationData, TSubstationNoteData } from './substation.types'
+import { TTheme } from './theme.type'
 import { TRespTypesKp, TTypeKpData } from './type-kp.types'
 import { TRespTypesWork, TTypeWorkData } from './typeWork.types'
 import { TChangeStatusAccount, TChangeUserRole, TRespUsers, TUserData } from './user.types'
 import { TResponseVoltageClass, TVoltageClassData } from './voltage-class.types'
 
-export type { TChangeStatusAccount, TChangeUserRole, TChannelCategoryData, TChannelData, TChannelEquipmentData, TChannelTypeData, TCompletedWorkData, TDistrictData, TFileUploadData, TGsmOperatorData, THeadControllerData, TRespChannelCategories, TRespChannelingEquipment, TRespChannelTypes, TRespCompletedWork, TRespDistricts, TRespHeadController, TResponseVoltageClass, TRespSubstations, TRespTypesKp, TRespTypesWork, TRespUsers, TSubstationData, TSubstationNoteData, TTypeKpData, TTypeWorkData, TUserData, TVoltageClassData, TObjectTypeData, TRespObjectTypes }
+export type { TChangeStatusAccount, TChangeUserRole, TChannelCategoryData, TChannelData, TChannelEquipmentData, TChannelTypeData, TCompletedWorkData, TDistrictData, TFileUploadData, TGsmOperatorData, THeadControllerData, TRespChannelCategories, TRespChannelingEquipment, TRespChannelTypes, TRespCompletedWork, TRespDistricts, TRespHeadController, TResponseVoltageClass, TRespSubstations, TRespTypesKp, TRespTypesWork, TRespUsers, TSubstationData, TSubstationNoteData, TTypeKpData, TTypeWorkData, TUserData, TVoltageClassData, TObjectTypeData, TRespObjectTypes, TTheme }
