3ffb83187ddec2f8598e201584740a36bf513421
Mikhail <bolotnikovms@yandex.ru>
2025-02-09 20:15:17 +0500

    fix: Поправлена опечатка

--- src/views/substations/components/info/infoParts/channelsInfo/ChannelsInfo.tsx
+++ src/views/substations/components/info/infoParts/channelsInfo/ChannelsInfo.tsx
@@ -6,48 +6,48 @@ import { IPropsChannelsInfo } from '../partsInfo.interfaces'
 import ChannelInfoNote from './ChannelInfoNote'
 
 const ChannelsInfo: FC<IPropsChannelsInfo> = ({ channels }) => {
-  if (!channels?.length) return <p className='substation-info__section-text text-red-500'>Нету данных по каналам для отображения!</p>
+	if (!channels?.length) return <p className='substation-info__section-text text-red-500'>Нет данных по каналам для отображения!</p>
 
-  return (
-    <div className="flex flex-col gap-3">
-      <p className='text-xl text-center font-bold'>Информация по каналам</p>
-      {channels.map(channel => (
-        <div key={channel.id} className='substation-info__channel-wrapper'>
-          <div className='substation-info__channel'>
-            <p className='substation-info__text'>{channel.channel_category_short}: </p>
-            <p className='text-content flex items-center gap-2'>
-              {channel.channel_type}
-            </p>
-            {channel.channel_equipment && (
-              <>
-                <p className='substation-info__text'>Оборудование: </p>
-                <p className='text-content'>{channel.channel_equipment}</p>
-              </>
-            )}
-            {channel.gsm_operator && (
-              <>
-                <p className='substation-info__text'>GSM оператор: </p>
-                <p className='text-content'>{channel.gsm_operator}</p>
-              </>
-            )}
-            {channel.ipAddress && (
-              <>
-                <p className='substation-info__text'>IP адрес канала: </p>
-                <p className='text-content'>{channel.ipAddress}</p>
-              </>
-            )}
-            {channel.note && (
-              <ChannelInfoNote channel={channel} />
-            )}
-          </div>
-          <div className="w-none">
-            <ChannelControlMenu channel={channel} />
-          </div>
-        </div>
-      ))
-      }
-    </div >
-  )
+	return (
+		<div className="flex flex-col gap-3">
+			<p className='text-xl text-center font-bold'>Информация по каналам</p>
+			{channels.map(channel => (
+				<div key={channel.id} className='substation-info__channel-wrapper'>
+					<div className='substation-info__channel'>
+						<p className='substation-info__text'>{channel.channel_category_short}: </p>
+						<p className='text-content flex items-center gap-2'>
+							{channel.channel_type}
+						</p>
+						{channel.channel_equipment && (
+							<>
+								<p className='substation-info__text'>Оборудование: </p>
+								<p className='text-content'>{channel.channel_equipment}</p>
+							</>
+						)}
+						{channel.gsm_operator && (
+							<>
+								<p className='substation-info__text'>GSM оператор: </p>
+								<p className='text-content'>{channel.gsm_operator}</p>
+							</>
+						)}
+						{channel.ipAddress && (
+							<>
+								<p className='substation-info__text'>IP адрес канала: </p>
+								<p className='text-content'>{channel.ipAddress}</p>
+							</>
+						)}
+						{channel.note && (
+							<ChannelInfoNote channel={channel} />
+						)}
+					</div>
+					<div className="w-none">
+						<ChannelControlMenu channel={channel} />
+					</div>
+				</div>
+			))
+			}
+		</div >
+	)
 }
 
 export default ChannelsInfo
