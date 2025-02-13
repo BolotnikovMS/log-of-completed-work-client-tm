eb8a1b0d65aeabaf87ff4b5bd87b3151e482d591
Mikhail <bolotnikovms@yandex.ru>
2025-02-13 21:18:02 +0500

    style: Поправлены отступы

--- src/components/pagination/Pagination.tsx
+++ src/components/pagination/Pagination.tsx
@@ -3,28 +3,27 @@ import { Button, Icon } from '..'
 import { IPropsPagination } from './pagination.interface'
 
 const Pagination: FC<IPropsPagination> = ({ meta, page, setPage }) => {
-  const { first_page, last_page } = meta
+	const { first_page, last_page } = meta
 
-  if (first_page === last_page) return null
+	if (first_page === last_page) return null
 
-
-  return (
-    <div className='flex justify-center'>
-      <div className="join">
-        <Button className='join-item' onClick={() => setPage(Math.max(page - 1, 1))} disabled={page === first_page}>
-          <Icon id='arrow-left' />
-          Назад
-        </Button>
-        <Button className='join-item' disabled>
-          {page}
-        </Button>
-        <Button className='join-item' onClick={() => setPage(Math.min(page + 1, last_page))} disabled={page === last_page}>
-          Дальше
-          <Icon id='arrow-right' />
-        </Button>
-      </div>
-    </div>
-  )
+	return (
+		<div className='flex justify-center'>
+			<div className="join">
+				<Button className='join-item' onClick={() => setPage(Math.max(page - 1, 1))} disabled={page === first_page}>
+					<Icon id='arrow-left' />
+					Назад
+				</Button>
+				<Button className='join-item' disabled>
+					{page}
+				</Button>
+				<Button className='join-item' onClick={() => setPage(Math.min(page + 1, last_page))} disabled={page === last_page}>
+					Дальше
+					<Icon id='arrow-right' />
+				</Button>
+			</div>
+		</div>
+	)
 }
 
 export default Pagination
