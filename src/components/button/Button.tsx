c74b825f49b22ff8afee3fa2905d3e3810bd0ee3
Mikhail <bolotnikovms@yandex.ru>
2025-02-16 15:25:33 +0500

    style: Поправлены отступы

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -4,9 +4,9 @@ import { IPropsButton } from './button.interface'
 import './button.scss'
 
 export const Button: FC<IPropsButton> = ({ children, className, ...attributes }) => {
-  return (
-    <button className={cx('mBtn', className, attributes.disabled && 'btn-disabled')} {...attributes}>
-      {children}
-    </button>
-  )
+	return (
+		<button className={cx('mBtn', className, attributes.disabled && 'btn-disabled')} {...attributes}>
+			{children}
+		</button>
+	)
 }
