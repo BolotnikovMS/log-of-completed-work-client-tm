31a7ce970a51c307e827fb622a040f7ad4945900
Mikhail <bolotnikovms@yandex.ru>
2025-02-16 15:27:00 +0500

    feat: Добавлен мини профиль

--- src/components/navBar/NavBar.tsx
+++ src/components/navBar/NavBar.tsx
@@ -1,19 +1,17 @@
 import React, { type FC } from 'react'
 import { Link } from 'react-router-dom'
-import { Button, Icon, ToggleTheme } from '..'
+import { Icon } from '..'
 import { pageConfig } from '../../config/pages.config'
 import { menuItemData } from '../../constants'
 import { ERoles } from '../../enums/roles.enum'
 import { checkRole } from '../../helpers'
-import { useLogout } from '../../hooks'
 import { useAuthStore } from '../../store/auth'
 import './navbar.scss'
+import Profile from './profile/Profile'
 
 export const NavBar: FC = () => {
 	const userAuthStore = useAuthStore()
 	const user = userAuthStore.authUser
-	const { logout } = useLogout()
-	const logoutHandel = () => logout.mutate()
 
 	return (
 		<div className='mNavBar'>
@@ -26,7 +24,6 @@ export const NavBar: FC = () => {
 					<span className='text-content text-center'>
 						Журнал выполненных работ по ТМ
 					</span>
-					<ToggleTheme />
 				</div>
 				<ul className='mNavBar__menu'>
 					<li>
@@ -86,33 +83,7 @@ export const NavBar: FC = () => {
 			{user && (
 				<nav className='mNavBar__nav'>
 					<ul className='mNavBar__menu !gap-2'>
-						<div className='flex items-center gap-3'>
-							<div className="avatar placeholder">
-								<div className="bg-neutral text-neutral-content w-12 rounded-full">
-									<span>
-										<Icon id='user' className='!w-7 !h-7' />
-									</span>
-								</div>
-							</div>
-							<div className='leading-3'>
-								<p className='text-lg'>
-									{user.shortName}
-								</p>
-								<span className='text-[11px]'>
-									{user.position}
-								</span>
-							</div>
-						</div>
-						<li>
-							<Link to={pageConfig.profile}>
-								<Icon id='profile' />
-								Профиль
-							</Link>
-						</li>
-						<Button className='mBtn_error mt-5' onClick={logoutHandel}>
-							<Icon id='logout' />
-							Выход
-						</Button>
+						<Profile user={user} />
 					</ul>
 				</nav>
 			)}
