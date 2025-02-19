6e48aee60fe8502ffaedea02936354a9b49d25b3
Mikhail <bolotnikovms@yandex.ru>
2025-02-19 20:00:29 +0500

    ui: Переделан блок профиля

--- src/components/navBar/profile/Profile.tsx
+++ src/components/navBar/profile/Profile.tsx
@@ -1,7 +1,5 @@
 import { memo, useCallback, type FC } from 'react'
-import { Link } from 'react-router-dom'
-import { Button, Dropdown, ToggleTheme } from '../..'
-import { pageConfig } from '../../../config/pages.config'
+import { Button, ToggleTheme, Tooltip } from '../..'
 import { useLogout } from '../../../hooks'
 import Icon from '../../icon/Icon'
 import Avatar from './avatar/Avatar'
@@ -13,36 +11,23 @@ const Profile: FC<IPropsProfile> = memo(({ user }) => {
 	const logoutHandler = useCallback(() => logout.mutate(), [logout])
 
 	return (
-		<div className='flex items-center justify-between gap-1'>
-			<div className="flex items-center gap-2">
-				<Avatar />
-				<UserInfo shortName={user.shortName} position={user.position} />
-			</div>
-			<Dropdown
-				children={<Icon id='dots' />}
-				classBtnTrigger='btn-circle'
-				classMenu='dropdown-top dropdown-end'
-				menuItems={[
-					<div className='flex justify-center'>
-						<ToggleTheme />
-					</div>,
-					<Link
-						to={pageConfig.profile}
-						className="flex items-center gap-2"
-					>
-						<Icon id='profile' />
-						Профиль
-					</Link>,
+		<div className='flex flex-col items-center gap-4'>
+			<ToggleTheme />
+			<div className="w-full flex gap-3 justify-between">
+				<div className="flex items-center gap-2">
+					<Avatar />
+					<UserInfo shortName={user.shortName} position={user.position} />
+				</div>
+				<Tooltip text='Выйти из системы'>
 					<Button
-						className='mBtn_error mt-4'
+						className='btn-circle mBtn_error'
 						onClick={logoutHandler}
 						aria-label="Выйти из системы"
 					>
-						<Icon id='logout' className='!w-3 !h-3' />
-						Выход
+						<Icon id='logout' />
 					</Button>
-				]}
-			/>
+				</Tooltip>
+			</div>
 		</div>
 	)
 })
--- src/components/navBar/profile/userInfo/UserInfo.tsx
+++ src/components/navBar/profile/userInfo/UserInfo.tsx
@@ -1,11 +1,18 @@
 import { memo, type FC } from 'react'
+import { Link } from 'react-router-dom'
+import { Tooltip } from '../../..'
+import { pageConfig } from '../../../../config/pages.config'
 import { IPropsUserInfo } from './userInfo.interface'
 
 const UserInfo: FC<IPropsUserInfo> = memo(({ shortName, position }) => {
 	return (
 		<div className='leading-3'>
 			<p className='text-sm font-bold' aria-label="Имя пользователя">
-				{shortName}
+				<Tooltip text='Перейти в профиль'>
+					<Link to={pageConfig.profile}>
+						{shortName}
+					</Link>
+				</Tooltip>
 			</p>
 			<span className='text-[12px]' aria-label="Должность пользователя">
 				{position}
