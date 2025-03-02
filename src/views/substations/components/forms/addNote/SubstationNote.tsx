fc3bdce1d8939fc218e090d3934b66306db27af3
Mikhail <bolotnikovms@yandex.ru>
2025-03-02 16:50:24 +0500

    fix: Правки
    Поправлен интерфейс с ISubstation -> ISubstationInfo. А также отступы.

--- src/views/substations/components/forms/addNote/SubstationNote.tsx
+++ src/views/substations/components/forms/addNote/SubstationNote.tsx
@@ -3,58 +3,59 @@ import { type FC } from 'react'
 import { SubmitHandler, useForm } from 'react-hook-form'
 import { Button, Error, Group, Loader, Textarea } from '../../../../../components'
 import { useUpdateSubstationNote } from '../../../../../hooks'
-import { IPropsForm, IPropsMutation, ISubstation } from '../../../../../interfaces'
+import { IPropsForm, IPropsMutation, ISubstationInfo } from '../../../../../interfaces'
 import { TSubstationNoteData } from '../../../../../types'
 import { validationSchema } from './substationNote.validation'
 
-const SubstationNote: FC<IPropsForm<ISubstation>> = ({ data: substation, isEdited, setIsEdited, toggleModal }) => {
-  const { register, handleSubmit, formState: { errors, isValid }, reset } = useForm<TSubstationNoteData>({
-    mode: 'all',
-    defaultValues: {
-      note: substation?.note
-    },
-    resolver: yupResolver(validationSchema)
-  })
-  const { mutateAsync: updateSubstation, isError: isErrorUpdate, error: errorUpdate, isPending: isPendingUpdate } = useUpdateSubstationNote()
+const SubstationNote: FC<IPropsForm<ISubstationInfo>> = ({ data: substation, isEdited, setIsEdited, toggleModal }) => {
+	const { register, handleSubmit, formState: { errors, isValid }, reset } = useForm<TSubstationNoteData>({
+		mode: 'all',
+		defaultValues: {
+			note: substation?.note
+		},
+		resolver: yupResolver(validationSchema)
+	})
+	const { mutateAsync: updateSubstation, isError: isErrorUpdate, error: errorUpdate, isPending: isPendingUpdate } = useUpdateSubstationNote()
 
-  const handleMutation = async ({ data, mutateFn, id }: IPropsMutation<TSubstationNoteData>) => {
-    await mutateFn(id ? { id, data } : data)
+	const handleMutation = async ({ data, mutateFn, id }: IPropsMutation<TSubstationNoteData>) => {
+		await mutateFn(id ? { id, data } : data)
 
-    reset()
-    toggleModal()
-    if (isEdited && setIsEdited) setIsEdited(false)
-  }
-  const submitUpdate: SubmitHandler<TSubstationNoteData> = data => {
-    if (!substation?.id) return null
+		reset()
+		toggleModal()
+		if (isEdited && setIsEdited) setIsEdited(false)
+	}
+	const submitUpdate: SubmitHandler<TSubstationNoteData> = data => {
+		if (!substation?.id) return null
 
-    handleMutation({ data, mutateFn: updateSubstation, id: substation.id })
-  }
-  const errorMessage = (isErrorUpdate && errorUpdate !== null) && <Error error={errorUpdate} />
+		handleMutation({ data, mutateFn: updateSubstation, id: substation.id })
+	}
+	const errorMessage = (isErrorUpdate && errorUpdate !== null) && <Error error={errorUpdate} />
 
-  if (isPendingUpdate) return <Loader />
+	if (isPendingUpdate) return <Loader />
 
-  return (
-    <div className="work-log__form">
-      {errorMessage}
-      <form className="form" onSubmit={handleSubmit(submitUpdate)}>
-        <Group>
-          <Textarea
-            label='Примечание'
-            name='note'
-            register={register}
-            className='!h-28'
-            error={errors.note?.message}
-            placeholder='Введите примечание...'
-          />
-        </Group>
-        <div className="form__btns">
-          <Button disabled={!isValid} className='mBtn_outline-green'>
-            Сохранить
-          </Button>
-        </div>
-      </form>
-    </div>
-  )
+	return (
+		<div className="work-log__form">
+			{errorMessage}
+			<form className="form" onSubmit={handleSubmit(submitUpdate)}>
+				<Group>
+					<Textarea
+						label='Примечание'
+						name='note'
+						register={register}
+						className='!h-28'
+						error={errors.note?.message}
+						placeholder='Введите примечание...'
+						aria-label='Поле ввода примечания'
+					/>
+				</Group>
+				<div className="form__btns">
+					<Button disabled={!isValid} className='mBtn_outline-green' aria-label='Сохранить'>
+						Сохранить
+					</Button>
+				</div>
+			</form>
+		</div>
+	)
 }
 
 export default SubstationNote
