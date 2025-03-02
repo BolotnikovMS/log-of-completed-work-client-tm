88076e2f29faf6572faa8aa5fc9d5692c8b40043
Mikhail <bolotnikovms@yandex.ru>
2025-03-02 19:41:11 +0500

    feat: Добавлен тип

--- src/types/file.types.ts
+++ src/types/file.types.ts
@@ -1,3 +1,5 @@
 import { IFile } from '../interfaces'
 
 export type TFileUploadData = {file: File} & Pick<IFile, 'substationId' | 'typeFile'>
+
+export type TFileList = Omit<IFile, 'userId' | 'substationId' | 'typeFile'>
--- src/types/index.ts
+++ src/types/index.ts
@@ -4,7 +4,7 @@ import { TChannelEquipmentData, TRespChannelingEquipment } from './channelingEqu
 import { TChannelCategoryData, TRespChannelCategories } from './channelСategory.types'
 import { TCompletedWorkData, TRespCompletedWork } from './completed-work.types'
 import { TDistrictData, TRespDistricts } from './district.types'
-import { TFileUploadData } from './file.types'
+import { TFileList, TFileUploadData } from './file.types'
 import { TGsmOperatorData } from './gsm-operator.type'
 import { THeadControllerData, TRespHeadController } from './head-controller.types'
 import { TObjectTypeData, TRespObjectTypes } from './objectType.types'
@@ -15,4 +15,4 @@ import { TRespTypesWork, TTypeWorkData } from './typeWork.types'
 import { TChangeStatusAccount, TChangeUserRole, TRespUsers, TUserData } from './user.types'
 import { TResponseVoltageClass, TVoltageClassData } from './voltage-class.types'
 
-export type { TChangeStatusAccount, TChangeUserRole, TChannelCategoryData, TChannelData, TChannelEquipmentData, TChannelTypeData, TCompletedWorkData, TDistrictData, TFileUploadData, TGsmOperatorData, THeadControllerData, TRespChannelCategories, TRespChannelingEquipment, TRespChannelTypes, TRespCompletedWork, TRespDistricts, TRespHeadController, TResponseVoltageClass, TRespSubstations, TRespTypesKp, TRespTypesWork, TRespUsers, TSubstationData, TSubstationNoteData, TTypeKpData, TTypeWorkData, TUserData, TVoltageClassData, TObjectTypeData, TRespObjectTypes, TTheme }
+export type { TChangeStatusAccount, TChangeUserRole, TChannelCategoryData, TChannelData, TChannelEquipmentData, TChannelTypeData, TCompletedWorkData, TDistrictData, TFileUploadData, TGsmOperatorData, THeadControllerData, TRespChannelCategories, TRespChannelingEquipment, TRespChannelTypes, TRespCompletedWork, TRespDistricts, TRespHeadController, TResponseVoltageClass, TRespSubstations, TRespTypesKp, TRespTypesWork, TRespUsers, TSubstationData, TSubstationNoteData, TTypeKpData, TTypeWorkData, TUserData, TVoltageClassData, TObjectTypeData, TRespObjectTypes, TTheme, TFileList }
