b0df37d3bc0b68d2efe21ffb09bfe5391bf4f9cf
Mikhail <bolotnikovms@yandex.ru>
2025-03-02 19:41:26 +0500

    style: Поправлены отступы

--- src/views/substations/components/info/infoParts/slider/SliderPhoto.tsx
+++ src/views/substations/components/info/infoParts/slider/SliderPhoto.tsx
@@ -4,34 +4,36 @@ import { urlFile } from '../../../../../../constants'
 import { IPropsPartsInfo } from '../partsInfo.interfaces'
 
 const SliderPhoto: FC<IPropsPartsInfo> = ({ substation }) => {
-  if (!substation) return <p className='substation-info__section-text text-red-500'>Нету данных для отображения!</p>
-  const toggleFullscreen = (e: MouseEvent<HTMLDivElement, MouseEvent>) => {
-    const target = e.currentTarget as HTMLDivElement
+	if (!substation) return null
+	const toggleFullscreen = (e: MouseEvent<HTMLDivElement, MouseEvent>) => {
+		const target = e.currentTarget as HTMLDivElement
 
-    if (target.requestFullscreen) {
-      if (!document.fullscreenElement) {
-        target.requestFullscreen().catch((err) => console.error('Error entering fullscreen:', err))
-      } else {
-        document.exitFullscreen().catch((err) => console.error('Error exiting fullscreen:', err))
-      }
-    } else {
-      console.warn('Fullscreen API is not supported in this browser.')
-    }
-  }
+		if (target.requestFullscreen) {
+			if (!document.fullscreenElement) {
+				target.requestFullscreen().catch((err) => console.error('Error entering fullscreen:', err))
+			} else {
+				document.exitFullscreen().catch((err) => console.error('Error exiting fullscreen:', err))
+			}
+		} else {
+			console.warn('Fullscreen API is not supported in this browser.')
+		}
+	}
 
-  return (
-    <div className="substation-info__imgs">
-      {substation.files_photos_ps?.length ? (
-        <CustomSlider>
-          {substation.files_photos_ps.map(photo => (
-            <div key={photo.id} data-src={`${urlFile}${photo.filePath}`} onClick={(e) => toggleFullscreen(e)} />
-          ))}
-        </CustomSlider>
-      ) : (
-        <Icon id='img-off' className='!w-96 !h-96' />
-      )}
-    </div>
-  )
+	return (
+		<div className="substation-info__imgs">
+			{substation.files_photos_ps?.length ? (
+				<>
+					<CustomSlider>
+						{substation.files_photos_ps.map(photo => (
+							<div key={photo.id} data-src={`${urlFile}${photo.filePath}`} onClick={(e) => toggleFullscreen(e)} />
+						))}
+					</CustomSlider>
+				</>
+			) : (
+				<Icon id='img-off' className='!w-96 !h-96' />
+			)}
+		</div>
+	)
 }
 
 export default SliderPhoto
