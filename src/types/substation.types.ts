864251025c4191ae1f85833a99c2b1dd62dcb4c9
Mikhail <bolotnikovms@yandex.ru>
2025-03-02 15:38:24 +0500

    fix: Правки

--- src/types/substation.types.ts
+++ src/types/substation.types.ts
@@ -1,7 +1,7 @@
-import { IMeta, ISubstation } from '../interfaces'
+import { IMeta, ISubstation, ISubstationList } from '../interfaces'
 
-export type TSubstationData = Omit<ISubstation, 'id' | 'fullNameSubstation' | 'voltage_class' | 'district' | 'type_kp' | 'head_controller' | 'files_photos_ps' | 'files_backups' | 'other_files' | 'channels' | 'numberCompletedWorks' |'object_type'>
+export type TSubstationData = Omit<ISubstation, 'id'>
 
 export type TSubstationNoteData = Pick<ISubstation, 'note'>
 
-export type TRespSubstations = { meta: IMeta, data: ISubstation[] }
+export type TRespSubstations = { meta: IMeta, data: ISubstationList[] }
