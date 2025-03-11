80e9f39b2129648f756923b2a376590afe45ca6c
Mikhail <bolotnikovms@yandex.ru>
2025-03-11 15:07:55 +0500

    refactor: Рефакторинг компонента

--- src/views/user/components/table/UsersTable.tsx
+++ src/views/user/components/table/UsersTable.tsx
@@ -1,102 +1,61 @@
 import { ColumnDef } from '@tanstack/react-table'
-import { useMemo, useState, type FC } from 'react'
+import { useMemo, type FC } from 'react'
 import { ChangeStatusAccountForm } from '..'
-import { BasicTable, Button, ChangePasswordForm, Error, Icon, InfoMessage, Loader, Modal, Tooltip } from '../../../../components'
+import { BasicTable, Error, Icon, InfoMessage, Loader } from '../../../../components'
 import { ERoles } from '../../../../enums/roles.enum'
 import { checkRole } from '../../../../helpers'
-import { useModal, useUsers } from '../../../../hooks'
+import { useUsers } from '../../../../hooks'
 import { IUser } from '../../../../interfaces'
 import { useAuthStore } from '../../../../store/auth'
-import ChangeUserRole from '../form/changeUserRole/ChangeUserRole'
+import ControlMenu from './parts/control/ControlMenu'
 
 const UsersTable: FC = () => {
-  const { authUser } = useAuthStore()
-  const isAdmin = checkRole(authUser, [ERoles.Admin])
-  const { data, error, isError, isLoading } = useUsers({})
-  const { isModal, toggleModal } = useModal()
-  const { isModal: isModalChangeRole, toggleModal: toggleModalChangeRole } = useModal()
-  const [currentUser, setCurrentUser] = useState<IUser | undefined>(undefined)
-  const columns = useMemo<ColumnDef<IUser>[]>(() => [
-    {
-      header: 'Ф.И.О.',
-      accessorKey: 'shortName',
-    },
-    {
-      header: 'Должность',
-      accessorKey: 'position',
-    },
-    {
-      header: 'УЗ активна',
-      accessorKey: 'active',
-      cell: ({ row }) => (
-        isAdmin && <ChangeStatusAccountForm active={row.original.active} id={row.original.id} />
-      )
-    },
-    {
-      header: 'Username',
-      accessorKey: 'username',
-    },
-    {
-      header: 'email',
-      accessorKey: 'email',
-    },
-    {
-      header: 'Роль',
-      cell: ({ row }) => (
-        (isAdmin && row.original.active) && (
-          <div className='table-cell-row'>
-            {row.original.role?.name}
-            <Tooltip text='Изменить роль пользователя'>
-              <Button className='btn-circle' onClick={() => { toggleModalChangeRole(), setCurrentUser(row.original) }}>
-                <Icon id='user-switch' />
-              </Button>
-            </Tooltip>
-          </div>
-        )
-      )
-    },
-    {
-      header: () => <Icon id='setting' />,
-      enableSorting: false,
-      accessorKey: 'setting',
-      cell: ({ row }) => (
-        (isAdmin && row.original.active) && (
-          <div className='table-cell-row'>
-          <Tooltip text='Изменить пароль' className='!tooltip-left'>
-            <Button className='btn-circle' onClick={() => { toggleModal(), setCurrentUser(row.original) }}>
-              <Icon id='reset-password' />
-            </Button>
-          </Tooltip>
-          </div>
-        )
-      )
-    }
-  ], [])
+	const { authUser } = useAuthStore()
+	const isAdmin = checkRole(authUser, [ERoles.Admin])
+	const { data, error, isError, isLoading } = useUsers({})
+	const columns = useMemo<ColumnDef<IUser>[]>(() => [
+		{
+			header: 'Ф.И.О.',
+			accessorKey: 'shortName',
+		},
+		{
+			header: 'Username',
+			accessorKey: 'username',
+		},
+		{
+			header: 'Роль',
+			accessorKey: 'role',
+		},
+		{
+			header: 'УЗ активна',
+			accessorKey: 'active',
+			cell: ({ row }) => (
+				isAdmin && <ChangeStatusAccountForm active={row.original.active} id={row.original.id} />
+			)
+		},
+		{
+			header: 'email',
+			accessorKey: 'email',
+		},
+		{
+			header: () => <Icon id='setting' />,
+			enableSorting: false,
+			accessorKey: 'setting',
+			cell: ({ row }) => (
+				(isAdmin && row.original.active) && (
+					<ControlMenu userId={row.original.id} />
+				)
+			)
+		}
+	], [isAdmin])
 
-  if (isError && error) return <Error error={error} />
+	if (isError && error) return <Error error={error} />
+	if (isLoading) return <Loader />
+	if (!data?.data) return <InfoMessage text='Пока нет данных для отображения!' />
 
-  if (isLoading) return <Loader />
-
-  if (!data?.data) return <InfoMessage text='Пока нет данных для отображения!' />
-
-  return (
-    <>
-      <BasicTable data={data.data} columns={columns} size={15} search />
-      <Modal
-        visible={isModal}
-        title='Форма сброса пароля'
-        content={<ChangePasswordForm toggleModal={toggleModal} isResetPassword user={currentUser} />}
-        onToggle={toggleModal}
-      />
-      <Modal
-        visible={isModalChangeRole}
-        title='Форма изменения роли пользователя'
-        classDialog='!h-[350px]'
-        content={<ChangeUserRole toggleModal={toggleModalChangeRole} data={currentUser} />}
-        onToggle={toggleModalChangeRole}
-      />
-    </>
-  )
+	return (
+		<BasicTable data={data.data} columns={columns} size={15} search />
+	)
 }
 
 export default UsersTable
