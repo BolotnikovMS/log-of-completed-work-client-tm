035bcf3691f6cc219662e740afb88d3f7f513bce
Mikhail <bolotnikovms@yandex.ru>
2025-03-11 15:03:11 +0500

    feat: Добавлены getUsers, getUserById

--- src/services/user/user.service.ts
+++ src/services/user/user.service.ts
@@ -2,11 +2,25 @@ import { AxiosResponse } from 'axios'
 import { instance } from '../../api/axios.api'
 import { url } from '../../constants'
 import { IChangePassword, IQueryParams, IUser, IUserRole } from '../../interfaces'
-import { TChangeStatusAccount, TRespUsers, TUserData } from '../../types'
+import { TChangeStatusAccount, TRespUsers, TUserData, TUserShort } from '../../types'
 
 export const UserService = {
-  async getUsers({ limit, page, active, cleanUser }: IQueryParams): Promise<TRespUsers> {
-    const { data } = await instance.get<TRespUsers>(`${url}/users`, {
+  async getUsers({ limit, page, active, cleanUser }: IQueryParams): Promise<TRespUsers<IUser[]>> {
+    const { data } = await instance.get<TRespUsers<IUser[]>>(`${url}/users`, {
+      params: { limit, page, active, cleanUser },
+    })
+
+    return data
+  },
+
+  async getUserById(idUser: number): Promise<IUser> {
+    const {data} = await instance.get<IUser>(`${url}/users/${idUser}`)
+
+    return data
+  },
+
+  async getShortUsers({ limit, page, active, cleanUser }: IQueryParams): Promise<TRespUsers<TUserShort[]>> {
+    const { data } = await instance.get<TRespUsers<TUserShort[]>>(`${url}/users/short`, {
       params: { limit, page, active, cleanUser },
     })
 
