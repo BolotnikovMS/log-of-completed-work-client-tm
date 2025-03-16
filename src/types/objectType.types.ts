e0fe9767c65bad7c3e7d7f5534b3f67876feb8c3
Mikhail <bolotnikovms@yandex.ru>
2025-03-16 17:54:19 +0500

    feat: Добавлен тип TObjectTypeShort

--- src/types/objectType.types.ts
+++ src/types/objectType.types.ts
@@ -2,4 +2,6 @@ import { IMeta, IObjectType } from '../interfaces'
 
 export type TObjectTypeData = Omit<IObjectType, 'id'>
 
-export type TRespObjectTypes = { meta: IMeta, data: IObjectType[] }
+export type TRespObjectTypes = { meta: IMeta, data: TObjectTypeShort[] }
+
+export type TObjectTypeShort = Omit<IObjectType, 'shortName'>
