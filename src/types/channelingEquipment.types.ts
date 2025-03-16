eb713f770f70043550d9467efeb3c3bf0eced51e
Mikhail <bolotnikovms@yandex.ru>
2025-03-16 19:07:30 +0500

    fix: Возвращено IChannelEquipment

--- src/types/channelingEquipment.types.ts
+++ src/types/channelingEquipment.types.ts
@@ -2,6 +2,4 @@ import { IChannelEquipment, IMeta } from '../interfaces'
 
 export type TChannelEquipmentData = Omit<IChannelEquipment, 'id'>
 
-export type TRespChannelingEquipment = { meta: IMeta, data: TChannelEquipment[] }
-
-export type TChannelEquipment = Omit<IChannelEquipment, 'channelTypeId'>
+export type TRespChannelingEquipment = { meta: IMeta, data: IChannelEquipment[] }
