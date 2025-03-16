68a72e8391100a0e47964478bee82510ff1b9d28
Mikhail <bolotnikovms@yandex.ru>
2025-03-16 18:52:43 +0500

    feat: Добавлен тип TChannelEquipment

--- src/types/channelingEquipment.types.ts
+++ src/types/channelingEquipment.types.ts
@@ -2,4 +2,6 @@ import { IChannelEquipment, IMeta } from '../interfaces'
 
 export type TChannelEquipmentData = Omit<IChannelEquipment, 'id'>
 
-export type TRespChannelingEquipment = { meta: IMeta, data: IChannelEquipment[] }
+export type TRespChannelingEquipment = { meta: IMeta, data: TChannelEquipment[] }
+
+export type TChannelEquipment = Omit<IChannelEquipment, 'channelTypeId'>
