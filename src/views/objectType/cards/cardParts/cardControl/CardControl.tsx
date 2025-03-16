c77694264050155b88d837bba5ef19bff54f6c9e
Mikhail <bolotnikovms@yandex.ru>
2025-03-16 17:56:59 +0500

    refactor: Обновлен компонент `CardControl` для обработки удаления ObjectType и отображения соответствующего модального содержимого в зависимости от состояний загрузки, ошибки или успешного завершения.

--- src/views/objectType/cards/cardParts/cardControl/CardControl.tsx
+++ src/views/objectType/cards/cardParts/cardControl/CardControl.tsx
@@ -1,19 +1,21 @@
-import { useCallback, useState, type FC } from 'react'
+import { memo, useCallback, useState, type FC } from 'react'
 import { ObjectTypeForm } from '../../..'
-import { Button, Dropdown, Icon, Modal } from '../../../../../components'
+import { Button, Dropdown, Error, Icon, Loader, Modal } from '../../../../../components'
 import { ERoles } from '../../../../../enums/roles.enum'
 import { checkRole } from '../../../../../helpers'
-import { useDeleteObjectType, useModal } from '../../../../../hooks'
-import { IObjectType, IPropsCardControl } from '../../../../../interfaces'
+import { useDeleteObjectType, useModal, useObjectType } from '../../../../../hooks'
 import { useAuthStore } from '../../../../../store/auth'
 
-const CardControl: FC<IPropsCardControl<IObjectType>> = ({ data: objectType }) => {
+const CardControl: FC<{ objectTypeId: number }> = memo(({ objectTypeId }) => {
 	const { authUser } = useAuthStore()
 	const isAdmin = checkRole(authUser, [ERoles.Admin])
 	const isAdminOrModerator = checkRole(authUser, [ERoles.Moderator, ERoles.Admin])
 	const { isModal, toggleModal } = useModal()
 	const [isEdited, setIsEdited] = useState<boolean>(false)
 	const { deleteObjectType } = useDeleteObjectType()
+	const { data, error, isError, isLoading } = useObjectType(objectTypeId, {
+		enabled: isModal
+	})
 	const handleDelete = useCallback((id: number): void | null => {
 		const answer = confirm('Подтвердите удаление записи.')
 
@@ -25,44 +27,48 @@ const CardControl: FC<IPropsCardControl<IObjectType>> = ({ data: objectType }) =
 		setIsEdited(!isEdited)
 		toggleModal()
 	}, [isEdited, toggleModal])
+	const modalContent =
+		isLoading ?
+			<Loader /> :
+			isError ?
+				<Error error={error} /> :
+				<ObjectTypeForm
+					data={data}
+					isEdited={isEdited}
+					setIsEdited={setIsEdited}
+					toggleModal={toggleModal}
+				/>
+
+	if (!isAdminOrModerator) return null
 
 	return (
 		<>
-			{isAdminOrModerator && (
-				<Dropdown
-					children={<Icon id='setting' className='icon__setting' />}
-					classBtnTrigger='btn-circle'
-					menuItems={[
-						isAdminOrModerator && (
-							<Button className='!justify-start' onClick={() => { handleEdit() }}>
-								<Icon id='edit' />
-								Редактировать
-							</Button>
-						),
-						isAdmin && (
-							<Button className='!justify-start mBtn_error' onClick={() => { handleDelete(objectType.id) }}>
-								<Icon id='delete' />
-								Удалить
-							</Button>
-						)
-					]}
-				/>
-			)}
+			<Dropdown
+				children={<Icon id='setting' className='icon__setting' />}
+				classBtnTrigger='btn-circle'
+				menuItems={[
+					isAdminOrModerator && (
+						<Button className='!justify-start' onClick={handleEdit}>
+							<Icon id='edit' />
+							Редактировать
+						</Button>
+					),
+					isAdmin && (
+						<Button className='!justify-start mBtn_error' onClick={() => { handleDelete(objectTypeId) }}>
+							<Icon id='delete' />
+							Удалить
+						</Button>
+					)
+				]}
+			/>
 			<Modal
 				visible={isModal}
 				title='Редактирование записи'
 				onToggle={() => { toggleModal(), setIsEdited(false) }}
-				content={
-					<ObjectTypeForm
-						data={objectType}
-						isEdited={isEdited}
-						setIsEdited={setIsEdited}
-						toggleModal={toggleModal}
-					/>
-				}
+				content={modalContent}
 			/>
 		</>
 	)
-}
+})
 
 export default CardControl
