55a4d95822c1a13006f9b68c313211d168b62e1f
Mikhail <bolotnikovms@yandex.ru>
2025-03-16 16:27:13 +0500

    feat: Обновлен `TRespDistricts` для включения дополнительного свойства `TDistrictShort`, которое является более кратким представлением данных о районах.

--- src/types/district.types.ts
+++ src/types/district.types.ts
@@ -2,4 +2,6 @@ import { IDistrict, IMeta } from '../interfaces'
 
 export type TDistrictData = Omit<IDistrict, 'id'>
 
-export type TRespDistricts = { meta: IMeta, data: IDistrict[] }
+export type TRespDistricts = { meta: IMeta, data: TDistrictShort[] }
+
+export type TDistrictShort = Omit<IDistrict, 'shortName'>
