e506f749e51e8220f017eee7c4297a5241b4d749
Mikhail <bolotnikovms@yandex.ru>
2025-03-23 14:30:53 +0500

    fix: Поправлен id

--- src/components/filters/search/Search.tsx
+++ src/components/filters/search/Search.tsx
@@ -5,37 +5,37 @@ import { transliterate } from '../../../helpers'
 import { IPropsSearch } from './search.interface'
 
 const Search: FC<IPropsSearch> = ({ name, placeholderText = 'Поиск...', classSearch }) => {
-  const [searchParams, setSearchParams] = useSearchParams()
-  const [searchValue, setSearchValue] = useState<string>(searchParams.get('search') || '')
+	const [searchParams, setSearchParams] = useSearchParams()
+	const [searchValue, setSearchValue] = useState<string>(searchParams.get('search') || '')
 
-  const handleSearch = useCallback(({ target }: ChangeEvent<HTMLInputElement>) => {
-    const substationName = target.value
+	const handleSearch = useCallback(({ target }: ChangeEvent<HTMLInputElement>) => {
+		const substationName = target.value
 
-    setSearchValue(substationName)
+		setSearchValue(substationName)
 
-    if (substationName.length >= 3) {
-      searchParams.set('search', transliterate(substationName))
-    } else {
-      searchParams.delete('search')
-    }
+		if (substationName.length >= 3) {
+			searchParams.set('search', transliterate(substationName))
+		} else {
+			searchParams.delete('search')
+		}
 
-    setSearchParams(searchParams)
-  }, [searchParams, setSearchParams])
+		setSearchParams(searchParams)
+	}, [searchParams, setSearchParams])
 
-  return (
-    <Input
-      name={name}
-      type='search'
-      onChange={(e) => handleSearch(e)}
-      value={searchValue}
-      autoComplete='off'
-      classInput={classSearch}
-      placeholder={placeholderText}
-      iconLeft={
-        <Icon id='search  ' />
-      }
-    />
-  )
+	return (
+		<Input
+			name={name}
+			type='search'
+			onChange={(e) => handleSearch(e)}
+			value={searchValue}
+			autoComplete='off'
+			classInput={classSearch}
+			placeholder={placeholderText}
+			iconLeft={
+				<Icon id='search' />
+			}
+		/>
+	)
 }
 
 export default Search
