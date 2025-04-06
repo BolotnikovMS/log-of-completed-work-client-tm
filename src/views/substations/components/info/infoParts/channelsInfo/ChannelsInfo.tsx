ebf500ef88158098c6eb0991be981e7eb9d297ce
Mikhail <bolotnikovms@yandex.ru>
2025-04-06 16:33:05 +0500

    refactor: Убран компонет ChannelInfoNote. Добавлен компонент ChannelInfo. Убраны лишний данные.

--- src/views/substations/components/info/infoParts/channelsInfo/ChannelsInfo.tsx
+++ src/views/substations/components/info/infoParts/channelsInfo/ChannelsInfo.tsx
@@ -4,7 +4,7 @@ import 'react-awesome-slider/dist/styles.css'
 import { Link } from 'react-router-dom'
 import { ChannelControlMenu } from '../../../../../channel/components/cards/cardParts'
 import { IPropsChannelsInfo } from '../partsInfo.interfaces'
-import ChannelInfoNote from './ChannelInfoNote'
+import ChannelInfo from './channelInfo/ChannelInfo'
 
 const ChannelsInfo: FC<IPropsChannelsInfo> = ({ channels }) => {
 	if (!channels?.length) return <p className='substation-info__section-text text-red-500'>Нет данных по каналам для отображения!</p>
@@ -25,12 +25,6 @@ const ChannelsInfo: FC<IPropsChannelsInfo> = ({ channels }) => {
 								<p className='text-content'>{channel.channel_equipment}</p>
 							</>
 						)}
-						{channel.gsm_operator && (
-							<>
-								<p className='substation-info__text'>GSM оператор: </p>
-								<p className='text-content'>{channel.gsm_operator}</p>
-							</>
-						)}
 						{channel.ipAddress && (
 							<>
 								<p className='substation-info__text'>IP адрес канала: </p>
@@ -41,11 +35,9 @@ const ChannelsInfo: FC<IPropsChannelsInfo> = ({ channels }) => {
 								</p>
 							</>
 						)}
-						{channel.note && (
-							<ChannelInfoNote channel={channel} />
-						)}
 					</div>
-					<div className="w-none">
+					<div className="w-none flex flex-col gap-1">
+						<ChannelInfo channelId={channel.id} />
 						<ChannelControlMenu channelId={channel.id} />
 					</div>
 				</div>
