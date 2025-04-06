db68b2d96ec1a8c277b7f45ba6dbbe5056da55d7
Mikhail <bolotnikovms@yandex.ru>
2025-04-06 16:30:25 +0500

    feat: Добавлен тип TChannelInfo

--- src/types/channel.types.ts
+++ src/types/channel.types.ts
@@ -3,3 +3,5 @@ import { IChannel, IMeta } from '../interfaces'
 export type TChannelData = Omit<IChannel, 'id' | 'channel_category' | 'channel_type' | 'channel_equipment' | 'gsm_operator' | 'substation'>
 
 export type TRespChannels = { meta: IMeta, data: IChannel[] }
+
+export type TChannelInfo = Pick<IChannel, 'id' | 'ipAddress' | 'note' | 'substation' | 'channel_category' | 'channel_type' | 'channel_equipment'>
