d02aa69596527ced798a74106a7bb9996a507404
Mikhail <bolotnikovms@yandex.ru>
2025-04-11 14:37:35 +0500

    feat: Добавленв разметка

--- src/views/docsFiles/components/cardsFolder/FolderCards.tsx
+++ src/views/docsFiles/components/cardsFolder/FolderCards.tsx
@@ -1,9 +1,43 @@
 import { type FC } from 'react'
+import { SmallCard } from '../../../../components'
 
 const FolderCards: FC = () => {
 	return (
-		<>
-		</>
+		<div className='cards'>
+			<SmallCard
+				childrenContent={
+					<p>File name</p>
+				}
+				childrenControl={
+					<>
+						<p>Размер</p>
+						<p>Menu</p>
+					</>
+				}
+			/>
+			<SmallCard
+				childrenContent={
+					<p>File name</p>
+				}
+				childrenControl={
+					<>
+						<p>Размер</p>
+						<p>Menu</p>
+					</>
+				}
+			/>
+			<SmallCard
+				childrenContent={
+					<p>File name</p>
+				}
+				childrenControl={
+					<>
+						<p>Размер</p>
+						<p>Menu</p>
+					</>
+				}
+			/>
+		</div>
 	)
 }
 
