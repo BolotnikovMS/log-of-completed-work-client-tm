47ed44660940e95aa8d357ff31f29b20fd5b7764
Mikhail <bolotnikovms@yandex.ru>
2025-04-13 17:32:15 +0500

    feat: Добавлено отображение наименования изображения

--- src/views/substations/components/info/infoParts/slider/SliderPhoto.tsx
+++ src/views/substations/components/info/infoParts/slider/SliderPhoto.tsx
@@ -1,10 +1,19 @@
-import { MouseEvent, type FC } from 'react'
+import { MouseEvent, useEffect, useState, type FC } from 'react'
 import { CustomSlider, Icon } from '../../../../../../components'
 import { urlFile } from '../../../../../../constants'
 import { IPropsPartsInfo } from '../partsInfo.interfaces'
 
 const SliderPhoto: FC<IPropsPartsInfo> = ({ substation }) => {
+	const [currentImgName, setCurrentImgName] = useState<string>('')
+
+	useEffect(() => {
+		if (substation?.files_photos_ps?.length) {
+			setCurrentImgName(substation.files_photos_ps[0].clientName)
+		}
+	}, [])
+
 	if (!substation) return null
+
 	const toggleFullscreen = (e: MouseEvent<HTMLDivElement, MouseEvent>) => {
 		const target = e.currentTarget as HTMLDivElement
 
@@ -18,16 +27,24 @@ const SliderPhoto: FC<IPropsPartsInfo> = ({ substation }) => {
 			console.warn('Fullscreen API is not supported in this browser.')
 		}
 	}
+	const handleSlideChange = (index: number) => {
+		if (!substation.files_photos_ps) return null
+
+		setCurrentImgName(substation.files_photos_ps[index].clientName)
+	}
 
 	return (
 		<div className="substation-info__imgs">
 			{substation.files_photos_ps?.length ? (
 				<>
-					<CustomSlider>
+					<CustomSlider onTransitionStart={(e) => handleSlideChange(e.nextIndex)}>
 						{substation.files_photos_ps.map(photo => (
 							<div key={photo.id} data-src={`${urlFile}${photo.filePath}`} onClick={(e) => toggleFullscreen(e)} />
 						))}
 					</CustomSlider>
+					<div className='mt-4'>
+						<p className='text-content'>{currentImgName}</p>
+					</div>
 				</>
 			) : (
 				<Icon id='img-off' className='!w-96 !h-96' />
