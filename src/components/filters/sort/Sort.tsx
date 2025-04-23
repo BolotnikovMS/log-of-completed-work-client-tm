c44cceb6e3ae2807f54ded1a84a770f78c415025
Mikhail <bolotnikovms@yandex.ru>
2025-04-23 16:50:59 +0500

    fix: Добавлен tooltipText

--- src/components/filters/sort/Sort.tsx
+++ src/components/filters/sort/Sort.tsx
@@ -5,33 +5,34 @@ import { TOrderSort } from '../../../types/order.types'
 import { IPropsSort } from './sort.interface'
 
 const Sort: FC<IPropsSort> = ({ orderSort, sort, sortOptions }) => {
-  const [searchParams, setSearchParams] = useSearchParams()
-  const handleSort = useCallback((orderValue: TOrderSort, sortValue: string = 'name') => {
-    searchParams.set('order', orderValue)
-    searchParams.set('sort', sortValue)
-    setSearchParams(searchParams)
-  }, [searchParams, setSearchParams])
+	const [searchParams, setSearchParams] = useSearchParams()
+	const handleSort = useCallback((orderValue: TOrderSort, sortValue: string = 'name') => {
+		searchParams.set('order', orderValue)
+		searchParams.set('sort', sortValue)
+		setSearchParams(searchParams)
+	}, [searchParams, setSearchParams])
 
-  return (
-    <>
-      <Dropdown
-        classMenu='dropdown-bottom'
-        children={
-          <>
-            {sortOptions.map(option => (
-              option.value === sort && option.order === orderSort && <span key={option.value} className='flex items-center gap-1'>{option.icon}{option.label}</span>
-            ))}
-          </>
-        }
-        menuItems={sortOptions.map(option => (
-          <Button key={option.value} className='!justify-start' onClick={() => handleSort(option.order, option.value)}>
-            {option.icon}
-            {option.label}
-          </Button>
-        ))}
-      />
-    </>
-  )
+	return (
+		<>
+			<Dropdown
+				classMenu='dropdown-bottom'
+				children={
+					<>
+						{sortOptions.map(option => (
+							option.value === sort && option.order === orderSort && <span key={option.value} className='flex items-center gap-1'>{option.icon}{option.label}</span>
+						))}
+					</>
+				}
+				tooltipText='Сортировка'
+				menuItems={sortOptions.map(option => (
+					<Button key={option.value} className='!justify-start' onClick={() => handleSort(option.order, option.value)}>
+						{option.icon}
+						{option.label}
+					</Button>
+				))}
+			/>
+		</>
+	)
 }
 
 export default Sort
