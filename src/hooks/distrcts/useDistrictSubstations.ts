49fc4cee81af64227cb06380e7d4707fa886d4cb
Mikhail <bolotnikovms@yandex.ru>
2025-04-24 18:50:04 +0500

    refactor: Пересмотрен хук Убраны параметы typeKp, headController. Изменен staleTime.

--- src/hooks/distrcts/useDistrictSubstations.ts
+++ src/hooks/distrcts/useDistrictSubstations.ts
@@ -6,14 +6,13 @@ interface IPropsDistrictSubstation extends IQueryParams {
   id: string | undefined
 }
 
-export const useDistrictSubstations = ({ id, search, sort, order, typeKp, headController }: IPropsDistrictSubstation) => {
+export const useDistrictSubstations = ({ id, search, sort, order }: IPropsDistrictSubstation) => {
   const { data, error, isError, isLoading } = useQuery({
-    queryKey: ['district-substations', id, search, sort, order, typeKp, headController],
-    queryFn: () => DistrictService.getSubstationsRelatedDistrict(id || '', { search, sort, order, typeKp, headController }),
-    staleTime: 1000 * 10,
+    queryKey: ['district-substations', id, search, sort, order],
+    queryFn: () => DistrictService.getSubstationsRelatedDistrict(id || '', { search, sort, order }),
+    staleTime: 1 * 60 * 1000,
     enabled: !!id,
   })
-  const substations = data?.data
 
-  return { substations, error, isError, isLoading }
+  return { data, error, isError, isLoading }
 }
