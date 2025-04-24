f8998d19182881c2b88858a16010e49300554eba
Mikhail <bolotnikovms@yandex.ru>
2025-04-24 18:50:52 +0500

    refactor: Переработаны компоненты. Использован компонент CardsSubstations

--- src/views/district/components/district-substations/DistrictSubstationCards.tsx
+++ src/views/district/components/district-substations/DistrictSubstationCards.tsx
@@ -1,10 +1,8 @@
-import { useMemo, type FC } from 'react'
+import { type FC } from 'react'
 import { useParams, useSearchParams } from 'react-router-dom'
-import { Error, InfoMessage, Loader, NumberRecords, SmallCard } from '../../../../components'
-import { pageConfig } from '../../../../config/pages.config'
+import { CardsSubstations } from '../../../../components'
 import { useDistrictSubstations } from '../../../../hooks'
 import { TOrderSort } from '../../../../types/order.types'
-import { CardContent, ControlMenu } from '../../../substations/components/substations-cards/cardParts'
 
 const DistrictSubstationCards: FC = () => {
 	const { id } = useParams()
@@ -12,36 +10,10 @@ const DistrictSubstationCards: FC = () => {
 	const searchParam = searchParams.get('search')
 	const sortParam = searchParams.get('sort')
 	const orderParam = searchParams.get('order')
-	const typeKpParam = searchParams.get('typeKp')
-	const headControllerParam = searchParams.get('headController')
-	const { substations, error, isError, isLoading } = useDistrictSubstations({ id, search: searchParam, sort: sortParam, order: orderParam as TOrderSort, typeKp: typeKpParam, headController: headControllerParam })
-	const memoizedSubstations = useMemo(() => substations, [substations])
-
-	if (isError && error) return <Error error={error} />
-	if (isLoading) return <Loader />
+	const { data, error, isError, isLoading } = useDistrictSubstations({ id, search: searchParam, sort: sortParam, order: orderParam as TOrderSort })
 
 	return (
-		<>
-			<NumberRecords text='Всего объектов:' numberRecords={substations?.length} />
-			{!!memoizedSubstations?.length && (
-				<div className="cards">
-					{memoizedSubstations.map(substation => (
-						<SmallCard
-							key={substation.id}
-							classContent='!block'
-							childrenContent={
-								<CardContent substation={substation} />
-							}
-							path={pageConfig.getDynamicUrl(pageConfig.substationInfo, { id: substation.id })}
-							childrenControl={
-								<ControlMenu substationId={substation.id} />
-							}
-						/>))
-					}
-				</div>
-			)}
-			{(!substations?.length && !isLoading && !isError) && <InfoMessage text='Объектов пока не добавлено...' />}
-		</>
+		<CardsSubstations substations={data} error={error} isError={isError} isLoading={isLoading} />
 	)
 }
 
--- src/views/substations/components/substations-cards/SubstationsCards.tsx
+++ src/views/substations/components/substations-cards/SubstationsCards.tsx
@@ -1,14 +1,12 @@
-import { useEffect, useMemo, useState, type FC } from 'react'
+import { useState, type FC } from 'react'
 import { useSearchParams } from 'react-router-dom'
-import { Error, InfoMessage, Loader, NumberRecords, Pagination, SmallCard } from '../../../../components'
-import { pageConfig } from '../../../../config/pages.config'
+import { CardsSubstations } from '../../../../components'
 import { EFilterParam } from '../../../../enums/filterParam.enums'
 import { useSubstations } from '../../../../hooks'
 import { TOrderSort } from '../../../../types/order.types'
-import { CardContent, ControlMenu } from './cardParts'
 
 const SubstationsCards: FC = () => {
-	const [searchParams, setSearchParams] = useSearchParams()
+	const [searchParams] = useSearchParams()
 	const [page, setPage] = useState<number>(Number(searchParams.get(EFilterParam.page)) || 1)
 	const searchParam = searchParams.get('search')
 	const sortParam = searchParams.get('sort')
@@ -20,47 +18,9 @@ const SubstationsCards: FC = () => {
 	const channelTypeParam = searchParams.get(EFilterParam.channelType)
 	const objectTypeParam = searchParams.get(EFilterParam.objectType)
 	const { substations: data, error, isError, isLoading } = useSubstations({ limit: 20, page, search: searchParam, sort: sortParam, order: orderParam as TOrderSort, typeKp: typeKpParam, headController: headControllerParam, channelCategory: channelCategoryParam, channelType: channelTypeParam, district: districtParam, objectType: objectTypeParam })
-	const memoizedSubstations = useMemo(() => data, [data])
-
-	useEffect(() => {
-		if (memoizedSubstations?.data.length === 0 && page !== 1) {
-			setPage(page - 1)
-		}
-
-		searchParams.set(EFilterParam.page, page.toString())
-		setSearchParams(searchParams)
-	}, [memoizedSubstations?.data.length, page, searchParams, setSearchParams])
-
-	if (isError && error) return <Error error={error} />
-	if (isLoading) return <Loader />
 
 	return (
-		<>
-			<NumberRecords text='Всего объектов:' numberRecords={data?.meta.total} />
-			{!!memoizedSubstations?.data.length && (
-				<div className='flex flex-col gap-2'>
-					<div className="cards">
-						{memoizedSubstations.data.map(substation => (
-							<SmallCard
-								key={substation.id}
-								classContent='!block'
-								childrenContent={
-									<CardContent substation={substation} />
-								}
-								path={pageConfig.getDynamicUrl(pageConfig.substationInfo, { id: substation.id })}
-								childrenControl={
-									<ControlMenu substationId={substation.id} />
-								}
-							/>
-						))}
-					</div>
-					<Pagination page={page} meta={memoizedSubstations.meta} setPage={setPage} />
-				</div>
-			)}
-			{(!data?.data.length && !isLoading && !isError) && (
-				<InfoMessage text='Объектов пока не добавлено...' />
-			)}
-		</>
+		<CardsSubstations substations={data} error={error} isError={isError} isLoading={isLoading} page={page} setPage={setPage} />
 	)
 }
 
