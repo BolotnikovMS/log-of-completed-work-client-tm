81ee7e8d346af2095f9f2c16d324e25704ce9da1
Mikhail <bolotnikovms@yandex.ru>
2025-05-06 16:31:15 +0500

    feat: Добавлены хелперы для экспорта

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,6 +1,8 @@
 import { checkRole } from './checkRole.helper'
+import { convertValue } from './convertValue/convertValue.helper'
 import { errorHandler } from './errorHandler.helper'
 import { joinClasses } from './joinClasses.helper'
+import { tmCoefficientCalculator } from './tmCoefficientCalculator/tmCoefficientCalculator.helper'
 import { transliterate } from './transliterate.helper'
 
-export { checkRole, errorHandler, joinClasses, transliterate }
+export { checkRole, convertValue, errorHandler, joinClasses, tmCoefficientCalculator, transliterate }
