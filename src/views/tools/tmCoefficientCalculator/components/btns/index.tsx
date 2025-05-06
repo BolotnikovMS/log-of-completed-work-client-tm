ad04ce7c9fc9d950699c97ec1eb3d09d5d18d364
Mikhail <bolotnikovms@yandex.ru>
2025-05-06 16:40:55 +0500

    feat(ui): Добавлен компонент с кнопкой для расчета результата

---
+++ src/views/tools/tmCoefficientCalculator/components/btns/index.tsx
@@ -0,0 +1,20 @@
+import { type FC } from 'react'
+import { Button, Icon } from '../../../../../components'
+import { useTmCoefficientCalculatorStore } from '../../../../../store/tmCoefficientCalculator'
+
+const CalculationBtn: FC = () => {
+	const quanta = useTmCoefficientCalculatorStore(state => state.quanta)
+	const calculateResult = useTmCoefficientCalculatorStore(state => state.calculateResult)
+
+	return (
+		<Button
+			type='button'
+			className='w-[300px] hover:bg-gray-300/85 transition ease-out duration-500'
+			onClick={calculateResult} disabled={!quanta}
+		>
+			<Icon id='calculator' />
+		</Button>
+	)
+}
+
+export default CalculationBtn
