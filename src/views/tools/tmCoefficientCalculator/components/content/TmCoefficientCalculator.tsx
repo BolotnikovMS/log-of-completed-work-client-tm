0caa0fcaa9cf72de9351cf683daabe8352c17cfe
Mikhail <bolotnikovms@yandex.ru>
2025-05-08 18:29:11 +0500

    feat(ui): Добавлен компонент InformationQuanta

--- src/views/tools/tmCoefficientCalculator/components/content/TmCoefficientCalculator.tsx
+++ src/views/tools/tmCoefficientCalculator/components/content/TmCoefficientCalculator.tsx
@@ -1,5 +1,6 @@
 import { type FC } from 'react'
 import CalculationBtn from '../btns'
+import InformationQuanta from '../informationQuanta'
 import CurrentCoefficientInput from '../inputs/currentCoefficient'
 import QuantaInput from '../inputs/quantaInput'
 import QuantitiesRadioGroup from '../inputs/quantitiesRadioGroup'
@@ -9,14 +10,17 @@ import Results from '../result'
 
 const TmCoefficientCalculator: FC = () => {
 	return (
-		<div className="flex flex-col gap-3 items-center">
-			<CurrentCoefficientInput />
-			<QuantaInput />
-			<VoltageSelect />
-			<TypeCalculationSelect />
-			<QuantitiesRadioGroup />
-			<CalculationBtn />
-			<Results />
+		<div className='flex flex-wrap justify-center gap-6'>
+			<div className="flex flex-col gap-3 items-center">
+				<CurrentCoefficientInput />
+				<QuantaInput />
+				<VoltageSelect />
+				<TypeCalculationSelect />
+				<QuantitiesRadioGroup />
+				<CalculationBtn />
+				<Results />
+			</div>
+			<InformationQuanta />
 		</div>
 	)
 }
