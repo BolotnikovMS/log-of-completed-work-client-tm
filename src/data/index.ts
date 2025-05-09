8ca8cef8202db23c3854d837c12ce7fb680e064b
Mikhail <bolotnikovms@yandex.ru>
2025-05-09 17:51:04 +0500

    fix(data): Убран импорт удаленного файла

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,6 +1,5 @@
 import { quantitiesData } from './quantitiesData'
-import { quantaData } from './tmCoefficientCalculator/quantaData'
 import { typeCalculationData } from './tmCoefficientCalculator/typeCalculationData'
 import { voltageClassessData } from './voltageClassessData'
 
-export { quantaData, quantitiesData, typeCalculationData, voltageClassessData }
+export { quantitiesData, typeCalculationData, voltageClassessData }
