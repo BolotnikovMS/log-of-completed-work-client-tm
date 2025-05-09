77cf5470340d972d17a8cb05650795ae1c2cc931
Mikhail <bolotnikovms@yandex.ru>
2025-05-09 17:53:28 +0500

    style(ui): Добавлено выделение текста

--- src/views/tools/tmCoefficientCalculator/components/informationQuanta/index.tsx
+++ src/views/tools/tmCoefficientCalculator/components/informationQuanta/index.tsx
@@ -2,14 +2,14 @@ import { type FC } from 'react'
 import { Tab } from '../../../../../components'
 
 const InformationQuanta: FC = () => {
-	const arisContent = <p className='text-content'>Полная шкала преобразованного сигнала - от 0 до 5 мА.</p>
-	const isetContent = <p className='text-content'>Полная шкала преобразованного сигнала - от минус 5000 до плюс 5000 квантов.</p>
-	const granitContent = <p className='text-content'>Полная шкала преобразованного сигнала - от 0 до 250 квантов.</p>
-	const telecontrolContent = <p className='text-content'>Полная шкала преобразованного сигнала взятого с icp con - от -32768 до 32768 квантов.</p>
+	const arisContent = <p className='text-content'>Полная шкала преобразованного сигнала - <span className='italic text-red-500'>от 0 до 5 мА.</span></p>
+	const isetContent = <p className='text-content'>Полная шкала преобразованного сигнала - <span className='italic text-red-500'>от -5000 до +5000 квантов</span>.</p>
+	const granitContent = <p className='text-content'>Полная шкала преобразованного сигнала - <span className='italic text-red-500'>от 0 до 250 квантов</span>.</p>
+	const telecontrolContent = <p className='text-content'>Полная шкала преобразованного сигнала взятого с icp con - <span className='italic text-red-500'>от -32768 до +32768 квантов</span>.</p>
 
 	return (
 		<Tab
-			classTab='w-1/2'
+			classTab='lg:w-1/2 md:w-full'
 			classContainer='py-6'
 			tabs={[
 				{ id: 'aris', label: 'Aris', content: arisContent },
