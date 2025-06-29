92c81f7345b4f9027273ea8b8be8797d902aa417
Mikhail <bolotnikovms@yandex.ru>
2025-06-29 17:50:53 +0500

    fix(cn): Добавлен пропс класса

--- src/components/loaders/loader/Loader.tsx
+++ src/components/loaders/loader/Loader.tsx
@@ -1,10 +1,12 @@
+import cn from 'classnames'
 import React from 'react'
+import { IPropsLoader } from './loader.interface'
 import './loader.scss'
 
-export const Loader: React.FC = () => {
-  return (
-    <div className='loader-spinner__wrapper'>
-      <span className='loader-spinner !bg-blue-300'></span>
-    </div>
-  )
+export const Loader: React.FC<IPropsLoader> = ({ className }) => {
+	return (
+		<div className={cn('loader-spinner__wrapper', className)}>
+			<span className='loader-spinner !bg-blue-300'></span>
+		</div>
+	)
 }
