e703e38e9968a0de59356f87fa81ce1a8fc90768
Mikhail <bolotnikovms@yandex.ru>
2025-06-29 17:52:20 +0500

    feat(hook): Добавлен хук Хук для получения кол-ва дефектов

---
+++ src/hooks/defectsTM/useDefectsTM.ts
@@ -0,0 +1,13 @@
+import { keepPreviousData, useQuery, UseQueryOptions } from '@tanstack/react-query'
+import { IQueryParams } from '../../interfaces'
+import { DefectsTMService } from '../../services/defects/defectsTM.service'
+
+export const useDefectsTM = (id: number, { status }: IQueryParams, options?: Omit<UseQueryOptions<number, Error>, 'queryKey'>) => {
+	return useQuery({
+		queryKey: ['defectsTM', id, status],
+  	queryFn: () => DefectsTMService.getNumberDefectsByIdSubstation(id, { status }),
+   	staleTime: 5 * 60 * 1000,
+    placeholderData: keepPreviousData,
+    ...options
+  })
+ }
