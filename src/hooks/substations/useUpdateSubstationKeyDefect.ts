bfdb8033749a20ec7c9c463927d600ac19dfca44
Mikhail <bolotnikovms@yandex.ru>
2025-07-10 20:29:02 +0500

    feat(хук): Добавлен хук обновления ключа

---
+++ src/hooks/substations/useUpdateSubstationKeyDefect.ts
@@ -0,0 +1,21 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { toast } from 'react-toastify'
+import { errorHandler } from '../../helpers'
+import { SubstationService } from '../../services/substations/substation.service'
+import { TSubstationKeyDefect } from '../../types/substation.types'
+
+export const useUpdateSubstationKeyDefect = () => {
+	const queryClient = useQueryClient()
+
+	return useMutation({
+		mutationFn: ({ id, data }: { id: number, data: TSubstationKeyDefect }) => SubstationService.updKeyDefect(id, data),
+		onSuccess: async () => {
+			await queryClient.invalidateQueries({ queryKey: ['substationInfo'] })
+
+			toast.success('Ключ связи успешно обновлен!')
+		},
+    onError: (errors) => {
+      toast.error(errorHandler(errors))
+    }
+	})
+}
