10084db7c08d77773aeab072afa1423d4bc3633d
Mikhail <bolotnikovms@yandex.ru>
2025-07-10 20:37:17 +0500

    refactor(улучшения): Внесены улучшения

--- src/views/substations/components/defectsNumber/DefectsNumber.tsx
+++ src/views/substations/components/defectsNumber/DefectsNumber.tsx
@@ -1,5 +1,5 @@
 import cn from 'classnames'
-import { type FC } from 'react'
+import { useEffect, type FC } from 'react'
 import { Link } from 'react-router-dom'
 import { toast } from 'react-toastify'
 import { Icon, Loader } from '../../../../components'
@@ -10,28 +10,28 @@ const DefectsNumber: FC<IPropsDefectsNumber> = ({ keyDefectSubstation }) => {
 	if (!keyDefectSubstation) return null
 
 	const { data: numberDefectsTM, isError: isErrorNDTM, isLoading: isLoadingNDTM } = useDefectsTM(keyDefectSubstation, { status: 'open' }, {
-		enabled: !!keyDefectSubstation
+		enabled: !!keyDefectSubstation,
+		retry: 2
 	})
 	const urlDefects = import.meta.env.VITE_DEFECTS_SHOW_URL
 	!urlDefects && console.info('URL адрес ЖД не задан!')
 
-	if (isErrorNDTM) {
-		toast.error('Ошибка при попытке получения количества дефектов!')
+	useEffect(() => {
+		if (isErrorNDTM) {
+			toast.error('Ошибка при попытке получения количества дефектов!')
+		}
+	}, [isErrorNDTM])
 
-		return null
-	}
+	if (isLoadingNDTM) return <Loader className='!p-0' />
+	if (isErrorNDTM) return null
 
 	return (
 		<>
-			{
-				isLoadingNDTM ?
-					<Loader className='!p-0' /> :
-					<Link to={`${urlDefects}/${keyDefectSubstation}?status=open`} className={cn('mBtn btn-sm', numberDefectsTM && numberDefectsTM >= 1 ? 'mBtn_error' : 'mBtn_green')} target='_blank' rel="noopener noreferrer">
-						<Icon id='alert' />
-						Дефекты
-						({numberDefectsTM})
-					</Link>
-			}
+			<Link to={`${urlDefects}/${keyDefectSubstation}?status=open`} className={cn('mBtn btn-sm', numberDefectsTM && numberDefectsTM >= 1 ? 'mBtn_error' : 'mBtn_green')} target='_blank' rel="noopener noreferrer">
+				<Icon id='alert' />
+				Дефекты
+				({numberDefectsTM})
+			</Link>
 		</>
 	)
 }
