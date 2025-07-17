b79c258eda04cff5309dba56b830e1c92695125c
Mikhail <bolotnikovms@yandex.ru>
2025-07-17 20:41:48 +0500

    fix: Поправлен хук Использование skipToken

--- src/hooks/defectsTM/useDefectsTM.ts
+++ src/hooks/defectsTM/useDefectsTM.ts
@@ -1,13 +1,13 @@
-import { keepPreviousData, useQuery, UseQueryOptions } from '@tanstack/react-query'
+import { keepPreviousData, skipToken, useQuery, UseQueryOptions } from '@tanstack/react-query'
 import { IQueryParams } from '../../interfaces'
 import { DefectsTMService } from '../../services/defects/defectsTM.service'
 
-export const useDefectsTM = (id: number, { status }: IQueryParams, options?: Omit<UseQueryOptions<number, Error>, 'queryKey'>) => {
+export const useDefectsTM = (id: number | null, { status }: IQueryParams, options?: Omit<UseQueryOptions<number, Error>, 'queryKey'>) => {
 	return useQuery({
 		queryKey: ['defectsTM', id, status],
-		queryFn: () => DefectsTMService.getNumberDefectsByIdSubstation(id, { status }),
+		queryFn: id ? () => DefectsTMService.getNumberDefectsByIdSubstation(id, { status }) : skipToken,
 		staleTime: 5 * 60 * 1000,
-	  placeholderData: keepPreviousData,
-	  ...options
-  })
- }
+		placeholderData: keepPreviousData,
+		...options
+	})
+}
