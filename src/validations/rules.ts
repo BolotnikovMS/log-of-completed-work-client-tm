e734c1c9a4c746e13e3068c5ecf87050cdf886a1
Mikhail <bolotnikovms@yandex.ru>
2025-07-21 16:06:28 +0500

    fix: Правки отступов

--- src/validations/rules.ts
+++ src/validations/rules.ts
@@ -13,93 +13,93 @@ setLocale({
 })
 
 export const text20 = yup
-  .string()
-  .trim()
-  .required('Поле является обязательным!')
-  .min(2)
-  .max(20)
+	.string()
+	.trim()
+	.required('Поле является обязательным!')
+	.min(2)
+	.max(20)
 export const text30 = yup
-  .string()
-  .trim()
-  .required('Поле является обязательным!')
-  .min(2)
-  .max(30)
+	.string()
+	.trim()
+	.required('Поле является обязательным!')
+	.min(2)
+	.max(30)
 export const text140 = yup
-  .string()
-  .trim()
-  .required('Поле является обязательным!')
-  .min(2)
-  .max(140)
+	.string()
+	.trim()
+	.required('Поле является обязательным!')
+	.min(2)
+	.max(140)
 export const text180 = yup
-  .string()
-  .trim()
-  .required('Поле является обязательным!')
-  .min(2)
-  .max(180)
+	.string()
+	.trim()
+	.required('Поле является обязательным!')
+	.min(2)
+	.max(180)
 export const text240 = yup
-  .string()
-  .trim()
-  .required('Поле является обязательным!')
-  .min(2)
-  .max(240)
+	.string()
+	.trim()
+	.required('Поле является обязательным!')
+	.min(2)
+	.max(240)
 export const text700Optional = yup
-  .lazy((value: string) =>
-    value && value.length ? yup
-      .string()
-      .trim()
-      .min(3)
-      .max(700)
-      : yup
-        .string()
-        .notRequired())
+	.lazy((value: string) =>
+		value && value.length ? yup
+			.string()
+			.trim()
+			.min(3)
+			.max(700)
+			: yup
+				.string()
+				.notRequired())
 export const text1000 = yup
-  .string()
-  .trim()
-  .required('Поле является обязательным!')
-  .min(2)
-  .max(1000)
+	.string()
+	.trim()
+	.required('Поле является обязательным!')
+	.min(2)
+	.max(1000)
 export const text1000Optional = yup
-  .lazy((value: string) =>
-    value && value.length ? yup
-      .string()
-      .trim()
-      .min(2)
-      .max(1000)
-      : yup
-        .string()
-        .notRequired())
+	.lazy((value: string) =>
+		value && value.length ? yup
+			.string()
+			.trim()
+			.min(2)
+			.max(1000)
+			: yup
+				.string()
+				.notRequired())
 export const numberCheck = yup
-  .number()
-  .positive()
-  .integer()
-  .required('Поле является обязательным!')
+	.number()
+	.positive()
+	.integer()
+	.required('Поле является обязательным!')
 export const numberOptional = yup
 	.number()
 	.nullable()
-	.transform((value, originalValue) => originalValue === "" ? null : value)
+	.transform((value, originalValue) => originalValue === '' ? null : value)
 	.integer()
 	.positive()
 	.typeError('Введите число!')
-  .notRequired()
-  // .lazy((value: number) =>
-  //   value ? yup
-  //     .number()
-  //     .nullable()
-  //     .positive()
-  //     .integer()
-  //     .typeError('message')
-  //     : yup.string().nullable().notRequired())
+	.notRequired()
+// .lazy((value: number) =>
+//   value ? yup
+//     .number()
+//     .nullable()
+//     .positive()
+//     .integer()
+//     .typeError('message')
+//     : yup.string().nullable().notRequired())
 export const date = yup
-  .date()
-  .required('Поле является обязательным!')
+	.date()
+	.required('Поле является обязательным!')
 export const ipOptional = yup
-  .lazy((value: string) =>
-    value && value.length ?
-      yup.string().trim().matches(/^((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)\.?\b){4}$/, 'Введите корректный ip адрес!') :
-      yup.string().notRequired())
+	.lazy((value: string) =>
+		value && value.length ?
+			yup.string().trim().matches(/^((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)\.?\b){4}$/, 'Введите корректный ip адрес!') :
+			yup.string().notRequired())
 export const email = yup
-  .string()
-  .trim()
-  .email('Введите корректный email!')
-  .matches(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,3}))$/, 'Формат email: xxxx@xxx.xx')
-  .required('Поле является обязательным!')
+	.string()
+	.trim()
+	.email('Введите корректный email!')
+	.matches(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,3}))$/, 'Формат email: xxxx@xxx.xx')
+	.required('Поле является обязательным!')
