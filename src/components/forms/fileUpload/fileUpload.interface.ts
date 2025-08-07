03fa3ce4f4e9f3204427f511ef1d44001bdc4294
Mikhail <bolotnikovms@yandex.ru>
2025-08-07 19:24:36 +0500

    fix: Удален дубль

--- src/components/forms/fileUpload/fileUpload.interface.ts
+++
@@ -1,7 +0,0 @@
-export interface IPropsFileUpload extends React.InputHTMLAttributes<HTMLInputElement> {
-	// eslint-disable-next-line @typescript-eslint/no-explicit-any
-	register: any
-	validation?: { [key: string]: unknown }
-	errorMessage?: string
-	file: File | null
-}
