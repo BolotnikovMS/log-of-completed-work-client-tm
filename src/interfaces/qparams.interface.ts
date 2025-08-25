a65535805c468b37f6e644d36ce25503bb438b3c
Mikhail <bolotnikovms@yandex.ru>
2025-08-25 17:56:40 +0500

    feat: Добавлен параметр action

--- src/interfaces/qparams.interface.ts
+++ src/interfaces/qparams.interface.ts
@@ -1,28 +1,29 @@
 import { TOrderSort } from '../types/order.types'
 
 export interface IQueryParams {
-  page?: number
-  size?: number
-  search?: string | null
-  sort?: string | null
-  order?: TOrderSort | null
-  offset?: number
-  limit?: number
-  substation?: string | null
-  active?: boolean
-  cleanUser?: boolean
-  executor?: string | null
-  dateStart?: string | null | Date
-  dateEnd?: string | null | Date
-  typeKp?: string | null
-  headController?: string | null
-  mainChannel?: string | null
-  backupChannel?: string | null
-  district?: string | null
-  channelType?: string | null
-  channelCategory?: string | null
-  typeWork?: string | null
-  inControl?: string | null
+	page?: number
+	size?: number
+	search?: string | null
+	sort?: string | null
+	order?: TOrderSort | null
+	offset?: number
+	limit?: number
+	substation?: string | null
+	active?: boolean
+	cleanUser?: boolean
+	executor?: string | null
+	dateStart?: string | null | Date
+	dateEnd?: string | null | Date
+	typeKp?: string | null
+	headController?: string | null
+	mainChannel?: string | null
+	backupChannel?: string | null
+	district?: string | null
+	channelType?: string | null
+	channelCategory?: string | null
+	typeWork?: string | null
+	inControl?: string | null
 	objectType?: string | null
 	status?: string
+	action?: string
 }
