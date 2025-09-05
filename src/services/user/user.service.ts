16b69b4c4bbd236f69e5cc768803feff45788e65
Mikhail <bolotnikovms@yandex.ru>
2025-09-05 16:37:23 +0500

    style: Поправлены отступы

--- src/services/user/user.service.ts
+++ src/services/user/user.service.ts
@@ -5,41 +5,41 @@ import { IChangePassword, IQueryParams, IUser, IUserRole } from '../../interface
 import { TChangeStatusAccount, TRespUsers, TUserData, TUserShort } from '../../types'
 
 export const UserService = {
-  async getUsers({ limit, page, active, cleanUser }: IQueryParams): Promise<TRespUsers<IUser[]>> {
-    const { data } = await instance.get<TRespUsers<IUser[]>>(`${url}/users`, {
-      params: { limit, page, active, cleanUser },
-    })
+	async getUsers({ limit, page, active, cleanUser }: IQueryParams): Promise<TRespUsers<IUser[]>> {
+		const { data } = await instance.get<TRespUsers<IUser[]>>(`${url}/users`, {
+			params: { limit, page, active, cleanUser },
+		})
 
-    return data
-  },
+		return data
+	},
 
-  async getUserById(idUser: number): Promise<IUser> {
-    const {data} = await instance.get<IUser>(`${url}/users/${idUser}`)
+	async getUserById(idUser: number): Promise<IUser> {
+		const { data } = await instance.get<IUser>(`${url}/users/${idUser}`)
 
-    return data
-  },
+		return data
+	},
 
-  async getShortUsers({ limit, page, active, cleanUser }: IQueryParams): Promise<TRespUsers<TUserShort[]>> {
-    const { data } = await instance.get<TRespUsers<TUserShort[]>>(`${url}/users/short`, {
-      params: { limit, page, active, cleanUser },
-    })
+	async getShortUsers({ limit, page, active, cleanUser }: IQueryParams): Promise<TRespUsers<TUserShort[]>> {
+		const { data } = await instance.get<TRespUsers<TUserShort[]>>(`${url}/users/short`, {
+			params: { limit, page, active, cleanUser },
+		})
 
-    return data
-  },
+		return data
+	},
 
-  async createUser(data: TUserData): Promise<AxiosResponse<IUser>> {
-    return instance.post<IUser>(`${url}/users/create-account`, data)
-  },
+	async createUser(data: TUserData): Promise<AxiosResponse<IUser>> {
+		return instance.post<IUser>(`${url}/users/create-account`, data)
+	},
 
-  async resetPassword(idUser: number, data: IChangePassword): Promise<AxiosResponse<string>> {
-    return instance.patch(`/users/reset-password/${idUser}`, data)
-  },
+	async resetPassword(idUser: number, data: IChangePassword): Promise<AxiosResponse<string>> {
+		return instance.patch(`/users/reset-password/${idUser}`, data)
+	},
 
-  async changeStatusAccount({ active, id }: TChangeStatusAccount): Promise<AxiosResponse<string>> {
-    return instance.patch(`/users/block-user-account/${id}`, { active })
-  },
+	async changeStatusAccount({ active, id }: TChangeStatusAccount): Promise<AxiosResponse<string>> {
+		return instance.patch(`/users/block-user-account/${id}`, { active })
+	},
 
-  async changeUserRole(idUser: number, data: IUserRole): Promise<AxiosResponse<string>> {
-    return instance.patch(`/users/change-role/${idUser}`, data)
-  },
+	async changeUserRole(idUser: number, data: IUserRole): Promise<AxiosResponse<string>> {
+		return instance.patch(`/users/change-role/${idUser}`, data)
+	},
 }
