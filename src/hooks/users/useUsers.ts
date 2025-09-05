83bd0ae89a8eb61e2d59171ddbdfa7a815588bdc
Mikhail <bolotnikovms@yandex.ru>
2025-09-05 16:36:33 +0500

    fix: Добавленв параметры limit, page

--- src/hooks/users/useUsers.ts
+++ src/hooks/users/useUsers.ts
@@ -2,13 +2,13 @@ import { useQuery } from '@tanstack/react-query'
 import { IQueryParams } from '../../interfaces'
 import { UserService } from '../../services/user/user.service'
 
-export const useUsers = ({ active, cleanUser }: IQueryParams) => {
-  const isParams = active ? `active:${active}` : cleanUser ? `clean:${cleanUser}` : 'all'
-  const { data, error, isError, isLoading, isFetching } = useQuery({
-    queryKey: ['users', isParams],
-    queryFn: () => UserService.getUsers({ active, cleanUser }),
-    staleTime: 4 * 60 * 1000,
-  })
+export const useUsers = ({ active, cleanUser, limit, page }: IQueryParams) => {
+	const isParams = active ? `active:${active}` : cleanUser ? `clean:${cleanUser}` : 'all'
+	const { data, error, isError, isLoading, isFetching } = useQuery({
+		queryKey: ['users', isParams, limit, page],
+		queryFn: () => UserService.getUsers({ active, cleanUser, limit, page }),
+		staleTime: 4 * 60 * 1000,
+	})
 
-  return { data, error, isError, isLoading, isFetching }
+	return { data, error, isError, isLoading, isFetching }
 }
