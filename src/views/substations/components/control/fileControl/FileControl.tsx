ab41c669b786662533975d4f3c6b1f692ce420d1
Mikhail <bolotnikovms@yandex.ru>
2025-09-23 16:32:35 +0500

    feat: Добавлен пункт меню

--- src/views/substations/components/control/fileControl/FileControl.tsx
+++ src/views/substations/components/control/fileControl/FileControl.tsx
@@ -1,17 +1,20 @@
-import { type FC } from 'react'
-import { Button, Dropdown, Icon, Loader } from '../../../../../components'
+import { useState, type FC } from 'react'
+import { Button, Dropdown, Icon, Loader, Modal } from '../../../../../components'
 import { ERoles } from '../../../../../enums/roles.enum'
 import { checkRole } from '../../../../../helpers'
-import { useDeleteFile } from '../../../../../hooks'
+import { useDeleteFile, useModal } from '../../../../../hooks'
 import { FileService } from '../../../../../services/file/file.service'
 import { useAuthStore } from '../../../../../store/auth'
 import { TFile } from '../../../../../types'
+import RenameFile from '../../forms/renameFile/RenameFile'
 import { IPropsFileControl } from './fileControl.interface'
 
 const FileControl: FC<IPropsFileControl> = ({ file, classDropDown }) => {
 	const { authUser } = useAuthStore()
 	const { deleteFile } = useDeleteFile()
 	const isAdminOrModerator = checkRole(authUser, [ERoles.Moderator, ERoles.Admin])
+	const { isModal: isModalRename, toggleModal: toggleModalRename } = useModal()
+	const [isEdited, setIsEdited] = useState<boolean>(false)
 
 	const handleDownload = (file: TFile | null) => {
 		if (!file) return null
@@ -31,26 +34,38 @@ const FileControl: FC<IPropsFileControl> = ({ file, classDropDown }) => {
 	if (deleteFile.isPending) return <Loader />
 
 	return (
-		<Dropdown
-			children={
-				<Icon id='setting' />
-			}
-			tooltipText='Меню'
-			classBtnTrigger='btn-circle'
-			classMenu={classDropDown}
-			menuItems={[
-				<Button className='!justify-start' onClick={() => handleDownload(file)}>
-					<Icon id='download' />
-					Скачать
-				</Button>,
-				isAdminOrModerator && (
+		<>
+			<Dropdown
+				children={
+					<Icon id='setting' />
+				}
+				tooltipText='Меню'
+				classBtnTrigger='btn-circle'
+				classMenu={classDropDown}
+				menuItems={[
+					<Button className='!justify-start' onClick={() => handleDownload(file)}>
+						<Icon id='download' />
+						Скачать
+					</Button>,
+					isAdminOrModerator &&
+					<Button className='!justify-start' onClick={() => toggleModalRename()}>
+						<Icon id='edit' />
+						Переименовать
+					</Button>,
+					isAdminOrModerator &&
 					<Button className='!justify-start mBtn_error' onClick={() => handleDeleteFile(file)}>
 						<Icon id='delete' />
 						Удалить
 					</Button>
-				)
-			]}
-		/>
+				]}
+			/>
+			<Modal
+				visible={isModalRename}
+				title='Добавление файла'
+				content={<RenameFile data={file} isEdited={isEdited} setIsEdited={setIsEdited} toggleModal={toggleModalRename} />}
+				onToggle={toggleModalRename}
+			/>
+		</>
 	)
 }
 
