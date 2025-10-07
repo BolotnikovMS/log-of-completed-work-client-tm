fbce058ce6f9b76914e5cfba79f1e29399795bbe
Mikhail <bolotnikovms@yandex.ru>
2025-10-07 15:52:58 +0500

    fix: Убраны поля typeKpId, headControllerId

--- src/interfaces/substation.interfaces.ts
+++ src/interfaces/substation.interfaces.ts
@@ -12,29 +12,35 @@ export interface ISubstation {
 	active: boolean
 	districtId: number
 	voltageClassesId: number
-  typeKpId: number
-  headControllerId: number
-  objectTypeId: number
-  name: string
-  rdu: boolean
-  note?: string | null
+	objectTypeId: number
+	name: string
+	rdu: boolean
+	note?: string | null
 }
 
 export interface ISubstationInfo {
 	id: number
 	active: boolean
 	rdu: boolean
-  note?: string | null
-  fullNameSubstation: string
-  numberCompletedWorks?: number
-  voltage_class?: string | null
-  district?: string | null
-  type_kp?: string | null
-  head_controller?: string | null
-  channels?: TChannelSubstationInfo[]
-  files_photos_ps?: TFile[]
-  files_backups?: TFile[]
-  other_files?: TFile[]
-  object_type?: string | null
-  keyDefectSubstation: number | null
+	note?: string | null
+	fullNameSubstation: string
+	numberCompletedWorks?: number
+	voltage_class?: string | null
+	district?: string | null
+	type_kp?: string | null
+	head_controller?: string | null
+	channels?: TChannelSubstationInfo[]
+	files_photos_ps?: TFile[]
+	files_backups?: TFile[]
+	other_files?: TFile[]
+	object_type?: string | null
+	keyDefectSubstation: number | null
+	telemechanics_devices: ISubstationTelemechanicsDevices[] | []
+}
+
+export interface ISubstationTelemechanicsDevices {
+	id: number
+	note: string | null
+	type_kp: string
+	head_controller: string
 }
