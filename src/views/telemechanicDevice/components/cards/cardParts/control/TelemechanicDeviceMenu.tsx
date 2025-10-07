854f98b0c01a4a18b33b87244168b1cf8975b673
Mikhail <bolotnikovms@yandex.ru>
2025-10-07 20:55:30 +0500

    feat: Добавлено редактирование Добавлен вызов компонента TelemechanicDeviceForm для редактирования записи

--- src/views/telemechanicDevice/components/cards/cardParts/control/TelemechanicDeviceMenu.tsx
+++ src/views/telemechanicDevice/components/cards/cardParts/control/TelemechanicDeviceMenu.tsx
@@ -1,27 +1,43 @@
 import { useCallback, useState, type FC } from 'react'
-import { Button, Dropdown, Icon } from '../../../../../../components'
+import { Button, Dropdown, Error, Icon, Loader, Modal } from '../../../../../../components'
 import { ERoles } from '../../../../../../enums/roles.enum'
 import { checkRole } from '../../../../../../helpers'
-import { useDeleteTelemechanicsDevice, useModal } from '../../../../../../hooks'
+import { useDeleteTelemechanicsDevice, useModal, useTelemechanicsDevice } from '../../../../../../hooks'
 import { useAuthStore } from '../../../../../../store/auth'
+import TelemechanicDeviceForm from '../../../form/TelemechanicDeviceForm'
 
-const TelemechanicDeviceMenu: FC<{deviceId: number}> = ({deviceId}) => {
-	const { authUser} = useAuthStore()
+const TelemechanicDeviceMenu: FC<{ deviceId: number }> = ({ deviceId }) => {
+	const { authUser } = useAuthStore()
 	const isAdminOrModerator = checkRole(authUser, [ERoles.Moderator, ERoles.Admin])
 	const { isModal, toggleModal } = useModal()
 	const [isEdited, setIsEdited] = useState<boolean>(false)
 	const { deleteTelemechanicsDevice } = useDeleteTelemechanicsDevice()
+	const { data, error, isLoading, isError } = useTelemechanicsDevice(deviceId, {
+		enabled: isModal
+	})
 
 	const handleDelete = useCallback((id: number) => {
 		const answer = confirm('Подтвердите удаление записи.')
-	
+
 		if (!answer) return null
-	
+
 		return deleteTelemechanicsDevice.mutate(id)
 	}, [deleteTelemechanicsDevice])
-	
+
+	const handleEdit = useCallback(() => {
+		setIsEdited(!isEdited)
+		toggleModal()
+	}, [isEdited, toggleModal])
+	const modalContent = isLoading ? (
+		<Loader />
+	) : isError ? (
+		<Error error={error} />
+	) : (
+		<TelemechanicDeviceForm data={data} toggleModal={toggleModal} isEdited={isEdited} setIsEdited={setIsEdited} />
+	)
+
 	if (!isAdminOrModerator) return null
-		
+
 	return (
 		<>
 			<Dropdown
@@ -29,11 +45,23 @@ const TelemechanicDeviceMenu: FC<{deviceId: number}> = ({deviceId}) => {
 				tooltipText='Меню'
 				classBtnTrigger='btn-circle'
 				menuItems={[
+					<Button className='!justify-start' onClick={handleEdit}>
+						<Icon id='edit' />
+						Редактировать
+					</Button>,
 					<Button className='mBtn_error !justify-start' onClick={() => handleDelete(deviceId)}>
 						<Icon id='delete' />
 						Удалить
 					</Button>
 				]}
+			/>
+			<Modal
+				visible={isModal}
+				title='Редактирование записи'
+				onToggle={() => {
+					(toggleModal(), setIsEdited(false))
+				}}
+				content={modalContent}
 			/>
 		</>
 	)
