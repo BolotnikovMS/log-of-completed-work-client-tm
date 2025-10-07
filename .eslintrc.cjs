0a2bf7bceb4308f448a090f3fa63c0b1580b5f81
Mikhail <bolotnikovms@yandex.ru>
2025-10-07 15:47:52 +0500

    fix: Правки

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,37 +1,25 @@
 module.exports = {
 	root: true,
 	env: { browser: true, es2020: true },
-	extends: [
-		"eslint:recommended",
-		"plugin:@typescript-eslint/recommended",
-		"plugin:react-hooks/recommended",
-	],
-	ignorePatterns: ["dist", ".eslintrc.cjs"],
-	parser: "@typescript-eslint/parser",
-	plugins: ["react-refresh"],
+	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react-hooks/recommended'],
+	ignorePatterns: ['dist', '.eslintrc.cjs'],
+	parser: '@typescript-eslint/parser',
+	plugins: ['react-refresh'],
 	rules: {
-		"indent": [1, "tab"],
+		indent: [1, 'tab'],
 		// "no-tabs": 0,
-		"react-hooks/exhaustive-deps": "warn",
-		"react-refresh/only-export-components": [
-			"warn",
-			{ allowConstantExport: true },
-		],
-		"quotes": [2, "single", { "avoidEscape": true, "allowTemplateLiterals": true }],
-		"@typescript-eslint/no-unused-vars": "off",
-		"sort-imports": [
-			"error",
+		'react-hooks/exhaustive-deps': 'warn',
+		'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+		quotes: [2, 'single', { avoidEscape: true, allowTemplateLiterals: true }],
+		'@typescript-eslint/no-unused-vars': 'off',
+		'sort-imports': [
+			'error',
 			{
-				"ignoreCase": true,
-				"ignoreDeclarationSort": true,
-				"ignoreMemberSort": true,
-				"memberSyntaxSortOrder": [
-					"none",
-					"all",
-					"single",
-					"multiple"
-				]
+				ignoreCase: true,
+				ignoreDeclarationSort: true,
+				ignoreMemberSort: true,
+				memberSyntaxSortOrder: ['none', 'all', 'single', 'multiple']
 			}
 		]
-	},
-};
+	}
+}
