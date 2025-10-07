7880993c81b8019035297b4d29a1e8e93635fb6a
Mikhail <bolotnikovms@yandex.ru>
2025-10-07 20:52:37 +0500

    fix: Убрано свойство userId

--- src/types/telemechanicsDevice.types.ts
+++ src/types/telemechanicsDevice.types.ts
@@ -1,6 +1,6 @@
 import { IMeta, ITelemechanicsDevices } from '../interfaces'
 
-export type TTelemechanicsDevice = Omit<ITelemechanicsDevices, 'id' | 'userId'>
+export type TTelemechanicsDevice = Omit<ITelemechanicsDevices, 'id'>
 
 export type TRespTelemechanicsDevices = {
 	meta: IMeta
