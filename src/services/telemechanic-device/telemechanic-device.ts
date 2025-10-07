15ffdd1070049fce34b3fa69ab87e1ae46e50bbe
Mikhail <bolotnikovms@yandex.ru>
2025-10-07 15:50:41 +0500

    feat: Сервис получения данных

---
+++ src/services/telemechanic-device/telemechanic-device.ts
@@ -0,0 +1,33 @@
+import { AxiosResponse } from 'axios'
+import { instance } from '../../api/axios.api'
+import { url } from '../../constants'
+import { IQueryParams, ITelemechanicsDevices } from '../../interfaces'
+import { TRespTelemechanicsDevices, TTelemechanicsDevice } from '../../types'
+
+export const TelemechanicDeviceService = {
+	async getAllDevices({ limit, page }: IQueryParams): Promise<TRespTelemechanicsDevices> {
+		const { data } = await instance.get<TRespTelemechanicsDevices>(`${url}/telemechanics-devices`, {
+			params: { page, limit }
+		})
+
+		return data
+	},
+
+	async getDeviceById(id: number): Promise<ITelemechanicsDevices> {
+		const { data } = await instance.get<ITelemechanicsDevices>(`${url}/telemechanics-devices/${id}`)
+
+		return data
+	},
+
+	async create(data: TTelemechanicsDevice): Promise<AxiosResponse<ITelemechanicsDevices>> {
+		return instance.post<ITelemechanicsDevices>(`${url}/telemechanics-devices`, data)
+	},
+
+	async update(id: number, data: TTelemechanicsDevice): Promise<AxiosResponse<ITelemechanicsDevices>> {
+		return instance.patch<ITelemechanicsDevices>(`${url}/telemechanics-devices/${id}`, data)
+	},
+
+	async delete(id: number): Promise<AxiosResponse<void>> {
+		return instance.delete<void>(`${url}/telemechanics-devices/${id}`)
+	}
+}
