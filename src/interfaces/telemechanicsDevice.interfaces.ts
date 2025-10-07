7ca657a0f365ec3daa7c28e8befccbd843341b9a
Mikhail <bolotnikovms@yandex.ru>
2025-10-07 20:52:16 +0500

    fix: Поле note не обязательное

--- src/interfaces/telemechanicsDevice.interfaces.ts
+++ src/interfaces/telemechanicsDevice.interfaces.ts
@@ -1,7 +1,7 @@
 export interface ITelemechanicsDevices {
-  id: number
-  substationId: number
-  typeKpId: number
-  headControllerId: number
-  note: string | null
+	id: number
+	substationId: number
+	typeKpId: number
+	headControllerId: number
+	note?: string | null
 }
