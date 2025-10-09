675f3afe182a8a5d02accd51265057a8d4b7b0e6
Mikhail <bolotnikovms@yandex.ru>
2025-10-09 17:10:23 +0500

    style: Поправлены отступы

--- src/components/cards/card/Card.tsx
+++ src/components/cards/card/Card.tsx
@@ -5,31 +5,31 @@ import './../card.scss'
 import { IPropsCard } from './card.interface'
 
 export const Card: FC<IPropsCard> = ({ childrenHeader, childrenContent, childrenFooter, childrenControl, className, classBody, classContent, path, ...attributes }) => {
-  return (
-    <div className={cx('mCard !w-full', className)} {...attributes}>
-      {path && <Link to={path} className='card__link' />}
-      <div className={cx('mCard__body', classBody)}>
-        {childrenHeader && (
-          <div className="mCard__header">
-            {childrenHeader}
-          </div>
-        )}
-        {childrenContent && (
-          <div className={cx('mCard__content', classContent)}>
-            {childrenContent}
-          </div>
-        )}
-        {childrenFooter && (
-          <div className="mCard__footer">
-            {childrenFooter}
-          </div>
-        )}
-        {childrenControl && (
-          <div className="mCard__actions">
-            {childrenControl}
-          </div>
-        )}
-      </div>
-    </div>
-  )
+	return (
+		<div className={cx('mCard !w-full', className)} {...attributes}>
+			{path && <Link to={path} className='card__link' />}
+			<div className={cx('mCard__body', classBody)}>
+				{childrenHeader && (
+					<div className="mCard__header">
+						{childrenHeader}
+					</div>
+				)}
+				{childrenContent && (
+					<div className={cx('mCard__content', classContent)}>
+						{childrenContent}
+					</div>
+				)}
+				{childrenFooter && (
+					<div className="mCard__footer">
+						{childrenFooter}
+					</div>
+				)}
+				{childrenControl && (
+					<div className="mCard__actions">
+						{childrenControl}
+					</div>
+				)}
+			</div>
+		</div>
+	)
 }
--- src/views/completedWork/components/cards/cardParts/cardFooter/CardFooter.tsx
+++ src/views/completedWork/components/cards/cardParts/cardFooter/CardFooter.tsx
@@ -3,18 +3,18 @@ import { memo, type FC } from 'react'
 import { IPropsCardFooter } from './cardFooter.interface'
 
 const CardFooter: FC<IPropsCardFooter> = memo(({ dateCompletion, workProducerShortName }) => {
-  if (!dateCompletion || !workProducerShortName) {
-    return <p className='text-title text-red-500'>Нет данных!</p>
-  }
+	if (!dateCompletion || !workProducerShortName) {
+		return <p className='text-title text-red-500'>Нет данных!</p>
+	}
 
-  return (
-    <>
-      <hr className='pb-2' />
-      <p className='text-base text-gray-400/70'>
-        Дата работ: {moment(dateCompletion, 'YYYY-MM-DD').format('DD.MM.yyyy')}. Выполнил: {workProducerShortName}
-      </p>
-    </>
-  )
+	return (
+		<>
+			<hr className='pb-2' />
+			<p className='text-base text-gray-400/70'>
+				Дата работ: {moment(dateCompletion, 'YYYY-MM-DD').format('DD.MM.yyyy')}. Выполнил: {workProducerShortName}
+			</p>
+		</>
+	)
 })
 
 export default CardFooter
