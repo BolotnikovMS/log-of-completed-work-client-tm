31eeb9866f140236df10c9215349de99389ddd81
Mikhail <bolotnikovms@yandex.ru>
2025-10-09 20:02:23 +0500

    feat: Добавлено свойситво actualFirmwareVersion

--- src/views/headControllers/components/form/HeadControllerForm.tsx
+++ src/views/headControllers/components/form/HeadControllerForm.tsx
@@ -8,54 +8,64 @@ import { THeadControllerData } from '../../../../types'
 import { validationSchema } from './headController.validation'
 
 const HeadControllerForm: FC<IPropsForm<IHeadController>> = ({ data: headController, isEdited, setIsEdited, toggleModal }) => {
-  const { register, handleSubmit, formState: { errors, isValid }, reset } = useForm<THeadControllerData>({
-    mode: 'all',
-    resolver: yupResolver(validationSchema),
-    defaultValues: {
-      name: headController?.name
-    }
-  })
-  const { mutateAsync: createHeadController, isError: isErrorCreate, error: errorCreate, isPending: isPendingCreate } = useCreateHeadController()
-  const { mutateAsync: updateHeadController, isError: isErrorUpdate, error: errorUpdate, isPending: isPendingUpdate } = useUpdateHeadController()
-  const handleMutation = async ({ data, mutateFn, id }: IPropsMutation<THeadControllerData>) => {
-    await mutateFn(id ? { id, data } : data)
+	const { register, handleSubmit, formState: { errors, isValid }, reset } = useForm<THeadControllerData>({
+		mode: 'all',
+		resolver: yupResolver(validationSchema),
+		defaultValues: {
+			name: headController?.name,
+			actualFirmwareVersion: headController?.actualFirmwareVersion
+		}
+	})
+	const { mutateAsync: createHeadController, isError: isErrorCreate, error: errorCreate, isPending: isPendingCreate } = useCreateHeadController()
+	const { mutateAsync: updateHeadController, isError: isErrorUpdate, error: errorUpdate, isPending: isPendingUpdate } = useUpdateHeadController()
+	const handleMutation = async ({ data, mutateFn, id }: IPropsMutation<THeadControllerData>) => {
+		await mutateFn(id ? { id, data } : data)
 
-    reset()
-    toggleModal()
-    if (isEdited && setIsEdited) setIsEdited(false)
-  }
-  const submitCreate: SubmitHandler<THeadControllerData> = data => handleMutation({ data, mutateFn: createHeadController })
-  const submitUpdate: SubmitHandler<THeadControllerData> = data => {
-    if (!headController?.id) return null
+		reset()
+		toggleModal()
+		if (isEdited && setIsEdited) setIsEdited(false)
+	}
+	const submitCreate: SubmitHandler<THeadControllerData> = data => handleMutation({ data, mutateFn: createHeadController })
+	const submitUpdate: SubmitHandler<THeadControllerData> = data => {
+		if (!headController?.id) return null
 
-    handleMutation({ data, mutateFn: updateHeadController, id: headController.id })
-  }
-  const errorMessage = (isErrorCreate || isErrorUpdate && errorCreate && errorUpdate !== null) && <Error error={errorCreate || errorUpdate} />
+		handleMutation({ data, mutateFn: updateHeadController, id: headController.id })
+	}
+	const errorMessage = (isErrorCreate || isErrorUpdate && errorCreate && errorUpdate !== null) && <Error error={errorCreate || errorUpdate} />
 
-  if (isPendingCreate || isPendingUpdate) return <Loader />
+	if (isPendingCreate || isPendingUpdate) return <Loader />
 
-  return (
-    <div className="work-log__form">
-      {errorMessage}
-      <form className="form" onSubmit={handleSubmit(isEdited ? submitUpdate : submitCreate)}>
-        <Group>
-          <Input
-            label='Название контроллера'
-            name='name'
-            register={register}
-            errorMessage={errors.name?.message}
-            mandatory={true}
-            placeholder='Введите название контроллера...'
-          />
-        </Group>
-        <div className="form__btns">
-          <Button disabled={!isValid} className='mBtn_outline-green'>
-            {isEdited ? 'Сохранить' : 'Добавить'}
-          </Button>
-        </div>
-      </form>
-    </div>
-  )
+	return (
+		<div className="work-log__form">
+			{errorMessage}
+			<form className="form" onSubmit={handleSubmit(isEdited ? submitUpdate : submitCreate)}>
+				<Group>
+					<Input
+						label='Название контроллера'
+						name='name'
+						register={register}
+						errorMessage={errors.name?.message}
+						mandatory={true}
+						placeholder='Введите название контроллера...'
+					/>
+				</Group>
+				<Group>
+					<Input
+						label='Актуальная версия прошивки контроллера'
+						name='actualFirmwareVersion'
+						register={register}
+						errorMessage={errors.actualFirmwareVersion?.message}
+						placeholder='Введите актуальную версию прошивки...'
+					/>
+				</Group>
+				<div className="form__btns">
+					<Button disabled={!isValid} className='mBtn_outline-green'>
+						{isEdited ? 'Сохранить' : 'Добавить'}
+					</Button>
+				</div>
+			</form>
+		</div>
+	)
 }
 
 export default HeadControllerForm
