adb708566cb1092849c4ffa0e3a6fbb0ce748d9f
Mikhail <bolotnikovms@yandex.ru>
2025-10-17 16:38:14 +0500

    feat: Добавлен бэйдж с типом

--- src/components/cards/cardsSubstations/cardParts/content/CardContent.tsx
+++ src/components/cards/cardsSubstations/cardParts/content/CardContent.tsx
@@ -5,9 +5,12 @@ import { ICardContent } from './cardContent.interface'
 const CardContent: FC<ICardContent> = memo(({ substation }) => {
 	return (
 		<>
-			<div className="p-1 flex items-center gap-2">
+			<div className="p-1 flex flex-wrap items-center gap-2">
 				<Badge text={substation.object_type!} className='mBadge_blue' />
 				{substation.rdu && <Badge text='РДУ' className='mBadge_red' />}
+				{substation.telemechanics_devices && substation.telemechanics_devices.map(typeKp => (
+					<Badge key={typeKp.id} text={typeKp.type_kp} className='mBadge_dark_blue' />
+				))}
 			</div>
 			<p className='text-content flex items-center gap-1'>
 				<Icon id='link' />
