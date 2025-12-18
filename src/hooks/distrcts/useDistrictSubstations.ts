50c064b109f40a3297e35fb92b5f184b90a66238
Mikhail <bolotnikovms@yandex.ru>
2025-12-18 17:48:53 +0500

    style: Поправлены отступы

--- src/hooks/distrcts/useDistrictSubstations.ts
+++ src/hooks/distrcts/useDistrictSubstations.ts
@@ -3,16 +3,16 @@ import { IQueryParams } from '../../interfaces'
 import { DistrictService } from '../../services/district/district.service'
 
 interface IPropsDistrictSubstation extends IQueryParams {
-  id: string | undefined
+	id: string | undefined
 }
 
 export const useDistrictSubstations = ({ id, search, sort, order }: IPropsDistrictSubstation) => {
-  const { data, error, isError, isLoading } = useQuery({
-    queryKey: ['district-substations', id, search, sort, order],
-    queryFn: () => DistrictService.getSubstationsRelatedDistrict(id || '', { search, sort, order }),
-    staleTime: 1 * 60 * 1000,
-    enabled: !!id,
-  })
+	const { data, error, isError, isLoading } = useQuery({
+		queryKey: ['district-substations', id, search, sort, order],
+		queryFn: () => DistrictService.getSubstationsRelatedDistrict(id || '', { search, sort, order }),
+		staleTime: 1 * 60 * 1000,
+		enabled: !!id,
+	})
 
-  return { data, error, isError, isLoading }
+	return { data, error, isError, isLoading }
 }
